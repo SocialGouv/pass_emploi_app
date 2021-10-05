4e0b77f9c8a8f2ec9e7055f1f0ee73e1eac51db0
@gabriel_adgeg <gabriel.adgeg.ext@francetv.fr>
2021-10-05 19:11:25 +0200

    [PUSH] BSR

--- lib/push/push_notification_manager.dart
+++ lib/push/push_notification_manager.dart
@@ -12,11 +12,7 @@ class PushNotificationManager {
   Future<void> init(Store<AppState> store) async {
     NotificationSettings settings = await _firebaseMessaging.requestPermission(
       alert: true,
-      announcement: false,
       badge: true,
-      carPlay: false,
-      criticalAlert: false,
-      provisional: false,
       sound: true,
     );
     print('User granted permission: ${settings.authorizationStatus}');
