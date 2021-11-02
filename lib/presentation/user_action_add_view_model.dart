8f4af01224b5bd720079041456ac7b438ca43f00
Maxime Bonnet <mbonnet@octo.com>
2021-11-02 16:31:38 +0100

    chore: Format file

--- lib/presentation/user_action_add_view_model.dart
+++ lib/presentation/user_action_add_view_model.dart
@@ -4,15 +4,14 @@ import 'package:pass_emploi_app/redux/states/app_state.dart';
 import 'package:redux/redux.dart';
 
 class UserActionAddViewModel {
-
   final Function(String? actionContent, String? actionComment, UserActionStatus initialStatus) createUserAction;
 
   UserActionAddViewModel({required this.createUserAction});
 
   factory UserActionAddViewModel.create(Store<AppState> store) {
     return UserActionAddViewModel(
-      createUserAction: (content, comment, initialStatus) => { store.dispatch(
-          CreateUserAction(content, comment, initialStatus)) },
+      createUserAction: (content, comment, initialStatus) =>
+          {store.dispatch(CreateUserAction(content, comment, initialStatus))},
     );
   }
 }
