3100e317c7da75c34b79a43354fbdcce51c37898
@gabriel_adgeg <gabriel.adgeg.ext@francetv.fr>
2021-11-08 17:54:04 +0100

    feat(16): use same color for selected and unselected menu item

--- lib/widgets/selected_menu_item.dart
+++ lib/widgets/selected_menu_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:pass_emploi_app/ui/app_colors.dart';
 import 'package:pass_emploi_app/ui/drawables.dart';
 import 'package:pass_emploi_app/widgets/unselected_menu_item.dart';
 
@@ -16,7 +15,7 @@ class SelectedMenuItem extends StatelessWidget {
       alignment: Alignment.center,
       clipBehavior: Clip.none,
       children: [
-        UnselectedMenuItem(drawableRes: drawableRes, label: label, color: AppColors.nightBlue),
+        UnselectedMenuItem(drawableRes: drawableRes, label: label),
         Positioned(
           bottom: -6,
           child: Center(child: SvgPicture.asset(Drawables.icMenuSelectedBullet)),
--- lib/widgets/unselected_menu_item.dart
+++ lib/widgets/unselected_menu_item.dart
@@ -7,21 +7,19 @@ import 'package:pass_emploi_app/ui/text_styles.dart';
 class UnselectedMenuItem extends StatelessWidget {
   final String drawableRes;
   final String label;
-  final Color color;
 
-  const UnselectedMenuItem({
-    required this.drawableRes,
-    required this.label,
-    this.color = AppColors.bluePurple,
-  }) : super();
+  const UnselectedMenuItem({required this.drawableRes, required this.label}) : super();
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(child: SvgPicture.asset(drawableRes, color: color), height: Dimens.bottomNavigationBarItemHeight),
+        Container(
+          child: SvgPicture.asset(drawableRes, color: AppColors.nightBlue),
+          height: Dimens.bottomNavigationBarItemHeight,
+        ),
         SizedBox(height: 6.0),
-        Text(label, style: TextStyles.textMenuRegular(color)),
+        Text(label, style: TextStyles.textMenuRegular(AppColors.nightBlue)),
       ],
     );
   }
