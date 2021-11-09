35bea8290adc9b2dfe85a1f8dda77317e37dd888
@gabriel_adgeg <gabriel.adgeg.ext@francetv.fr>
2021-11-09 16:14:15 +0100

    fix(16): fix overflow issue on iPhone form factors

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -5,7 +5,6 @@ import 'package:pass_emploi_app/pages/home_page.dart';
 import 'package:pass_emploi_app/pages/rendezvous_list_page.dart';
 import 'package:pass_emploi_app/pages/user_action_list_page.dart';
 import 'package:pass_emploi_app/ui/app_colors.dart';
-import 'package:pass_emploi_app/ui/dimens.dart';
 import 'package:pass_emploi_app/ui/drawables.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
 import 'package:pass_emploi_app/widgets/menu_item.dart';
@@ -33,22 +32,19 @@ class _MainPageState extends State<MainPage> {
         color: AppColors.lightBlue,
         child: _content(_selectedIndex),
       ),
-      bottomNavigationBar: Container(
-        height: Dimens.bottomNavigationBarHeight,
-        child: BottomNavigationBar(
-          backgroundColor: Colors.white,
-          type: BottomNavigationBarType.fixed,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          items: <BottomNavigationBarItem>[
-            MenuItem(drawableRes: Drawables.icMenuHome, label: Strings.menuHome),
-            MenuItem(drawableRes: Drawables.icMenuAction, label: Strings.menuActions),
-            MenuItem(drawableRes: Drawables.icMenuChat, label: Strings.menuChat),
-            MenuItem(drawableRes: Drawables.icMenuRendezvous, label: Strings.menuRendezvous),
-          ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: <BottomNavigationBarItem>[
+          MenuItem(drawableRes: Drawables.icMenuHome, label: Strings.menuHome),
+          MenuItem(drawableRes: Drawables.icMenuAction, label: Strings.menuActions),
+          MenuItem(drawableRes: Drawables.icMenuChat, label: Strings.menuChat),
+          MenuItem(drawableRes: Drawables.icMenuRendezvous, label: Strings.menuRendezvous),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
--- lib/ui/dimens.dart
+++ lib/ui/dimens.dart
@@ -3,6 +3,5 @@ class Dimens {
 
   static const double appBarHeight = 93;
   static const double flatAppBarHeight = 59;
-  static const double bottomNavigationBarHeight = 71;
   static const double bottomNavigationBarItemHeight = 21;
 }
