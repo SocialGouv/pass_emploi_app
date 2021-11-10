a711e3043fd09f85df3f121e254bddc475691095
Maxime Bonnet <mbonnet@octo.com>
2021-11-10 08:55:13 +0100

    feat: 17 - add analytics and adjust app bar font

--- lib/pages/offre_emploi_list_page.dart
+++ lib/pages/offre_emploi_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pass_emploi_app/analytics/analytics_constants.dart';
 import 'package:pass_emploi_app/presentation/offre_emploi_list_page_view_model.dart';
 import 'package:pass_emploi_app/redux/actions/offre_emploi_actions.dart';
 import 'package:pass_emploi_app/redux/states/app_state.dart';
@@ -10,7 +11,12 @@ import 'package:pass_emploi_app/ui/text_styles.dart';
 import 'package:pass_emploi_app/widgets/default_app_bar.dart';
 
 class OffreEmploiListPage extends StatelessWidget {
-  const OffreEmploiListPage({Key? key}) : super(key: key);
+  OffreEmploiListPage._();
+
+  static MaterialPageRoute materialPageRoute() {
+    return MaterialPageRoute(
+        builder: (context) => OffreEmploiListPage._(), settings: AnalyticsRouteSettings.offreEmploiList());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +24,7 @@ class OffreEmploiListPage extends StatelessWidget {
       onInit: (store) => store.dispatch(SearchOffreEmploiAction(keywords: "", department: "")),
       builder: (context, vm) => _scaffold(context, vm),
       converter: (store) => OffreEmploiListPageViewModel.create(store),
+      onDidChange: (previousViewModel, viewModel) => print("LOL - ${viewModel.displayState}"),
     );
   }
 
@@ -25,7 +32,7 @@ class OffreEmploiListPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.lightBlue,
       appBar: FlatDefaultAppBar(
-        title: Text(Strings.offresEmploiTitle, style: TextStyles.h3Semi),
+        title: Text(Strings.offresEmploiTitle, style: TextStyles.textLgMedium),
       ),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
@@ -54,11 +61,14 @@ class OffreEmploiListPage extends StatelessWidget {
             itemViewModel.title,
             style: TextStyles.textSmMedium(),
           ),
-          SizedBox(height: 8),
-          Text(
-            itemViewModel.companyName,
-            style: TextStyles.textSmRegular(color: AppColors.bluePurple),
-          ),
+          if (itemViewModel.companyName != null)
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                itemViewModel.companyName!,
+                style: TextStyles.textSmRegular(color: AppColors.bluePurple),
+              ),
+            ),
           SizedBox(height: 8),
           Row(
             children: [
