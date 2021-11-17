0924854b382d472ee88c5509f1acaabc5c2c062a
Maxime Bonnet <mbonnet@octo.com>
2021-11-17 15:11:46 +0100

    build: fix staging release script

--- scripts/staging_release.sh
+++ scripts/staging_release.sh
@@ -16,18 +16,7 @@ flutter build ipa \
     --dart-define=FIREBASE_ENVIRONMENT_PREFIX=staging \
     --export-options-plist=ios/StagingOptionsPlist.plist
 
-flutter build apk \
-    --flavor staging \
-    --dart-define=SERVER_BASE_URL=$STAGING_SERVER_BASE_URL \
-    --dart-define=FIREBASE_ENVIRONMENT_PREFIX=staging
-
-firebase appdistribution:distribute build/app/outputs/flutter-apk/app-staging-release.apk \
-    --token "$STAGING_FIREBASE_CI_TOKEN" \
-    --app "$STAGING_ANDROID_APP_ID" \
-    --release-notes "$RELEASE_NOTE" \
-    --groups "$STAGING_FIREBASE_RELEASE_GROUPS"
-
-firebase appdistribution:distribute ios-release/pass_emploi_app.ipa \
+firebase appdistribution:distribute build/ios/ipa/pass_emploi_app.ipa \
     --token "$STAGING_FIREBASE_CI_TOKEN" \
     --app "$STAGING_IOS_APP_ID" \
     --release-notes "$RELEASE_NOTE" \
