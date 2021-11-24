c28fc04a177d3bc6ded211111451449440c09acb
@gabriel_adgeg <gabriel.adgeg.ext@francetv.fr>
2021-11-24 19:44:37 +0100

    feat : code-review > BSR on offre_emploi_details_page.dart

--- lib/pages/offre_emploi_details_page.dart
+++ lib/pages/offre_emploi_details_page.dart
@@ -45,10 +45,7 @@ class OffreEmploiDetailsPage extends TraceableStatelessWidget {
   }
 
   Scaffold _scaffold(Widget body) {
-    return Scaffold(
-      appBar: DefaultAppBar(title: Text(Strings.offerDetails, style: TextStyles.h3Semi)),
-      body: body,
-    );
+    return Scaffold(appBar: FlatDefaultAppBar(title: Text(Strings.offerDetails, style: TextStyles.h3Semi)), body: body);
   }
 
   Widget _loading() => Center(child: CircularProgressIndicator(color: AppColors.nightBlue));
@@ -101,7 +98,6 @@ class OffreEmploiDetailsPage extends TraceableStatelessWidget {
     final contractType = viewModel.contractType;
     final salary = viewModel.salary;
     final duration = viewModel.duration;
-
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       if (location != null)
         Padding(
@@ -127,25 +123,22 @@ class OffreEmploiDetailsPage extends TraceableStatelessWidget {
   }
 
   Widget _offerDescription(OffreEmploiDetailsPageViewModel viewModel) {
-    var offerDescription = viewModel.offerDescription != null ? viewModel.offerDescription! : "";
-
+    final offerDescription = viewModel.offerDescription;
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       _descriptionTitle(title: Strings.offerDetailsTitle, icon: SvgPicture.asset("assets/ic_one_point.svg")),
       _separator(8, 12),
-      Text(offerDescription, style: TextStyles.textSmRegular()),
+      if (offerDescription != null) Text(offerDescription, style: TextStyles.textSmRegular()),
       _spacer(30),
     ]);
   }
 
   Widget _profileDescription(OffreEmploiDetailsPageViewModel viewModel) {
-    var experience = viewModel.experience != null ? viewModel.experience! : "";
-
-    Widget? skills = _isDisplaySkillsBlock(skills: viewModel.skills);
-    Widget? softSkills = _isDisplaySoftSkillsBlock(softSkills: viewModel.softSkills);
-    Widget? educations = _isDisplayEducationsBlock(educations: viewModel.educations);
-    Widget? languages = _isDisplayLanguagesBlock(languages: viewModel.languages);
-    Widget? driverLicences = _isDisplayDriverLicencesBlock(driverLicences: viewModel.driverLicences);
-
+    final experience = viewModel.experience;
+    Widget? skills = _skillsBlock(skills: viewModel.skills);
+    Widget? softSkills = _softSkillsBlock(softSkills: viewModel.softSkills);
+    Widget? educations = _educationsBlock(educations: viewModel.educations);
+    Widget? languages = _languagesBlock(languages: viewModel.languages);
+    Widget? driverLicences = _driverLicencesBlock(driverLicences: viewModel.driverLicences);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -153,7 +146,7 @@ class OffreEmploiDetailsPage extends TraceableStatelessWidget {
         _separator(8, 20),
         Text(Strings.experienceTitle, style: TextStyles.textSmMedium(color: AppColors.bluePurple)),
         _spacer(12),
-        _experience(xp: experience),
+        if (experience != null) _experience(xp: experience),
         _separator(20, 20),
         if (skills != null) skills,
         if (softSkills != null) softSkills,
@@ -165,30 +158,25 @@ class OffreEmploiDetailsPage extends TraceableStatelessWidget {
   }
 
   Widget _companyDescription(OffreEmploiDetailsPageViewModel viewModel) {
-    Widget? companyDescription =
-        _isDisplayTextBlock(title: Strings.companyDescriptionTitle, content: viewModel.companyDescription);
-    Widget? adaptationTag =
-        _isDisplayBlueTag(tagTitle: Strings.companyAdaptedTitle, tagValue: viewModel.companyAdapted);
-    Widget? accessibilityTag =
-        _isDisplayBlueTag(tagTitle: Strings.companyAccessibilityTitle, tagValue: viewModel.companyAccessibility);
-
+    final companyName = viewModel.companyName;
+    final companyDescription = viewModel.companyDescription;
+    final companyAdapted = viewModel.companyAdapted != null ? viewModel.companyAdapted! : false;
+    final companyAccessibility = viewModel.companyAccessibility != null ? viewModel.companyAccessibility! : false;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         _descriptionTitle(title: Strings.companyTitle, icon: SvgPicture.asset("assets/ic_three_points.svg")),
         _separator(8, 30),
-        Text(viewModel.companyName!, style: TextStyles.textMdMedium),
-        if (adaptationTag != null) adaptationTag,
-        if (accessibilityTag != null) accessibilityTag,
+        if (companyName != null) Text(companyName, style: TextStyles.textMdMedium),
+        if (companyAdapted) _blueTag(tagTitle: Strings.companyAdaptedTitle),
+        if (companyAccessibility) _blueTag(tagTitle: Strings.companyAccessibilityTitle),
         _spacer(20),
-        if (companyDescription != null) companyDescription,
+        if (companyDescription != null) _companyDescriptionBlock(content: companyDescription),
       ],
     );
   }
 
-  Widget _spacer(double _height) {
-    return SizedBox(height: _height);
-  }
+  Widget _spacer(double _height) => SizedBox(height: _height);
 
   Widget _separator(double spacerBefore, double spacerAfter) {
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
@@ -201,60 +189,44 @@ class OffreEmploiDetailsPage extends TraceableStatelessWidget {
   Container _descriptionTitle({required String title, SvgPicture? icon}) {
     return Container(
       child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              if (icon != null)
-                Padding(
-                  padding: const EdgeInsets.only(right: 14),
-                  child: icon,
-                ),
-              Flexible(
-                child: Text(
-                  title,
-                  style: TextStyles.textLgMedium,
-                ),
-              ),
-            ],
-          )),
+        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (icon != null) Padding(padding: const EdgeInsets.only(right: 14), child: icon),
+            Flexible(child: Text(title, style: TextStyles.textLgMedium)),
+          ],
+        ),
+      ),
     );
   }
 
-  Container _experience({required String xp}) {
-    return Container(
-      child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Flexible(
-                child: Text(
-                  "· $xp",
-                  style: TextStyles.textSmRegular(),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 8),
-                child: SvgPicture.asset("assets/ic_info.svg"),
-              ),
-            ],
-          )),
+  Widget _experience({required String xp}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Flexible(child: Text("· $xp", style: TextStyles.textSmRegular())),
+          Padding(
+            padding: const EdgeInsets.only(left: 8),
+            child: SvgPicture.asset("assets/ic_info.svg"),
+          ),
+        ],
+      ),
     );
   }
 
-  Widget? _isDisplayTextBlock({required String title, required String? content}) {
-    return content != null
-        ? Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Text(title, style: TextStyles.textSmMedium(color: AppColors.bluePurple)),
-            _spacer(12),
-            Text(content, style: TextStyles.textSmRegular()),
-            _separator(12, 20),
-          ])
-        : null;
+  Widget _companyDescriptionBlock({required String content}) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Text(Strings.companyDescriptionTitle, style: TextStyles.textSmMedium(color: AppColors.bluePurple)),
+      _spacer(12),
+      Text(content, style: TextStyles.textSmRegular()),
+      _separator(12, 20),
+    ]);
   }
 
-  Widget? _isDisplaySkillsBlock({required List<Skill>? skills}) {
+  Widget? _skillsBlock({required List<Skill>? skills}) {
     if (skills == null || skills.isEmpty) return null;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -271,7 +243,7 @@ class OffreEmploiDetailsPage extends TraceableStatelessWidget {
     );
   }
 
-  Widget? _isDisplaySoftSkillsBlock({required List<String>? softSkills}) {
+  Widget? _softSkillsBlock({required List<String>? softSkills}) {
     if (softSkills == null || softSkills.isEmpty) return null;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -288,7 +260,7 @@ class OffreEmploiDetailsPage extends TraceableStatelessWidget {
     );
   }
 
-  Widget? _isDisplayEducationsBlock({required List<EducationViewModel>? educations}) {
+  Widget? _educationsBlock({required List<EducationViewModel>? educations}) {
     if (educations == null || educations.isEmpty) return null;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -305,7 +277,7 @@ class OffreEmploiDetailsPage extends TraceableStatelessWidget {
     );
   }
 
-  Widget? _isDisplayLanguagesBlock({required List<Language>? languages}) {
+  Widget? _languagesBlock({required List<Language>? languages}) {
     if (languages == null || languages.isEmpty) return null;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -322,7 +294,7 @@ class OffreEmploiDetailsPage extends TraceableStatelessWidget {
     );
   }
 
-  Widget? _isDisplayDriverLicencesBlock({required List<DriverLicence>? driverLicences}) {
+  Widget? _driverLicencesBlock({required List<DriverLicence>? driverLicences}) {
     if (driverLicences == null || driverLicences.isEmpty) return null;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -339,8 +311,7 @@ class OffreEmploiDetailsPage extends TraceableStatelessWidget {
     );
   }
 
-  Widget? _isDisplayBlueTag({required String tagTitle, required bool? tagValue}) {
-    if (tagValue == false) return null;
+  Widget _blueTag({required String tagTitle}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
