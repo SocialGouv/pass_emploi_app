15d648ca2da01cd66cfa92e9c980f5ee309a6ce4
@gabriel_adgeg <gad@octo.com>
2021-12-07 13:39:43 +0100

    feat: update login page design

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:matomo/matomo.dart';
 import 'package:pass_emploi_app/analytics/analytics_constants.dart';
 import 'package:pass_emploi_app/presentation/login_view_model.dart';
 import 'package:pass_emploi_app/redux/states/app_state.dart';
 import 'package:pass_emploi_app/ui/app_colors.dart';
+import 'package:pass_emploi_app/ui/drawables.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
 import 'package:pass_emploi_app/ui/text_styles.dart';
 
@@ -17,7 +19,53 @@ class LoginPage extends TraceableStatelessWidget {
     return StoreConnector<AppState, LoginViewModel>(
       converter: (store) => LoginViewModel.create(store),
       distinct: true,
-      builder: (context, viewModel) => Scaffold(body: _body(viewModel)),
+      builder: (context, viewModel) => _content(viewModel),
+    );
+  }
+
+  Scaffold _content(LoginViewModel viewModel) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [AppColors.lightBlue, AppColors.lightPurple],
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: SvgPicture.asset(Drawables.icLogo, width: 145, semanticsLabel: Strings.logoTextDescription),
+              ),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(left: 16, right: 16),
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(width: 1, color: Colors.white),
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(Strings.performLogin, style: TextStyles.textMdMedium, textAlign: TextAlign.center),
+                      SizedBox(height: 16),
+                      _body(viewModel),
+                    ],
+                  ),
+                ),
+              ),
+              Flexible(child: SizedBox()),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -32,23 +80,26 @@ class LoginPage extends TraceableStatelessWidget {
     }
   }
 
-  ClipRRect _loginButton(LoginViewModel viewModel) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(8),
-      child: Material(
-        child: InkWell(
-          onTap: () => viewModel.onLoginAction(),
-          child: Container(
-            height: 56,
-            child: Center(
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [Text(Strings.login, style: TextStyles.textSmMedium(color: Colors.white))],
+  Widget _loginButton(LoginViewModel viewModel) {
+    return Container(
+      margin: EdgeInsets.only(left: 16, right: 16),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Material(
+          child: InkWell(
+            onTap: () => viewModel.onLoginAction(),
+            child: Container(
+              height: 56,
+              child: Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [Text(Strings.login, style: TextStyles.textSmMedium(color: Colors.white))],
+                ),
               ),
             ),
           ),
+          color: AppColors.nightBlue,
         ),
-        color: AppColors.nightBlue,
       ),
     );
   }
