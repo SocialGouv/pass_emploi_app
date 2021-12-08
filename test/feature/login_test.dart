e0588e9f2af78f5480e57e759c04f7fb09837905
@gabriel_adgeg <gad@octo.com>
2021-12-08 16:53:57 +0100

    chore: move BootstrapAction to specific file

--- test/feature/login_test.dart
+++ test/feature/login_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pass_emploi_app/models/user.dart';
+import 'package:pass_emploi_app/redux/actions/bootstrap_action.dart';
 import 'package:pass_emploi_app/redux/actions/login_actions.dart';
-import 'package:pass_emploi_app/redux/actions/ui_actions.dart';
 import 'package:pass_emploi_app/redux/states/app_state.dart';
 import 'package:pass_emploi_app/redux/states/login_state.dart';
 import 'package:pass_emploi_app/redux/states/rendezvous_state.dart';
