26e461fefcf050418f60a21fd7a4999ac48961dc
@gabriel_adgeg <gad@octo.com>
2021-12-27 16:19:55 +0100

    refactor: 160 - use generic DisplayState in OffreEmploiSearchResultsViewModel

--- lib/pages/offre_emploi_list_page.dart
+++ lib/pages/offre_emploi_list_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:matomo/matomo.dart';
 import 'package:pass_emploi_app/analytics/analytics_constants.dart';
 import 'package:pass_emploi_app/pages/offre_emploi_details_page.dart';
+import 'package:pass_emploi_app/presentation/display_state.dart';
 import 'package:pass_emploi_app/presentation/offre_emploi_search_results_view_model.dart';
 import 'package:pass_emploi_app/redux/actions/offre_emploi_actions.dart';
 import 'package:pass_emploi_app/redux/states/app_state.dart';
@@ -49,11 +50,11 @@ class _OffreEmploiListPageState extends State<OffreEmploiListPage> {
       converter: (store) => OffreEmploiSearchResultsViewModel.create(store),
       onInitialBuild: (viewModel) => _currentViewModel = viewModel,
       builder: (context, viewModel) => _scaffold(context, viewModel),
-      onDidChange: (previousViewModel, viewModel) => {
+      onDidChange: (previousViewModel, viewModel) =>
+      {
         _currentViewModel = viewModel,
         _scrollController.jumpTo(_offsetBeforeLoading),
-        _shouldLoadAtBottom = viewModel.displayLoaderAtBottomOfList &&
-            viewModel.displayState != OffreEmploiSearchResultsDisplayState.SHOW_ERROR
+        _shouldLoadAtBottom = viewModel.displayLoaderAtBottomOfList && viewModel.displayState != DisplayState.FAILURE,
       },
       distinct: true,
       onDispose: (store) => store.dispatch(OffreEmploiResetResultsAction()),
@@ -111,7 +112,7 @@ class _OffreEmploiListPageState extends State<OffreEmploiListPage> {
   }
 
   Padding _buildLastItem(OffreEmploiSearchResultsViewModel resultsViewModel) {
-    if (resultsViewModel.displayState == OffreEmploiSearchResultsDisplayState.SHOW_ERROR) {
+    if (resultsViewModel.displayState == DisplayState.FAILURE) {
       return _buildErrorItem();
     } else {
       return _buildLoaderItem();
