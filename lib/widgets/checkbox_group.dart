dd773e2281ec0c5d0f05063332a5afab5f0b3dd1
Maxime Bonnet <mbonnet@octo.com>
2021-12-31 11:01:12 +0100

    feat: 119 - create a checkbox_group widget

---
+++ lib/widgets/checkbox_group.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:pass_emploi_app/ui/text_styles.dart';
+
+class CheckBoxGroup extends StatefulWidget {
+  final String title;
+  final List<String> options;
+  final Function(List<String> selectedOptions) onSelectedOptionsUpdated;
+
+  const CheckBoxGroup({
+    Key? key,
+    required this.title,
+    required this.options,
+    required this.onSelectedOptionsUpdated,
+  }) : super(key: key);
+
+  @override
+  _CheckBoxGroupState createState() => _CheckBoxGroupState();
+}
+
+class _CheckBoxGroupState extends State<CheckBoxGroup> {
+  late Map<String, bool> _optionsSelectionStatus;
+
+  @override
+  void initState() {
+    super.initState();
+    _optionsSelectionStatus = Map.fromEntries(widget.options.map((e) => MapEntry(e, false)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Text(widget.title, style: TextStyles.textMdMedium),
+          ),
+          SizedBox(height: 12),
+          ..._optionsSelectionStatus.entries.map<Widget>((entry) => _createCheckBox(entry.key, entry.value)).toList()
+        ],
+    );
+  }
+
+  Widget _createCheckBox(String key, bool isSelected) {
+    return CheckboxListTile(
+      title: Text(key, style: TextStyles.textMdRegular),
+      value: isSelected,
+      onChanged: (value) {
+        setState(() {
+          if (value != null) {
+            _optionsSelectionStatus[key] = value;
+            widget.onSelectedOptionsUpdated(_listOfSelectedOptions());
+          }
+        });
+      },
+    );
+  }
+
+  List<String> _listOfSelectedOptions() =>
+      _optionsSelectionStatus.entries.where((element) => element.value).map((e) => e.key).toList();
+}
