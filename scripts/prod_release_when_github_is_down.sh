1641064359c1ac48fbab524ef2f8570671264bbb
Maxime Bonnet <mbonnet@octo.com>
2022-01-06 14:56:53 +0100

    build: prod script

---
+++ scripts/prod_release_when_github_is_down.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Script stops if any command fails
+set -e
+set -o pipefail
+
+set -a
+source scripts/build.env
+set +a
+
+flutter test
+
+date=`echo "$(date +%s)"`
+
+flutter build ipa \
+    --flavor prod \
+    --export-options-plist=ios/ProdOptionsPlist.plist \
+    --build-number=$date
+
+flutter build apk \
+    --flavor prod \
+    --build-number=$date
