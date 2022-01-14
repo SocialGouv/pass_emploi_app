1ac4f1bc74fd1aab86c0d06ce7957ef08546d61b
remi <remi.dormoy@octo.com>
2022-01-14 17:25:11 +0100

    feat: refonte UI tabs

--- lib/pages/solutions_tabs_page.dart
+++ lib/pages/solutions_tabs_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:pass_emploi_app/pages/search_annonces_page.dart';
+import 'package:pass_emploi_app/ui/app_colors.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
 import 'package:pass_emploi_app/ui/text_styles.dart';
 import 'package:pass_emploi_app/widgets/default_app_bar.dart';
@@ -9,8 +10,8 @@ import 'boite_a_outils_page.dart';
 
 class SolutionsTabPage extends StatelessWidget {
   final List<Tab> solutionTabs = <Tab>[
-    Tab(child: Text(Strings.offresTabTitle, style: TextStyles.textMdMedium)),
-    Tab(child: Text(Strings.boiteAOutilsTabTitle, style: TextStyles.textMdMedium)),
+    Tab(child: Text(Strings.offresTabTitle)),
+    Tab(child: Text(Strings.boiteAOutilsTabTitle)),
   ];
 
   @override
@@ -20,7 +21,13 @@ class SolutionsTabPage extends StatelessWidget {
       child: Scaffold(
         appBar: passEmploiAppBar(
           label: Strings.solutionsAppBarTitle,
-          bottom: TabBar(tabs: solutionTabs),
+          bottom: TabBar(
+              tabs: solutionTabs,
+              indicatorColor: AppColors.primary,
+              indicatorWeight: 5,
+              labelStyle: TextStyles.textBaseBold,
+              labelColor: AppColors.contentColor,
+              unselectedLabelColor: AppColors.neutralColor),
         ),
         body: TabBarView(
           children: [
