26eef4456c834fb045440f027cb91d02b51993f0
remi <remi.dormoy@octo.com>
2022-01-14 17:25:11 +0100

    feat: refonte UI tabs

--- lib/pages/mon_suivi_tabs_page.dart
+++ lib/pages/mon_suivi_tabs_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:pass_emploi_app/pages/rendezvous_list_page.dart';
 import 'package:pass_emploi_app/pages/user_action_list_page.dart';
+import 'package:pass_emploi_app/ui/app_colors.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
 import 'package:pass_emploi_app/ui/text_styles.dart';
 import 'package:pass_emploi_app/widgets/default_app_bar.dart';
@@ -16,8 +17,12 @@ class MonSuiviTabPage extends StatelessWidget {
   MonSuiviTabPage({required this.initialTab, required this.showContent}) : super();
 
   final List<Tab> monSuiviTabs = <Tab>[
-    Tab(child: Text(Strings.actionsTabTitle, style: TextStyles.textMdMedium)),
-    Tab(child: Text(Strings.rendezvousTabTitle, style: TextStyles.textMdMedium)),
+    Tab(
+        child: Text(
+      Strings.actionsTabTitle,
+      textAlign: TextAlign.left,
+    )),
+    Tab(child: Text(Strings.rendezvousTabTitle, textAlign: TextAlign.left)),
   ];
 
   @override
@@ -28,7 +33,14 @@ class MonSuiviTabPage extends StatelessWidget {
       child: Scaffold(
         appBar: passEmploiAppBar(
           label: Strings.monSuiviAppBarTitle,
-          bottom: TabBar(tabs: monSuiviTabs),
+          bottom: TabBar(
+            indicatorColor: AppColors.primary,
+            indicatorWeight: 5,
+            labelStyle: TextStyles.textBaseBold,
+            labelColor: AppColors.contentColor,
+            unselectedLabelColor: AppColors.neutralColor,
+            tabs: monSuiviTabs,
+          ),
         ),
         body: _setTabContent(),
       ),
