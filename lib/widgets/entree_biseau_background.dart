c9f759d77b43a6c6b4751eb8d852aefbfc9373a5
@gabriel_adgeg <gad@octo.com>
2022-02-02 11:28:52 +0100

    chore: 274 - BSR

--- lib/widgets/entree_biseau_background.dart
+++ lib/widgets/entree_biseau_background.dart
@@ -2,21 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:pass_emploi_app/ui/app_colors.dart';
 
 class EntreeBiseauBackground extends StatelessWidget {
-  const EntreeBiseauBackground({
-    Key? key,
-  }) : super(key: key);
+  const EntreeBiseauBackground({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          color: AppColors.primary,
-        ),
+        Container(color: AppColors.primary),
         ClipPath(
-          child: Container(
-            color: AppColors.primaryDarken.withOpacity(0.25),
-          ),
+          child: Container(color: AppColors.primaryDarken.withOpacity(0.25)),
           clipper: DiagonalClipper(),
         ),
       ],
