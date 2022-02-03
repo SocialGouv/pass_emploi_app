6a288bb08799f30c53b0a713bf7f18ba160fa2b2
TTO <tto@octo.com>
2022-02-03 17:41:23 +0100

    fix: 310 - Use future to avoid lag on UI

--- test/repositories/metier_repository_test.dart
+++ test/repositories/metier_repository_test.dart
@@ -6,42 +6,42 @@ main() {
   group("_getMetiers should return ...", () {
     final MetierRepository repository = MetierRepository();
 
-    test("filtered list of metier when user input matches metier libelle", () {
-      matchesBtpEtPaysageMetier(repository.getMetiers("Dessin BTP et paysage"));
+    test("filtered list of metier when user input matches metier libelle", () async {
+      matchesBtpEtPaysageMetier(await repository.getMetiers("Dessin BTP et paysage"));
     });
 
-    test("filtered list of metier when user input has different cases in the metier name", () {
-      matchesBtpEtPaysageMetier(repository.getMetiers("dessin btp et paysage"));
-      matchesBtpEtPaysageMetier(repository.getMetiers("DESSIN BTP ET PAYSAGE"));
-      matchesBtpEtPaysageMetier(repository.getMetiers("DeSSin BTP eT paYSAgE"));
+    test("filtered list of metier when user input has different cases in the metier name", () async {
+      matchesBtpEtPaysageMetier(await repository.getMetiers("dessin btp et paysage"));
+      matchesBtpEtPaysageMetier(await repository.getMetiers("DESSIN BTP ET PAYSAGE"));
+      matchesBtpEtPaysageMetier(await repository.getMetiers("DeSSin BTP eT paYSAgE"));
     });
 
-    test("filtered list of metier when user input not complete", () {
-      expect(repository.getMetiers("paysage").length, 6);
+    test("filtered list of metier when user input not complete", () async {
+      expect((await repository.getMetiers("paysage")).length, 6);
     });
 
-    test("empty list when user input is empty", () {
-      expect(repository.getMetiers(""), []);
+    test("empty list when user input is empty", () async {
+      expect(await repository.getMetiers(""), []);
     });
 
-    test("empty list when user input contains only one character", () {
-      expect(repository.getMetiers("d"), []);
+    test("empty list when user input contains only one character", () async {
+      expect(await repository.getMetiers("d"), []);
     });
 
-    test("filtered list of metier when user input contains diacritics in the department name", () {
-      expect(repository.getMetiers("hôtellerie").length,28);
-      expect(repository.getMetiers("hotellerie").length,28);
+    test("filtered list of metier when user input contains diacritics in the department name", () async {
+      expect((await repository.getMetiers("hôtellerie")).length, 28);
+      expect((await repository.getMetiers("hotellerie")).length, 28);
     });
 
-    test("filtered list of metier when user input contains spaces in the department name", () {
-      expect(repository.getMetiers("Expertise technique couleur en industrie").length, 1);
-      expect(repository.getMetiers("Expertise technique couleur en industrie").first.codeRome, "H1201");
+    test("filtered list of metier when user input contains spaces in the department name", () async {
+      expect((await repository.getMetiers("Expertise technique couleur en industrie")).length, 1);
+      expect((await repository.getMetiers("Expertise technique couleur en industrie")).first.codeRome, "H1201");
 
-      expect(repository.getMetiers("Personnel d attractions").length, 1);
-      expect(repository.getMetiers("Personnel d attractions").first.codeRome, "G1205");
+      expect((await repository.getMetiers("Personnel d attractions")).length, 1);
+      expect((await repository.getMetiers("Personnel d attractions")).first.codeRome, "G1205");
 
-      expect(repository.getMetiers("           Chaudronnerie    tôlerie   ").length, 2);
-      expect(repository.getMetiers("           Chaudronnerie    tôlerie   ").first.codeRome, "H2902");
+      expect((await repository.getMetiers("           Chaudronnerie    tôlerie   ")).length, 2);
+      expect((await repository.getMetiers("           Chaudronnerie    tôlerie   ")).first.codeRome, "H2902");
     });
   });
 }
