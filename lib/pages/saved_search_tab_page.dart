4c31eb16536b1f0c70507dbea5681eaa79711f12
lela-oc-to <svetlana.lesik@octo.com>
2022-02-10 17:31:40 +0100

    chore: add favorite offers / search tabs

---
+++ lib/pages/saved_search_tab_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class SavedSearchTabPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
