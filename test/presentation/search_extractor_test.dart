f92a904565fe26e97910e1e3b48e37bba2431373
@gabriel_adgeg <gad@octo.com>
2022-02-14 15:15:30 +0100

    feat: 325 - add saved search button to empty immersion results page

--- test/presentation/search_extractor_test.dart
+++ test/presentation/search_extractor_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pass_emploi_app/models/immersion.dart';
+import 'package:pass_emploi_app/models/metier.dart';
 import 'package:pass_emploi_app/models/offre_emploi_filtres_parameters.dart';
 import 'package:pass_emploi_app/models/saved_search/immersion_saved_search.dart';
 import 'package:pass_emploi_app/models/saved_search/offre_emploi_saved_search.dart';
@@ -7,6 +8,7 @@ import 'package:pass_emploi_app/models/saved_search/saved_search_extractors.dart
 import 'package:pass_emploi_app/redux/states/app_state.dart';
 import 'package:pass_emploi_app/redux/states/immersion_search_request_state.dart';
 import 'package:pass_emploi_app/redux/states/offre_emploi_search_parameters_state.dart';
+import 'package:pass_emploi_app/redux/states/search_metier_state.dart';
 import 'package:pass_emploi_app/redux/states/state.dart';
 
 import '../doubles/fixtures.dart';
@@ -44,7 +46,7 @@ main() {
     expect(result, expected);
   });
 
-  test("tests immersion search extractor", () {
+  test("tests immersion search extractor when immersion result is not empty", () {
     // Given
     final testStoreFactory = TestStoreFactory();
     final immersionState = State.success([
@@ -56,29 +58,71 @@ main() {
         ville: "ville",
       )
     ]);
+    final searchedMetier = Metier.values.first;
     AppState state = AppState.initialState().copyWith(
-        immersionSearchState: immersionState,
-        immersionSearchRequestState: RequestedImmersionSearchRequestState(
-          codeRome: "codeRome",
-          latitude: 12,
-          longitude: 34,
-          ville: "ville"));
+      searchMetierState: SearchMetierState([searchedMetier]),
+      immersionSearchState: immersionState,
+      immersionSearchRequestState: RequestedImmersionSearchRequestState(
+        codeRome: searchedMetier.codeRome,
+        latitude: 12,
+        longitude: 34,
+        ville: "ville",
+      ),
+    );
     final store = testStoreFactory.initializeReduxStore(initialState: state);
 
     // When
     final result = ImmersionSearchExtractor().getSearchFilters(store);
 
     // Then
-    final expected = ImmersionSavedSearch(
-      title: "metier - ville",
-      metier: "metier",
-      location: "ville",
-      filters: ImmersionSearchParametersFilters.withFilters(
-        codeRome: "codeRome",
-        lat: 12,
-        lon: 34,
+    expect(
+      result,
+      ImmersionSavedSearch(
+        title: "metier - ville",
+        metier: "metier",
+        location: "ville",
+        filters: ImmersionSearchParametersFilters.withFilters(
+          codeRome: searchedMetier.codeRome,
+          lat: 12,
+          lon: 34,
+        ),
       ),
     );
-    expect(result, expected);
+  });
+
+  test("tests immersion search extractor when immersion result is empty", () {
+    // Given
+    final testStoreFactory = TestStoreFactory();
+    final immersionState = State.success(<Immersion>[]);
+    final searchedMetier = Metier.values.first;
+    AppState state = AppState.initialState().copyWith(
+      searchMetierState: SearchMetierState([searchedMetier]),
+      immersionSearchState: immersionState,
+      immersionSearchRequestState: RequestedImmersionSearchRequestState(
+        codeRome: searchedMetier.codeRome,
+        latitude: 12,
+        longitude: 34,
+        ville: "ville",
+      ),
+    );
+    final store = testStoreFactory.initializeReduxStore(initialState: state);
+
+    // When
+    final result = ImmersionSearchExtractor().getSearchFilters(store);
+
+    // Then
+    expect(
+      result,
+      ImmersionSavedSearch(
+        title: "${searchedMetier.libelle} - ville",
+        metier: searchedMetier.libelle,
+        location: "ville",
+        filters: ImmersionSearchParametersFilters.withFilters(
+          codeRome: searchedMetier.codeRome,
+          lat: 12,
+          lon: 34,
+        ),
+      ),
+    );
   });
 }
