9ed213a17f75dd52a32649d43074a70c0111e6f0
lela-oc-to <svetlana.lesik@octo.com>
2022-02-14 15:15:39 +0100

    chore: saved search extractor

--- lib/network/filtres_request.dart
+++ lib/network/filtres_request.dart
@@ -12,6 +12,19 @@ class FiltresRequest {
     }
   }
 
+  static ExperienceFiltre experienceFromUrlParameter(String url) {
+    switch (url) {
+      case "1":
+        return ExperienceFiltre.de_zero_a_un_an;
+      case "2":
+        return ExperienceFiltre.de_un_a_trois_ans;
+      case "3":
+        return ExperienceFiltre.trois_ans_et_plus;
+      default:
+        throw Exception("Experience type not defined: $url");
+    }
+  }
+
   static String contratToUrlParameter(ContratFiltre filtre) {
     switch (filtre) {
       case ContratFiltre.cdi:
@@ -23,12 +36,36 @@ class FiltresRequest {
     }
   }
 
+  static ContratFiltre contratFromUrlParameter(String url) {
+    switch (url) {
+      case "CDI":
+        return ContratFiltre.cdi;
+      case "CDD-interim-saisonnier":
+        return ContratFiltre.cdd_interim_saisonnier;
+      case "autre":
+        return ContratFiltre.autre;
+      default:
+        throw Exception("Contrat type not defined: $url");
+    }
+  }
+
   static String dureeToUrlParameter(DureeFiltre filtre) {
     switch (filtre) {
       case DureeFiltre.temps_plein:
         return "1";
       case DureeFiltre.temps_partiel:
         return "2";
+    }
+  }
+
+  static DureeFiltre dureeFromUrlParameter(String url) {
+    switch (url) {
+      case "1":
+        return DureeFiltre.temps_plein;
+      case "2":
+        return DureeFiltre.temps_partiel;
+      default:
+        throw Exception("Duree type not defined: $url");
     }
   }
 }
--- lib/repositories/saved_search/saved_search_response.dart
+++ lib/repositories/saved_search/saved_search_response.dart
@@ -1,3 +1,10 @@
+import 'package:pass_emploi_app/models/location.dart';
+import 'package:pass_emploi_app/models/saved_search/offre_emploi_saved_search.dart';
+import 'package:pass_emploi_app/network/filtres_request.dart';
+
+import '../../models/offre_emploi_filtres_parameters.dart';
+import '../../models/saved_search/immersion_saved_search.dart';
+
 class SavedSearchResponse {
   final String id;
   final String titre;
@@ -30,34 +37,99 @@ class SavedSearchResponse {
 class SavedSearchResponseCriteres {
   final String? q;
   final String? departement;
-  final bool? alternance;
+  final bool alternance;
   final List<String>? experience;
   final List<String>? contrat;
   final List<String>? duree;
   final String? commune;
   final double? rayon;
+  final double? lon;
+  final double? lat;
+  final String? rome;
 
-  SavedSearchResponseCriteres({
-    this.q,
-    this.departement,
-    this.alternance,
-    this.experience,
-    this.contrat,
-    this.duree,
-    this.commune,
-    this.rayon,
-  });
+  SavedSearchResponseCriteres(
+      {this.q,
+      this.departement,
+      required this.alternance,
+      this.experience,
+      this.contrat,
+      this.duree,
+      this.commune,
+      this.rayon,
+      this.lon,
+      this.lat,
+      this.rome});
 
   factory SavedSearchResponseCriteres.fromJson(dynamic json) {
     return SavedSearchResponseCriteres(
       q: json["q"] as String?,
       departement: json["departement"] as String?,
-      alternance: json["alternance"] as bool?,
+      alternance: json["alternance"] as bool,
       experience: (json["experience"] as List?)?.map((e) => e as String).toList(),
       contrat: (json["contrat"] as List?)?.map((e) => e as String).toList(),
       duree: (json["duree"] as List?)?.map((e) => e as String).toList(),
       commune: json["commune"] as String?,
       rayon: json["rayon"] as double?,
+      lat: json["lat"] as double?,
+      lon: json["lon"] as double?,
+      rome: json["rome"] as String?,
+    );
+  }
+}
+
+class SavedSearchEmploiExtractor {
+  OffreEmploiSavedSearch extract(SavedSearchResponse savedSearch) {
+    return OffreEmploiSavedSearch(
+      title: savedSearch.titre,
+      metier: savedSearch.metier,
+      location: _getLocation(savedSearch),
+      keywords: savedSearch.criteres.q,
+      isAlternance: savedSearch.criteres.alternance,
+      filters: _getFilters(savedSearch.criteres),
+    );
+  }
+
+  Location? _getLocation(SavedSearchResponse savedSearch) {
+    if (savedSearch.localisation != null) {
+      final type = savedSearch.criteres.commune == null ? LocationType.DEPARTMENT : LocationType.COMMUNE;
+      return Location(
+        type: type,
+        code: type == LocationType.DEPARTMENT ? savedSearch.criteres.departement! : savedSearch.criteres.commune!,
+        libelle: savedSearch.localisation ?? "",
+        codePostal: null,
+        longitude: null,
+        latitude: null,
+      );
+    } else {
+      return null;
+    }
+  }
+
+  OffreEmploiSearchParametersFiltres _getFilters(SavedSearchResponseCriteres criteres) {
+    return OffreEmploiSearchParametersFiltres.withFiltres(
+      distance: criteres.rayon?.toInt(),
+      experience: criteres.experience?.map((e) => FiltresRequest.experienceFromUrlParameter(e)).toList(),
+      contrat: criteres.contrat?.map((e) => FiltresRequest.contratFromUrlParameter(e)).toList(),
+      duree: criteres.duree?.map((e) => FiltresRequest.dureeFromUrlParameter(e)).toList(),
+    );
+  }
+}
+
+class SavedSearchImmersionExtractor {
+  ImmersionSavedSearch extract(SavedSearchResponse savedSearch) {
+    return ImmersionSavedSearch(
+      title: savedSearch.titre,
+      metier: savedSearch.metier ?? "",
+      location: savedSearch.localisation ?? "",
+      filters: _getFilters(savedSearch.criteres),
+    );
+  }
+
+  ImmersionSearchParametersFilters _getFilters(SavedSearchResponseCriteres criteres) {
+    return ImmersionSearchParametersFilters.withFilters(
+      codeRome: criteres.rome,
+      lat: criteres.lat,
+      lon: criteres.lon,
     );
   }
 }
