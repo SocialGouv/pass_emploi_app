2bf066bf40cacbb65516a65538e4aa5aec8ec17a
remi <remi.dormoy@octo.com>
2022-02-23 15:22:22 +0100

    fixed call and middleware

--- lib/redux/middlewares/search_service_civique_middleware.dart
+++ lib/redux/middlewares/search_service_civique_middleware.dart
@@ -1,11 +1,8 @@
-import 'package:pass_emploi_app/models/location.dart';
 import 'package:pass_emploi_app/redux/actions/search_service_civique_actions.dart';
 import 'package:pass_emploi_app/redux/states/app_state.dart';
-
 import 'package:redux/redux.dart';
 
 import '../../repositories/service_civique_repository.dart';
-import '../actions/search_location_action.dart';
 
 class SearchServiceCiviqueMiddleware extends MiddlewareClass<AppState> {
   final ServiceCiviqueRepository _repository;
@@ -16,19 +13,20 @@ class SearchServiceCiviqueMiddleware extends MiddlewareClass<AppState> {
   call(Store<AppState> store, action, NextDispatcher next) async {
     next(action);
     if (action is SearchServiceCiviqueAction) {
-      final List<Location> locations = [];
-      final input = action.input;
-      if (input != null && input.length > 1) {
-        final loginState = store.state.loginState;
-        if (loginState.isSuccess()) {
-          locations.addAll(await _repository.getLocations(
-            userId: loginState.getResultOrThrow().id,
-            query: input,
-            villesOnly: action.villesOnly,
-          ));
-        }
+      final loginState = store.state.loginState;
+      if (loginState.isSuccess()) {
+        _repository.search(
+          userId: loginState.getResultOrThrow().id,
+          request: SearchServiceCiviqueRequest(
+            domain: null,
+            location: action.location,
+            distance: null,
+            startDate: null,
+            endDate: null,
+            page: 0,
+          ),
+        );
       }
-      store.dispatch(SearchLocationsSuccessAction(locations));
     }
   }
 }
