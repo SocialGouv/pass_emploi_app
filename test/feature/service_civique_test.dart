9996505745ee042cf401e2482a9e82df5b5da5f1
lela-oc-to <svetlana.lesik@octo.com>
2022-02-28 14:27:01 +0100

    chore: add tests

---
+++ test/feature/service_civique_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pass_emploi_app/redux/actions/search_service_civique_actions.dart';
+import 'package:pass_emploi_app/redux/states/service_civique/service_civique_search_result_state.dart';
+
+import '../doubles/fixtures.dart';
+import '../doubles/stubs.dart';
+import '../utils/test_setup.dart';
+
+main() {
+  test("service civique should be loaded", () async {
+    // Given
+    final factory = TestStoreFactory();
+    factory.serviceCiviqueRepository = ServiceCiviqueRepositorySuccessWithMoreDataStub();
+    final store = factory.initializeReduxStore(initialState: loggedInState());
+
+    final displayedLoading = store.onChange.any((e) => e.serviceCiviqueSearchResultState is ServiceCiviqueSearchResultLoadingState);
+    final successState = store.onChange.firstWhere((e) => e.serviceCiviqueSearchResultState is ServiceCiviqueSearchResultDataState);
+
+    // When
+    store.dispatch(SearchServiceCiviqueAction(location: mockLocation()));
+
+    // Then
+    expect(await displayedLoading, true);
+
+    final successAppState = await successState;
+    final searchState = (successAppState.serviceCiviqueSearchResultState as ServiceCiviqueSearchResultDataState);
+    expect(searchState.offres.length, 1);
+    expect(searchState.loadedPage, 1);
+  });
+
+
+  test("service civique should be fetched and an error be displayed if something wrong happens", () async {
+    // Given
+    final testStoreFactory = TestStoreFactory();
+    testStoreFactory.serviceCiviqueRepository = ServiceCiviqueRepositoryFailureStub();
+    final store = testStoreFactory.initializeReduxStore(initialState: loggedInState());
+
+    final displayedLoading = store.onChange.any((e) => e.serviceCiviqueSearchResultState is ServiceCiviqueSearchResultLoadingState);
+    final displayedError = store.onChange.any((e) => e.serviceCiviqueSearchResultState is ServiceCiviqueSearchResultErrorState);
+
+    // When
+    store.dispatch(SearchServiceCiviqueAction(location: mockLocation()));
+
+    // Then
+    expect(await displayedLoading, true);
+    expect(await displayedError, true);
+  });
+}
