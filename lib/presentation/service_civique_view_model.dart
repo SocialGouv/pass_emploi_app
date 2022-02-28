7f30857a0514acf07b17994947f51f4f71423401
remi <remi.dormoy@octo.com>
2022-02-28 15:37:46 +0100

    chore: added display state

--- lib/presentation/service_civique_view_model.dart
+++ lib/presentation/service_civique_view_model.dart
@@ -1,21 +1,26 @@
 import 'package:equatable/equatable.dart';
+import 'package:pass_emploi_app/features/service_civique/search/search_service_civique_actions.dart';
+import 'package:pass_emploi_app/features/service_civique/search/service_civique_search_result_state.dart';
 import 'package:pass_emploi_app/models/location.dart';
+import 'package:pass_emploi_app/presentation/display_state.dart';
 import 'package:pass_emploi_app/presentation/location_view_model.dart';
 import 'package:redux/redux.dart';
 
-import '../redux/actions/search_location_action.dart';
 import '../features/service_civique/search/search_service_civique_actions.dart';
+import '../redux/actions/search_location_action.dart';
 import '../redux/states/app_state.dart';
 
 class ServiceCiviqueViewModel extends Equatable {
   final List<LocationViewModel> locations;
   final Function(Location? location) onSearchRequest;
   final Function(String? input) onInputLocation;
+  final DisplayState displayState;
 
   ServiceCiviqueViewModel._({
     required this.locations,
     required this.onSearchRequest,
     required this.onInputLocation,
+    required this.displayState,
   });
 
   factory ServiceCiviqueViewModel.create(Store<AppState> store) {
@@ -23,6 +28,7 @@ class ServiceCiviqueViewModel extends Equatable {
         locations: store.state.searchLocationState.locations
             .map((location) => LocationViewModel.fromLocation(location))
             .toList(),
+        displayState: _displayState(store.state.serviceCiviqueSearchResultState),
         onSearchRequest: (location) {
           store.dispatch(SearchServiceCiviqueAction(location: location));
         },
@@ -31,4 +37,14 @@ class ServiceCiviqueViewModel extends Equatable {
 
   @override
   List<Object?> get props => [locations];
+
+  static DisplayState _displayState(ServiceCiviqueSearchResultState state) {
+    if (state is ServiceCiviqueSearchResultDataState) {
+      return DisplayState.CONTENT;
+    } else if (state is ServiceCiviqueSearchResultErrorState) {
+      return DisplayState.FAILURE;
+    } else {
+      return DisplayState.LOADING;
+    }
+  }
 }
