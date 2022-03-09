3fafdabb7662aae7a02cf2775b6d3156b5a16af3
remi <remi.dormoy@octo.com>
2022-03-09 16:09:20 +0100

    fix: empty immersion saved search

--- lib/features/immersion/list/immersion_list_request.dart
+++ lib/features/immersion/list/immersion_list_request.dart
@@ -3,6 +3,7 @@ import 'package:pass_emploi_app/models/location.dart';
 class ImmersionListRequest {
   final String codeRome;
   final Location location;
+  final String? title;
 
-  ImmersionListRequest(this.codeRome, this.location);
+  ImmersionListRequest(this.codeRome, this.location, [this.title]);
 }
