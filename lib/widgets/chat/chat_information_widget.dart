1c713369ab28a4aab7108ff1e37e799448e48438
lela-oc-to <svetlana.lesik@octo.com>
2022-04-07 18:10:28 +0200

    chore: add ui

--- lib/widgets/chat/chat_information_widget.dart
+++ lib/widgets/chat/chat_information_widget.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:pass_emploi_app/ui/app_colors.dart';
+import 'package:pass_emploi_app/ui/drawables.dart';
+import 'package:pass_emploi_app/ui/margins.dart';
+import 'package:pass_emploi_app/ui/text_styles.dart';
 
 class ChatInformationWidget extends StatelessWidget {
   final String title;
@@ -8,6 +13,49 @@ class ChatInformationWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: Margins.spacing_s),
+      decoration: BoxDecoration(
+        color: AppColors.primaryLighten,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(Margins.spacing_m),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _title(),
+            SizedBox(height: Margins.spacing_s),
+            _description(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _title() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(Margins.spacing_xs),
+          child: SvgPicture.asset(Drawables.icInfo, color: AppColors.primary),
+        ),
+        SizedBox(width: Margins.spacing_s),
+        Flexible(
+          child: Text(
+            title,
+            style: TextStyles.textBaseBoldWithColor(AppColors.primary),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _description() {
+    return Text(
+      description,
+      style: TextStyles.textBaseRegularWithColor(AppColors.primary),
+    );
   }
 }
