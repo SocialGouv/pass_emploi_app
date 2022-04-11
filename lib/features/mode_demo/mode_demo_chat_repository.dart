d8bc17cf66b156fe894b5c6264846ef7804c6467
lela-oc-to <svetlana.lesik@octo.com>
2022-04-11 17:19:22 +0200

    chore: update json + add hard coded chat messages

--- lib/features/mode_demo/mode_demo_chat_repository.dart
+++ lib/features/mode_demo/mode_demo_chat_repository.dart
@@ -1,3 +1,16 @@
 import 'package:pass_emploi_app/models/message.dart';
 
-List<Message> ModeDemoChat() => [];
+List<Message> modeDemoChat() => [
+      Message(
+        "Hello !",
+        DateTime.now().subtract(Duration(minutes: 34)),
+        Sender.jeune,
+        MessageType.message,
+      ),
+      Message(
+        " Bonjour Monsieur",
+        DateTime.now().subtract(Duration(minutes: 30)),
+        Sender.conseiller,
+        MessageType.message,
+      )
+    ];
