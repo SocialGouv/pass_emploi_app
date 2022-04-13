574032944e6cada7423a816600fc688082bb2228
remi <remi.dormoy@octo.com>
2022-04-13 17:13:44 +0200

    tech: add custom linter rules to ban launch and pushes

--- lib/analytics/analytics_extensions.dart
+++ lib/analytics/analytics_extensions.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:matomo/matomo.dart';
 
-extension StatelessWidgetAnalyticsExtension on StatelessWidget {
+extension StatelessWidgetAnalyticsExtension on Widget {
   Future<T?> pushAndTrackBack<T>(BuildContext context, Route<T> route, String name) {
+    // ignore: ban-name
     return Navigator.push(context, route).then((value) {
       MatomoTracker.trackScreenWithName(name, "");
       return value;
@@ -12,6 +13,7 @@ extension StatelessWidgetAnalyticsExtension on StatelessWidget {
 
 extension StatefulWidgetAnalyticsExtension on StatefulWidget {
   Future<T?> pushAndTrackBack<T>(BuildContext context, Route<T> route, String name) {
+    // ignore: ban-name
     return Navigator.push(context, route).then((value) {
       MatomoTracker.trackScreenWithName(name, "");
       return value;
