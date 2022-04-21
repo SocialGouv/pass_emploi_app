d0aaae573d2a0f46dbc75a781a4bd7bc4d53eae4
lela-oc-to <svetlana.lesik@octo.com>
2022-04-21 15:19:52 +0200

    fix: remove useless class

--- lib/pages/mon_suivi_tabs_page.dart
+++ lib/pages/mon_suivi_tabs_page.dart
@@ -72,25 +72,3 @@ class MonSuiviTabPage extends StatelessWidget {
     );
   }
 }
-
-class PersistentHeader extends SliverPersistentHeaderDelegate {
-  final Widget widget;
-
-  PersistentHeader({required this.widget});
-
-  @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container();
-  }
-
-  @override
-  double get maxExtent => 0;
-
-  @override
-  double get minExtent => 0;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return true;
-  }
-}
