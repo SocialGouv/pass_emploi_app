593b887117630c69a3e50e7b0898e633ed302b11
remi <remi.dormoy@octo.com>
2022-05-06 11:59:05 +0200

    corrected linter

--- lib/features/mode_demo/mode_demo_reducer.dart
+++ lib/features/mode_demo/mode_demo_reducer.dart
@@ -3,7 +3,8 @@ import 'package:pass_emploi_app/features/login/login_actions.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
 
 bool modeDemoReducer(AppState current, dynamic action) {
-  if (action is LoginSuccessAction)
+  if (action is LoginSuccessAction) {
     return action.user.loginMode == LoginMode.DEMO_PE || action.user.loginMode == LoginMode.DEMO_MILO;
+  }
   return current.demoState;
 }
