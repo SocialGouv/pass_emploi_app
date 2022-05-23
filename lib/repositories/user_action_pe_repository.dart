70e600c082cdf1bae9ad7655e45e362fd3e739e1
Jordan Chapuy <chapuyj@gmail.com>
2022-05-23 11:57:47 +0200

    feat: 636 - add new route home demarches to repository

--- lib/repositories/user_action_pe_repository.dart
+++ lib/repositories/user_action_pe_repository.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'package:pass_emploi_app/crashlytics/crashlytics.dart';
+import 'package:pass_emploi_app/models/home_demarches.dart';
 import 'package:pass_emploi_app/models/user_action_pe.dart';
 import 'package:pass_emploi_app/network/json_utf8_decoder.dart';
 import 'package:pass_emploi_app/network/status_code.dart';
@@ -11,6 +12,20 @@ class UserActionPERepository {
   final Crashlytics? _crashlytics;
 
   UserActionPERepository(this._baseUrl, this._httpClient, [this._crashlytics]);
+
+  Future<HomeDemarches?> getHomeDemarches(String userId) async {
+    final url = Uri.parse(_baseUrl + "/jeunes/$userId/home/demarches");
+    try {
+      final response = await _httpClient.get(url);
+      if (response.statusCode.isValid()) {
+        final json = jsonUtf8Decode(response.bodyBytes);
+        return HomeDemarches.fromJson(json);
+      }
+    } catch (e, stack) {
+      _crashlytics?.recordNonNetworkException(e, stack, url);
+    }
+    return null;
+  }
 
   Future<List<UserActionPE>?> getUserActions(String userId) async {
     final url = Uri.parse(_baseUrl + "/jeunes/$userId/pole-emploi/actions");
--- test/repositories/user_action_pe_repository_test.dart
+++ test/repositories/user_action_pe_repository_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
+import 'package:pass_emploi_app/models/campagne.dart';
+import 'package:pass_emploi_app/models/home_demarches.dart';
 import 'package:pass_emploi_app/models/user_action_pe.dart';
 import 'package:pass_emploi_app/repositories/user_action_pe_repository.dart';
 
@@ -9,6 +11,60 @@ import '../utils/test_assets.dart';
 import '../utils/test_datetime.dart';
 
 void main() {
+  test('get page action', () async {
+    // Given
+    final httpClient = MockClient((request) async {
+      if (request.method != "GET") return invalidHttpResponse();
+      if (!request.url.toString().startsWith("BASE_URL/jeunes/UID/home/demarches")) return invalidHttpResponse();
+      return Response.bytes(loadTestAssetsAsBytes("home_demarches.json"), 200);
+    });
+    final repository = UserActionPERepository("BASE_URL", httpClient);
+
+    // When
+    final HomeDemarches? result = await repository.getHomeDemarches("UID");
+
+    // Then
+    expect(result, isNotNull);
+    expect(result?.campagne, isNotNull);
+    expect(
+      result?.campagne,
+      Campagne(
+        id: "id-campagne",
+        titre: "Votre expérience sur l'application",
+        description: "Donnez nous votre avis",
+        questions: [
+          Question(id: 1, libelle: "la question ?", options: [
+            Option(id: 1, libelle: "Non, pas du tout"),
+          ])
+        ],
+      ),
+    );
+    expect(result?.actions, isNotNull);
+    expect(result?.actions.length, 5);
+    expect(
+      result?.actions.first,
+      UserActionPE(
+        id: "8802034",
+        content: "Faire le CV",
+        status: UserActionPEStatus.NOT_STARTED,
+        endDate: parseDateTimeUtcWithCurrentTimeZone('2022-03-28T16:06:48.396Z'),
+        deletionDate: parseDateTimeUtcWithCurrentTimeZone('2022-03-28T16:06:48.396Z'),
+        createdByAdvisor: true,
+      ),
+    );
+    expect(
+      result?.actions[1],
+      UserActionPE(
+        id: "8392839",
+        content: "Compléter son CV",
+        status: UserActionPEStatus.IN_PROGRESS,
+        endDate: parseDateTimeUtcWithCurrentTimeZone('2022-03-28T16:06:48.396Z'),
+        deletionDate: parseDateTimeUtcWithCurrentTimeZone('2022-03-28T16:06:48.396Z'),
+        createdByAdvisor: true,
+      ),
+    );
+  });
+
   test('get user actions pôle emploi when response is valid should return user actions list', () async {
     // Given
     final httpClient = MockClient((request) async {
