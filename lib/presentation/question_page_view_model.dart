3b1f2c668acfa84a8938f396bcf0051336aeb3b3
Jordan Chapuy <chapuyj@gmail.com>
2022-05-24 14:44:27 +0200

    feat: 636 - k peut évaluer viewmodel wip

--- lib/presentation/question_page_view_model.dart
+++ lib/presentation/question_page_view_model.dart
@@ -1,5 +1,23 @@
 import 'package:pass_emploi_app/models/campagne.dart';
 
-class QuestionViewModel {
-  QuestionViewModel();
+class QuestionPageViewModel {
+  final String titre = "Votre expérience 1/2";
+  final String? information = "Les questions marquées d'une * sont obligatoires";
+  final String question = "Est-ce c'est super la vie ?";
+  final List<String> options = ["Non", "Oui", "Peut-être"];
+
+  final int? answerSelectedIndex = null;
+  final String? pourquoiAnswer = null;
+
+  final QuestionBottomButton bottomButton = QuestionBottomButton.next;
+
+  QuestionPageViewModel({
+    required Campagne campagne,
+    required ReponsesCampagneState reponsesState,
+    required int pageOffset,
+  });
 }
+
+enum QuestionBottomButton { next, validate }
+
+class ReponsesCampagneState {}
