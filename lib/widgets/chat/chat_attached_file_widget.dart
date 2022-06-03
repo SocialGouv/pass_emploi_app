b24d1c58bb7adc22aa969cc00c9dae107345b52e
Jordan Chapuy <chapuyj@gmail.com>
2022-06-03 16:16:02 +0200

    feat: 678 - displaying Piece Jointe on chat extracting widgets

--- lib/widgets/chat/chat_attached_file_widget.dart
+++ lib/widgets/chat/chat_attached_file_widget.dart
@@ -20,48 +20,97 @@ class ChatAttachedFileWidget extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            margin: EdgeInsets.only(
-              left: 0,
-              right: 77.0,
-            ),
-            padding: const EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              color: AppColors.primaryLighten,
-              borderRadius: BorderRadius.all(Radius.circular(16)),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(item.message, style: TextStyles.textSRegular()),
-                SizedBox(height: 14),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Padding(padding: const EdgeInsets.only(right: 10), child: SvgPicture.asset(Drawables.icClip)),
-                    Flexible(
-                      child: Text(
-                        item.filename,
-                        style: TextStyles.textSBoldWithColor(Colors.black),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: PrimaryActionButton(
-                    label: Strings.download,
-                    drawableRes: Drawables.icDownload,
-                    onPressed: () => {},
-                    heightPadding: 2,
-                  ),
-                ),
-              ],
-            ),
+          _MessageBubble(
+            children: [
+              Text(item.message, style: TextStyles.textSRegular()),
+              SizedBox(height: 14),
+              _PieceJointeName(item.filename),
+              SizedBox(height: 20),
+              _DownloadButton(),
+            ],
           ),
-          SizedBox(height: Margins.spacing_xs),
-          Text(item.caption, style: TextStyles.textXsRegular()),
+          _Caption(item.caption),
         ],
+      ),
+    );
+  }
+}
+
+class _MessageBubble extends StatelessWidget {
+  final List<Widget> children;
+
+  const _MessageBubble({required this.children}) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        left: 0,
+        right: 77.0,
+      ),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: AppColors.primaryLighten,
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: children,
+      ),
+    );
+  }
+}
+
+class _PieceJointeName extends StatelessWidget {
+  final String filename;
+
+  const _PieceJointeName(this.filename) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(padding: const EdgeInsets.only(right: 10), child: SvgPicture.asset(Drawables.icClip)),
+        Flexible(
+          child: Text(
+            filename,
+            style: TextStyles.textSBoldWithColor(Colors.black),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Caption extends StatelessWidget {
+  final String text;
+
+  const _Caption(this.text) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: Margins.spacing_xs),
+        Text(text, style: TextStyles.textXsRegular()),
+      ],
+    );
+  }
+}
+
+class _DownloadButton extends StatelessWidget {
+  const _DownloadButton() : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: PrimaryActionButton(
+        label: Strings.download,
+        drawableRes: Drawables.icDownload,
+        onPressed: () => {},
+        heightPadding: 2,
       ),
     );
   }
