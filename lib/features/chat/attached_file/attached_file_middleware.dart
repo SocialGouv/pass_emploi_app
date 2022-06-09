d97cd5e8ee82ccf50259ae775e722601266bce44
lela-oc-to <svetlana.lesik@octo.com>
2022-06-09 16:52:06 +0200

    poc: add share in middleware

--- lib/features/chat/attached_file/attached_file_middleware.dart
+++ lib/features/chat/attached_file/attached_file_middleware.dart
@@ -3,6 +3,7 @@ import 'package:pass_emploi_app/features/login/login_state.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
 import 'package:pass_emploi_app/repositories/attached_file_repository.dart';
 import 'package:redux/redux.dart';
+import 'package:share_plus/share_plus.dart';
 
 class AttachedFileMiddleware extends MiddlewareClass<AppState> {
   final AttachedFileRepository _repository;
@@ -18,6 +19,9 @@ class AttachedFileMiddleware extends MiddlewareClass<AppState> {
       store.dispatch(path != null && path.isNotEmpty
           ? AttachedFileSuccessAction(action.fileId, path)
           : AttachedFileFailureAction(action.fileId));
+      if (path != null && path.isNotEmpty) {
+        Share.shareFiles([path], text: 'Cute one');
+      }
     }
   }
 }
