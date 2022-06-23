7647a768107e539c41e29c4914d2a1c34bf71e50
Jordan Chapuy <chapuyj@gmail.com>
2022-06-23 18:10:09 +0200

    feat: 745 - retrouver son message dans le chat state tests

--- lib/features/chat/brouillon/chat_brouillon_state.dart
+++ lib/features/chat/brouillon/chat_brouillon_state.dart
@@ -1,5 +1,10 @@
-class ChatBrouillonState {
+import 'package:equatable/equatable.dart';
+
+class ChatBrouillonState extends Equatable {
   final String? message;
 
   ChatBrouillonState(this.message);
+
+  @override
+  List<Object?> get props => [message];
 }
