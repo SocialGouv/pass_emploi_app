4d8ecea1832da3c307d451dff44a6303e2cca21f
lela-oc-to <svetlana.lesik@octo.com>
2022-07-26 18:59:58 +0200

    test: 813 add feature tests

---
+++ test/feature/share_preferences/share_preferences_test.dart
@@ -0,0 +1,64 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pass_emploi_app/features/share_preferences/share_preferences_actions.dart';
+import 'package:pass_emploi_app/features/share_preferences/share_preferences_state.dart';
+import 'package:pass_emploi_app/models/share_preferences.dart';
+import 'package:pass_emploi_app/repositories/share_preferences_repository.dart';
+
+import '../../doubles/dummies.dart';
+import '../../dsl/app_state_dsl.dart';
+
+void main() {
+  test("share preferences should be fetched and displayed when screen loads", () async {
+    // Given
+
+    final store = givenState()
+        .loggedInMiloUser()
+        .store((factory) => {factory.sharePreferencesRepository = SharePreferencesRepositorySuccessStub()});
+
+    final displayedLoading = store.onChange.any((e) => e.sharePreferencesState is SharePreferencesLoadingState);
+    final successState = store.onChange.firstWhere((e) => e.sharePreferencesState is SharePreferencesSuccessState);
+
+    // When
+    store.dispatch(SharePreferencesRequestAction());
+
+    // Then
+    expect(await displayedLoading, true);
+    final appState = await successState;
+    expect((appState.sharePreferencesState as SharePreferencesSuccessState).preferences.shareFavoris, true);
+  });
+
+  test("share preferences should be loaded and error displayed when repository returns null", () async {
+    // Given
+    final store = givenState()
+        .loggedInMiloUser()
+        .store((factory) => {factory.sharePreferencesRepository = SharePreferencesRepositoryFailureStub()});
+
+    final displayedLoading = store.onChange.any((e) => e.sharePreferencesState is SharePreferencesLoadingState);
+    final displayedError = store.onChange.any((e) => e.sharePreferencesState is SharePreferencesFailureState);
+
+    // When
+    store.dispatch(SharePreferencesRequestAction());
+
+    // Then
+    expect(await displayedLoading, true);
+    expect(await displayedError, true);
+  });
+}
+
+class SharePreferencesRepositorySuccessStub extends SharePreferencesRepository {
+  SharePreferencesRepositorySuccessStub() : super("", DummyHttpClient());
+
+  @override
+  Future<SharePreferences?> getSharePreferences(String userId) async {
+    return SharePreferences(shareFavoris: true);
+  }
+}
+
+class SharePreferencesRepositoryFailureStub extends SharePreferencesRepository {
+  SharePreferencesRepositoryFailureStub() : super("", DummyHttpClient());
+
+  @override
+  Future<SharePreferences?> getSharePreferences(String userId) async {
+    return null;
+  }
+}
---
+++ test/feature/share_preferences/share_preferences_update_test.dart
@@ -0,0 +1,94 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pass_emploi_app/features/share_preferences/share_preferences_state.dart';
+import 'package:pass_emploi_app/features/share_preferences/update/share_preferences_update_actions.dart';
+import 'package:pass_emploi_app/features/share_preferences/update/share_preferences_update_state.dart';
+import 'package:pass_emploi_app/models/share_preferences.dart';
+import 'package:pass_emploi_app/redux/app_state.dart';
+import 'package:pass_emploi_app/repositories/share_preferences_repository.dart';
+import 'package:redux/src/store.dart';
+
+import '../../doubles/dummies.dart';
+import '../../dsl/app_state_dsl.dart';
+
+void main() {
+  test("share preferences state should be updated when user change preferences and api call succeeds", () async {
+    // Given
+    final Store<AppState> store = _successStoreWithLoadedSharePreferences();
+
+    final loadingState = store.onChange.any((e) => e.sharePreferencesUpdateState is SharePreferencesUpdateLoadingState);
+    final successState =
+        store.onChange.firstWhere((e) => e.sharePreferencesUpdateState is SharePreferencesUpdateSuccessState);
+
+    // When
+    store.dispatch(SharePreferencesUpdateRequestAction(true));
+
+    // Then
+    expect(await loadingState, true);
+    final updatedPreferences = await successState;
+    final preferencesState = (updatedPreferences.sharePreferencesUpdateState as SharePreferencesUpdateSuccessState);
+    expect(preferencesState.favorisShared, true);
+  });
+
+  test("share preferences state should not be updated when user change preferences and api call fails", () async {
+    // Given
+    final Store<AppState> store = _failureStoreWithLoadedSharePreferences();
+
+    final loadingState = store.onChange.any((e) => e.sharePreferencesUpdateState is SharePreferencesUpdateLoadingState);
+    final failureState =
+        store.onChange.firstWhere((e) => e.sharePreferencesUpdateState is SharePreferencesUpdateFailureState);
+
+    // When
+    store.dispatch(SharePreferencesUpdateRequestAction(true));
+
+    // Then
+    expect(await loadingState, true);
+    final updatedPreferences = await failureState;
+    expect(updatedPreferences.sharePreferencesUpdateState, isA<SharePreferencesUpdateFailureState>());
+  });
+}
+
+Store<AppState> _successStoreWithLoadedSharePreferences() {
+  final store = givenState()
+      .loggedInMiloUser()
+      .copyWith(sharePreferencesState: SharePreferencesSuccessState(SharePreferences(shareFavoris: false)))
+      .store((factory) => {factory.sharePreferencesRepository = SharePreferencesRepositorySuccessStub()});
+
+  return store;
+}
+
+Store<AppState> _failureStoreWithLoadedSharePreferences() {
+  final store = givenState()
+      .loggedInMiloUser()
+      .copyWith(sharePreferencesState: SharePreferencesSuccessState(SharePreferences(shareFavoris: false)))
+      .store((factory) => {factory.sharePreferencesRepository = SharePreferencesRepositoryFailureStub()});
+
+  return store;
+}
+
+class SharePreferencesRepositorySuccessStub extends SharePreferencesRepository {
+  SharePreferencesRepositorySuccessStub() : super("", DummyHttpClient());
+
+  @override
+  Future<SharePreferences?> getSharePreferences(String userId) async {
+    return SharePreferences(shareFavoris: true);
+  }
+
+  @override
+  Future<bool> updateSharePreferences(String userId, bool isShare) async {
+    return true;
+  }
+}
+
+class SharePreferencesRepositoryFailureStub extends SharePreferencesRepository {
+  SharePreferencesRepositoryFailureStub() : super("", DummyHttpClient());
+
+  @override
+  Future<SharePreferences?> getSharePreferences(String userId) async {
+    return null;
+  }
+
+  @override
+  Future<bool> updateSharePreferences(String userId, bool isShare) async {
+    return false;
+  }
+}
