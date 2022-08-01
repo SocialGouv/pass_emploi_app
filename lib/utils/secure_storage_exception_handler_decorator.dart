ffe9e6eab1df1e9e0c904b495fb808dc23ed020c
gabriel.adgeg <gabriel.adgeg@octo.com>
2022-08-01 12:48:22 +0200

    fix: Delete corrupted ID token when required.
    This is made to avoid bugs (mainly on iOS) where saved ID TOKEN is not compliant to its expected interface.

---
+++ lib/utils/secure_storage_exception_handler_decorator.dart
@@ -0,0 +1,87 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:pass_emploi_app/crashlytics/crashlytics.dart';
+
+class SecureStorageExceptionHandlerDecorator extends FlutterSecureStorage {
+  final FlutterSecureStorage decorated;
+  final Crashlytics? crashlytics;
+
+  SecureStorageExceptionHandlerDecorator(this.decorated, [this.crashlytics]);
+
+  @override
+  Future<void> write({
+    required String key,
+    required String? value,
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) async {
+    try {
+      await decorated.write(
+        key: key,
+        value: value,
+        iOptions: iOptions,
+        aOptions: aOptions,
+        lOptions: lOptions,
+        webOptions: webOptions,
+        mOptions: mOptions,
+        wOptions: wOptions,
+      );
+    } catch (exception, stack) {
+      crashlytics?.recordNonNetworkException(exception, stack);
+    }
+  }
+
+  @override
+  Future<String?> read({
+    required String key,
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) async {
+    try {
+      return await decorated.read(
+        key: key,
+        iOptions: iOptions,
+        aOptions: aOptions,
+        lOptions: lOptions,
+        webOptions: webOptions,
+        mOptions: mOptions,
+        wOptions: wOptions,
+      );
+    } catch (exception, stack) {
+      crashlytics?.recordNonNetworkException(exception, stack);
+      return null;
+    }
+  }
+
+  @override
+  Future<void> delete({
+    required String key,
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) async {
+    try {
+      await decorated.delete(
+        key: key,
+        iOptions: iOptions,
+        aOptions: aOptions,
+        lOptions: lOptions,
+        webOptions: webOptions,
+        mOptions: mOptions,
+        wOptions: wOptions,
+      );
+    } catch (exception, stack) {
+      crashlytics?.recordNonNetworkException(exception, stack);
+    }
+  }
+}
---
+++ test/utils/secure_storage_exception_handler_decorator_test.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pass_emploi_app/utils/secure_storage_exception_handler_decorator.dart';
+
+void main() {
+  final secureStorage = SecureStorageExceptionHandlerDecorator(_ExceptionThrowerSecureStorage());
+
+  test('write should not propagate exception', () {
+    expect(() => secureStorage.write(key: 'key', value: 'value'), returnsNormally);
+  });
+
+  test('read should not propagate exception', () {
+    expect(() => secureStorage.read(key: 'key'), returnsNormally);
+  });
+
+  test('delete should not propagate exception', () {
+    expect(() => secureStorage.delete(key: 'key'), returnsNormally);
+  });
+}
+
+class _ExceptionThrowerSecureStorage extends FlutterSecureStorage {
+  @override
+  Future<void> write({
+    required String key,
+    required String? value,
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) async {
+    throw PlatformException(code: 'code');
+  }
+
+  @override
+  Future<String?> read({
+    required String key,
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) async {
+    throw PlatformException(code: 'code');
+  }
+
+  @override
+  Future<void> delete({
+    required String key,
+    IOSOptions? iOptions,
+    AndroidOptions? aOptions,
+    LinuxOptions? lOptions,
+    WebOptions? webOptions,
+    MacOsOptions? mOptions,
+    WindowsOptions? wOptions,
+  }) async {
+    throw PlatformException(code: 'code');
+  }
+}
