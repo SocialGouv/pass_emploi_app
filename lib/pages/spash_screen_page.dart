a5c3d5c3dbcbd7eca6329eab743fbff6df3ec967
gabriel.adgeg <gabriel.adgeg@octo.com>
2022-08-01 13:00:53 +0200

    chore: Add app version on splash screen

--- lib/pages/spash_screen_page.dart
+++ lib/pages/spash_screen_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:matomo/matomo.dart';
+import 'package:package_info/package_info.dart';
 import 'package:pass_emploi_app/analytics/analytics_constants.dart';
 import 'package:pass_emploi_app/ui/drawables.dart';
+import 'package:pass_emploi_app/ui/margins.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
+import 'package:pass_emploi_app/ui/text_styles.dart';
 import 'package:pass_emploi_app/widgets/entree_biseau_background.dart';
 
 class SplashScreenPage extends TraceableStatelessWidget {
@@ -11,11 +14,40 @@ class SplashScreenPage extends TraceableStatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        EntreeBiseauBackground(),
-        Center(child: SvgPicture.asset(Drawables.cejAppLogo, semanticsLabel: Strings.logoTextDescription)),
-      ],
+    return FutureBuilder<String>(
+      future: PackageInfo.fromPlatform().then((platform) => platform.version),
+      builder: (context, snapshot) {
+        final appVersion = snapshot.data;
+        return Scaffold(
+          body: Stack(
+            children: [
+              EntreeBiseauBackground(),
+              Center(child: SvgPicture.asset(Drawables.cejAppLogo, semanticsLabel: Strings.logoTextDescription)),
+              if (appVersion != null) _AppVersion(appVersion),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _AppVersion extends StatelessWidget {
+  final String _appVersion;
+
+  const _AppVersion(this._appVersion);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: Padding(
+        padding: const EdgeInsets.all(Margins.spacing_base),
+        child: Text(
+          _appVersion,
+          style: TextStyles.textBaseBoldWithColor(Colors.white),
+        ),
+      ),
     );
   }
 }
