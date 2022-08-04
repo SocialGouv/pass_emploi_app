59bb60c1b4841cc38c43e50504462d82398a4adc
gabriel.adgeg <gabriel.adgeg@octo.com>
2022-08-04 14:20:15 +0200

    refactor: Add 4 different ways to test Redux loop

--- test/feature/rendezvous/rendezvous_list_test.dart
+++ test/feature/rendezvous/rendezvous_list_test.dart
@@ -5,9 +5,11 @@ import 'package:pass_emploi_app/features/rendezvous/rendezvous_state.dart';
 import 'package:pass_emploi_app/models/rendezvous.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
 import 'package:pass_emploi_app/repositories/rendezvous/rendezvous_repository.dart';
+import 'package:redux/src/store.dart';
 
 import '../../doubles/dummies.dart';
 import '../../doubles/fixtures.dart';
+import '../../dsl/app_state_dsl.dart';
 import '../../utils/test_setup.dart';
 
 void main() {
@@ -29,11 +31,11 @@ void main() {
 
     group("when user is logged in", () {
       group("when fetching rendez-vous futurs", () {
-        test("should update to success state", () async {
+        test("should update to success state 0", () async {
           // Given
-          final testStoreFactory = TestStoreFactory();
-          testStoreFactory.rendezvousRepository = RendezvousRepositorySuccessStub(expectedUserId: "id");
-          final store = testStoreFactory.initializeReduxStore(initialState: loggedInState());
+          final Store<AppState> store = givenState()
+              .loggedInUser() //
+              .store((f) => {f.rendezvousRepository = RendezvousRepositorySuccessStub(expectedUserId: "id")});
 
           final displayedLoading =
               store.onChange.any((e) => e.rendezvousState.futurRendezVousStatus == RendezvousStatus.LOADING);
@@ -48,6 +50,109 @@ void main() {
           final appState = await successAppState;
           expect(appState.rendezvousState.rendezvous.length, 1);
           expect(appState.rendezvousState.rendezvous.first.id, 'futur');
+        });
+
+        test("should update to success state 1", () async {
+          // Given
+          final Store<AppState> store = givenState()
+              .loggedInUser() //
+              .store((f) => {f.rendezvousRepository = RendezvousRepositorySuccessStub(expectedUserId: "id")});
+
+          // When
+          store.whenActionIsDispatched(RendezvousRequestAction(RendezvousPeriod.FUTUR));
+
+          // Then
+          expect(await store.onChange.any((e) => e.rendezvousState.futurRendezVousStatus == RendezvousStatus.LOADING),
+              true);
+          final appState = await store.onChange
+              .firstWhere((e) => e.rendezvousState.futurRendezVousStatus == RendezvousStatus.SUCCESS);
+          expect(appState.rendezvousState.rendezvous.length, 1);
+          expect(appState.rendezvousState.rendezvous.first.id, 'futur');
+        });
+
+        test("should update to success state 2", () async {
+          // Given
+          final Store<AppState> store = givenState()
+              .loggedInUser() //
+              .store((f) => {f.rendezvousRepository = RendezvousRepositorySuccessStub(expectedUserId: "id")});
+
+          // When
+          store.whenActionIsDispatched(RendezvousRequestAction(RendezvousPeriod.FUTUR));
+
+          // Then
+          await expectLater(
+            store.onChange.map((state) => state.rendezvousState),
+            emitsInOrder([
+              predicate<RendezvousState>((state) {
+                expect(state.futurRendezVousStatus, RendezvousStatus.NOT_INITIALIZED);
+                return true;
+              }),
+              predicate<RendezvousState>((state) {
+                expect(state.futurRendezVousStatus, RendezvousStatus.LOADING);
+                return true;
+              }),
+              predicate<RendezvousState>((state) {
+                expect(state.futurRendezVousStatus, RendezvousStatus.SUCCESS);
+                expect(state.rendezvous.length, 1);
+                expect(state.rendezvous.first.id, 'futur');
+                return true;
+              })
+            ]),
+          );
+        });
+
+        test("should update to success state 3", () async {
+          // Given
+          final Store<AppState> store = givenState()
+              .loggedInUser() //
+              .store((f) => {f.rendezvousRepository = RendezvousRepositorySuccessStub(expectedUserId: "id")});
+
+          // When
+          store.whenActionIsDispatched(RendezvousRequestAction(RendezvousPeriod.FUTUR));
+
+          // Then
+          await expectLater(
+            store.observe((state) => state.rendezvousState),
+            emitsInOrder([
+              expectState<RendezvousState>((state) => state.futurRendezVousStatus, RendezvousStatus.NOT_INITIALIZED),
+              expectState<RendezvousState>((state) => state.futurRendezVousStatus, RendezvousStatus.LOADING),
+              expectState<RendezvousState>(
+                (state) => state.futurRendezVousStatus,
+                RendezvousStatus.SUCCESS,
+                additionalAssertions: (state) {
+                  expect(state.rendezvous.length, 1);
+                  expect(state.rendezvous.first.id, 'futur');
+                },
+              ),
+            ]),
+          );
+        });
+
+        test("should update to success state 4", () async {
+          // Given
+          final Store<AppState> store = givenState()
+              .loggedInUser() //
+              .store((f) => {f.rendezvousRepository = RendezvousRepositorySuccessStub(expectedUserId: "id")});
+
+          // When
+          store.whenActionIsDispatched(RendezvousRequestAction(RendezvousPeriod.FUTUR));
+
+          // Then
+          await store.thenExpectStateChanges(
+            (state) => state.rendezvousState,
+            [
+              expectState<RendezvousState>((state) => state.futurRendezVousStatus, RendezvousStatus.NOT_INITIALIZED),
+              expectState<RendezvousState>((state) => state.futurRendezVousStatus, RendezvousStatus.LOADING),
+              expectState<RendezvousState>(
+                (state) => state.futurRendezVousStatus,
+                RendezvousStatus.SUCCESS,
+                additionalAssertions: (state) {
+                  expect(state.rendezvous.length, 1);
+                  expect(state.rendezvous.first.id, 'futur');
+                },
+              ),
+            ],
+          );
         });
 
         test("should update to failure state", () async {
@@ -144,4 +249,26 @@ class RendezvousRepositoryFailureStub extends RendezvousRepository {
     if (userId != expectedUserId) throw Exception("Unexpected user ID: $userId");
     return null;
   }
+}
+
+extension StoreTestExtension on Store<AppState> {
+  Future<dynamic> whenActionIsDispatched(dynamic action) {
+    return Future.delayed(Duration(milliseconds: 10)).then((_) => dispatch(action));
+  }
+
+  Stream<S> observe<S>(S Function(AppState state) convert) {
+    return onChange.map((event) => convert(event));
+  }
+
+  Future<void> thenExpectStateChanges<S>(S Function(AppState state) convert, Iterable matchers) async {
+    return expectLater(observe(convert), emitsInOrder(matchers));
+  }
+}
+
+Matcher expectState<T>(Function(T) f, dynamic matcher, {Function(T)? additionalAssertions}) {
+  return predicate<T>((state) {
+    expect(f(state), matcher);
+    additionalAssertions?.call(state);
+    return true;
+  });
 }
