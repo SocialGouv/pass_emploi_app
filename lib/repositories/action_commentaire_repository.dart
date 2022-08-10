fd2f521695b6efd61ca350c01edeee167bd3663d
lela-oc-to <svetlana.lesik@octo.com>
2022-08-10 17:52:02 +0200

    chore(891): clean repo

--- lib/repositories/action_commentaire_repository.dart
+++ lib/repositories/action_commentaire_repository.dart
@@ -1,11 +1,8 @@
 import 'package:http/http.dart';
-import 'package:intl/intl.dart';
 import 'package:pass_emploi_app/crashlytics/crashlytics.dart';
 import 'package:pass_emploi_app/models/commentaire.dart';
 import 'package:pass_emploi_app/network/json_utf8_decoder.dart';
 import 'package:pass_emploi_app/network/status_code.dart';
-
-import '../models/user_action_creator.dart';
 
 class ActionCommentaireRepository {
   final String _baseUrl;
