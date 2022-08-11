dbb988519ad996a96a7d354e99288a9354b7b332
lela-oc-to <svetlana.lesik@octo.com>
2022-08-11 16:34:56 +0200

    chore(891): add strings

--- lib/ui/strings.dart
+++ lib/ui/strings.dart
@@ -216,6 +216,10 @@ class Strings {
   static String doneActionsTitle = "Actions terminées et annulées";
   static String rappelSwitch = 'Recevoir une notification de rappel 3 jours avant l’échéance';
 
+  // Commentaires d'action
+  static String lastComment = "Dernier commentaire";
+  static String noComments = "Vous n’avez pas encore de commentaire";
+  static String createdByAdvisor(String advisor) => "Votre conseiller $advisor";
 
   // Demarches
   static String modifierStatut = "Modifier le statut";
