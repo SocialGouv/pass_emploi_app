6198cfef460715283fd02dbbbd2815008baeccdc
Jordan Chapuy <chapuyj@gmail.com>
2022-08-18 14:54:15 +0200

    feat(923): creating agenda page

---
+++ lib/pages/agenda_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:pass_emploi_app/analytics/tracker.dart';
+import 'package:pass_emploi_app/features/agenda/agenda_actions.dart';
+import 'package:pass_emploi_app/presentation/agenda_view_model.dart';
+import 'package:pass_emploi_app/redux/app_state.dart';
+import 'package:pass_emploi_app/ui/app_colors.dart';
+
+class AgendaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Tracker(
+      tracking: "todo",
+      child: StoreConnector<AppState, AgendaPageViewModel>(
+        onInit: (store) => store.dispatch(AgendaRequestAction()),
+        builder: (context, viewModel) => _scaffold(context, viewModel),
+        converter: (store) => AgendaPageViewModel.create(store),
+        distinct: true,
+      ),
+    );
+  }
+
+  Widget _scaffold(BuildContext context, AgendaPageViewModel viewModel) {
+    return Scaffold(
+      backgroundColor: AppColors.grey100,
+      body: Text("hey"),
+    );
+  }
+}
