863759a3cf4fd31bd1353d97606ac02c36244cff
Jordan Chapuy <chapuyj@gmail.com>
2022-08-22 12:11:08 +0200

    feat(923): viewmodel have both actions and rendezvous

--- test/presentation/agenda/agenda_view_model_test.dart
+++ test/presentation/agenda/agenda_view_model_test.dart
@@ -1,15 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pass_emploi_app/features/agenda/agenda_state.dart';
+import 'package:pass_emploi_app/models/user_action.dart';
 import 'package:pass_emploi_app/presentation/agenda/agenda_view_model.dart';
 import 'package:pass_emploi_app/presentation/display_state.dart';
+import 'package:pass_emploi_app/presentation/user_action/user_action_tag_view_model.dart';
+import 'package:pass_emploi_app/presentation/user_action/user_action_view_model.dart';
 
 import '../../doubles/fixtures.dart';
 import '../../dsl/app_state_dsl.dart';
 
 void main() {
-
   group('display state', () {
-
     test('should be loading on notinit state', () {
       // Given
       final store = givenState().loggedInUser().copyWith(agendaState: AgendaNotInitializedState()).store();
@@ -65,4 +67,25 @@ void main() {
     });
   });
 
+  group('events', () {
+    test('have both actions and rendezvous', () {
+      // Given
+      final actions = [userActionStub(), userActionStub()];
+      final rendezvous = [rendezvousStub(), rendezvousStub(), rendezvousStub()];
+      final store = givenState().loggedInUser().agenda(actions: actions, rendezvous: rendezvous).store();
+
+      // When
+      final viewModel = AgendaPageViewModel.create(store);
+
+      // Then
+      _expectCount(events: viewModel.events, actions: 2, rendezvous: 3);
+    });
+  });
+}
+
+void _expectCount({required List<dynamic> events, required int actions, required int rendezvous}) {
+  final actualActionCount = events.where((e) => e is UserActionViewModel).length;
+  final actualRendezvousCount = events.where((e) => e is RendezvousAgendaViewModel).length;
+  expect(actualActionCount, actions, reason: "Mauvais nombre d'actions");
+  expect(actualRendezvousCount, rendezvous, reason: "Mauvais nombre de rendez-vous");
 }
