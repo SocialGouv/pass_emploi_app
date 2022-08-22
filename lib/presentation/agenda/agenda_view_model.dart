01027443f7726fc8437710c3816ad3270e5b0660
Jordan Chapuy <chapuyj@gmail.com>
2022-08-22 12:11:08 +0200

    feat(923): viewmodel have both actions and rendezvous

--- lib/presentation/agenda/agenda_view_model.dart
+++ lib/presentation/agenda/agenda_view_model.dart
@@ -1,20 +1,25 @@
 import 'package:equatable/equatable.dart';
 import 'package:pass_emploi_app/features/agenda/agenda_state.dart';
 import 'package:pass_emploi_app/presentation/display_state.dart';
+import 'package:pass_emploi_app/presentation/user_action/user_action_view_model.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
 import 'package:redux/redux.dart';
 
 class AgendaPageViewModel extends Equatable {
   final DisplayState displayState;
+  final List<dynamic> events;
 
-  AgendaPageViewModel({required this.displayState});
+  AgendaPageViewModel({required this.displayState, required this.events});
 
   factory AgendaPageViewModel.create(Store<AppState> store) {
-    return AgendaPageViewModel(displayState: _displayState(store));
+    return AgendaPageViewModel(
+      displayState: _displayState(store),
+      events: _events(store),
+    );
   }
 
   @override
-  List<Object?> get props => [displayState];
+  List<Object?> get props => [displayState, events];
 }
 
 DisplayState _displayState(Store<AppState> store) {
@@ -29,4 +34,22 @@ DisplayState _displayState(Store<AppState> store) {
     }
   }
   return DisplayState.LOADING;
+}
+
+List<dynamic> _events(Store<AppState> store) {
+  final agendaState = store.state.agendaState;
+  if (agendaState is! AgendaSuccessState) return [];
+
+  final actions = agendaState.agenda.actions.map(UserActionViewModel.create);
+  final rendezvous = agendaState.agenda.rendezvous.map((e) => RendezvousAgendaViewModel());
+  return [
+    ...actions,
+    ...rendezvous,
+  ];
+}
+
+// todo : move
+class RendezvousAgendaViewModel extends Equatable {
+  @override
+  List<Object?> get props => [];
 }
