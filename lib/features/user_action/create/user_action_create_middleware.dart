ce982ba32783c0daecfcd25d3080b8085c45602e
Jordan Chapuy <chapuyj@gmail.com>
2022-08-26 15:37:58 +0200

    feat(923): reload agenda after creating action

--- lib/features/user_action/create/user_action_create_middleware.dart
+++ lib/features/user_action/create/user_action_create_middleware.dart
@@ -1,3 +1,4 @@
+import 'package:pass_emploi_app/features/agenda/agenda_actions.dart';
 import 'package:pass_emploi_app/features/login/login_state.dart';
 import 'package:pass_emploi_app/features/user_action/create/user_action_create_actions.dart';
 import 'package:pass_emploi_app/features/user_action/list/user_action_list_actions.dart';
@@ -19,7 +20,9 @@ class UserActionCreateMiddleware extends MiddlewareClass<AppState> {
       final result = await _repository.createUserAction(loginState.user.id, action.request);
       if (result) {
         store.dispatch(UserActionCreateSuccessAction());
+        // todo optim : on pourrait éviter de faire des appels réseaux ici
         store.dispatch(UserActionListRequestAction());
+        store.dispatch(AgendaRequestAction(DateTime.now()));
       } else {
         store.dispatch(UserActionCreateFailureAction());
       }
