bfab3bed476604567a4fe456a4f4e06844f72035
Jordan Chapuy <chapuyj@gmail.com>
2022-08-26 14:42:27 +0200

    feat(923): updating action on agenda state

--- lib/features/agenda/agenda_reducer.dart
+++ lib/features/agenda/agenda_reducer.dart
@@ -1,9 +1,19 @@
 import 'package:pass_emploi_app/features/agenda/agenda_actions.dart';
 import 'package:pass_emploi_app/features/agenda/agenda_state.dart';
+import 'package:pass_emploi_app/features/user_action/update/user_action_update_actions.dart';
+import 'package:pass_emploi_app/models/user_action.dart';
 
 AgendaState agendaReducer(AgendaState current, dynamic action) {
   if (action is AgendaRequestAction) return AgendaLoadingState();
   if (action is AgendaRequestFailureAction) return AgendaFailureState();
   if (action is AgendaRequestSuccessAction) return AgendaSuccessState(action.agenda);
+  if (action is UserActionUpdateNeededAction) return _listWithUpdatedAction(current, action);
   return current;
+}
+
+AgendaState _listWithUpdatedAction(AgendaState current, UserActionUpdateNeededAction action) {
+  if (current is! AgendaSuccessState) return current;
+  final newActions = current.agenda.actions.withUpdatedAction(action.actionId, action.newStatus);
+  final newAgenda = current.agenda.copyWith(actions: newActions);
+  return AgendaSuccessState(newAgenda);
 }
