4336f38939366d1b9cba3557fe676e0bea569747
lela-oc-to <svetlana.lesik@octo.com>
2022-08-31 18:03:40 +0200

    refacto(937): CR corrections

--- lib/features/demarche/update/update_demarhce_state.dart
+++
@@ -1,9 +0,0 @@
-abstract class UpdateDemarcheState {}
-
-class UpdateDemarcheNotInitializedState extends UpdateDemarcheState {}
-
-class UpdateDemarcheLoadingState extends UpdateDemarcheState {}
-
-class UpdateDemarcheSuccessState extends UpdateDemarcheState {}
-
-class UpdateDemarcheFailureState extends UpdateDemarcheState {}
