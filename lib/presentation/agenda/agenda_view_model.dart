0e166a558b6291e6f4e058e9837398b2594a586d
Jordan Chapuy <chapuyj@gmail.com>
2022-09-02 14:40:37 +0200

    refactor: agenda events sections

--- lib/presentation/agenda/agenda_view_model.dart
+++ lib/presentation/agenda/agenda_view_model.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 import 'package:pass_emploi_app/features/agenda/agenda_actions.dart';
 import 'package:pass_emploi_app/features/agenda/agenda_state.dart';
 import 'package:pass_emploi_app/features/user_action/create/user_action_create_actions.dart';
+import 'package:pass_emploi_app/models/agenda.dart';
 import 'package:pass_emploi_app/presentation/display_state.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
 import 'package:pass_emploi_app/utils/date_extensions.dart';
@@ -15,7 +16,8 @@ class AgendaPageViewModel extends Equatable {
   final Function() resetCreateAction;
   final Function(DateTime) retry;
 
-  AgendaPageViewModel({required this.displayState, required this.events, required this.resetCreateAction, required this.retry});
+  AgendaPageViewModel(
+      {required this.displayState, required this.events, required this.resetCreateAction, required this.retry});
 
   factory AgendaPageViewModel.create(Store<AppState> store) {
     return AgendaPageViewModel(
@@ -44,36 +46,50 @@ DisplayState _displayState(Store<AppState> store) {
   return DisplayState.LOADING;
 }
 
-// todo peut-être un refactoring avec plusieurs fonctions à extraire
 List<AgendaItem> _events(Store<AppState> store) {
   final agendaState = store.state.agendaState;
   if (agendaState is! AgendaSuccessState) return [];
 
+  final events = _allEventsSorted(agendaState.agenda);
+  final nextWeekFirstDay = agendaState.agenda.dateDeDebut.addWeeks(1);
+
+  return [
+    if (agendaState.agenda.delayedActions > 0) DelayedActionsBanner(agendaState.agenda.delayedActions),
+    _makeCurrentWeek(events, nextWeekFirstDay),
+    _makeNextWeek(events, nextWeekFirstDay),
+  ];
+}
+
+List<EventAgenda> _allEventsSorted(Agenda agenda) {
   final events = [
-    ...agendaState.agenda.actions.map((e) => UserActionEventAgenda(e.id, e.dateEcheance)),
-    ...agendaState.agenda.rendezvous.map((e) => RendezvousEventAgenda(e.id, e.date)),
+    ...agenda.actions.map((e) => UserActionEventAgenda(e.id, e.dateEcheance)),
+    ...agenda.rendezvous.map((e) => RendezvousEventAgenda(e.id, e.date)),
   ];
 
   events.sort((a, b) => a.date.compareTo(b.date));
 
-  final nextWeekFirstDay = agendaState.agenda.dateDeDebut.addWeeks(1);
+  return events;
+}
+
+CurrentWeekAgendaItem _makeCurrentWeek(List<EventAgenda> events, DateTime nextWeekFirstDay) {
   final currentWeekEvents = events.where((element) => element.date.isBefore(nextWeekFirstDay));
-  final nextWeekEvents = events.where((element) => !element.date.isBefore(nextWeekFirstDay)).toList();
 
-  final grouped = currentWeekEvents.groupListsBy((element) => element.date.toDayOfWeekWithFullMonth().firstLetterUpperCased());
-  final keys = grouped.keys.toList();
+  final eventGroupedByDays = currentWeekEvents.groupListsBy((element) {
+    return element.date.toDayOfWeekWithFullMonth().firstLetterUpperCased();
+  });
 
-  final daySections = keys.map((date) {
+  final daySections = eventGroupedByDays.keys.map((date) {
     final title = date;
-    final events = grouped[date]!.toList();
+    final events = eventGroupedByDays[date]!.toList();
     return DaySectionAgenda(title, events);
   }).toList();
 
-  return [
-    if (agendaState.agenda.delayedActions > 0) DelayedActionsBanner(agendaState.agenda.delayedActions),
-    CurrentWeekAgendaItem(daySections),
-    NextWeekAgendaItem(nextWeekEvents),
-  ];
+  return CurrentWeekAgendaItem(daySections);
+}
+
+NextWeekAgendaItem _makeNextWeek(List<EventAgenda> events, DateTime nextWeekFirstDay) {
+  final nextWeekEvents = events.where((element) => !element.date.isBefore(nextWeekFirstDay)).toList();
+  return NextWeekAgendaItem(nextWeekEvents);
 }
 
 abstract class AgendaItem extends Equatable {}
