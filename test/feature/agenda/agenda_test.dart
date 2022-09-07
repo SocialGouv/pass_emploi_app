a872cc75afd40f44ef31dc626687520f687801e0
gabriel.adgeg <gabriel.adgeg@octo.com>
2022-09-07 13:04:36 +0200

    chore: handle login mode in agenda use case

--- test/feature/agenda/agenda_test.dart
+++ test/feature/agenda/agenda_test.dart
@@ -13,22 +13,44 @@ import '../../utils/expects.dart';
 
 void main() {
   group('Agenda', () {
-    group('when requesting agenda', () {
+    group('when requesting agenda for Mission Local user', () {
       final sut = SUT();
 
       sut.whenDispatching = () => AgendaRequestAction(DateTime(2022, 7, 7));
 
       test('should load then succeed when request succeed', () async {
         sut.givenStore = givenState()
-            .loggedInUser() //
+            .loggedInMiloUser() //
             .store((f) => {f.agendaRepository = AgendaRepositorySuccessStub()});
 
-        sut.thenExpectChangingStatesInOrder([_shouldLoad, _shouldSucceed]);
+        sut.thenExpectChangingStatesInOrder([_shouldLoad, _shouldSucceedForMissionLocaleUser]);
       });
 
       test('should load then fail when request fail', () async {
         sut.givenStore = givenState()
-            .loggedInUser() //
+            .loggedInMiloUser() //
+            .store((f) => {f.agendaRepository = AgendaRepositoryErrorStub()});
+
+        sut.thenExpectChangingStatesInOrder([_shouldLoad, _shouldFail]);
+      });
+    });
+
+    group('when requesting agenda for Pole Emploi user', () {
+      final sut = SUT();
+
+      sut.whenDispatching = () => AgendaRequestAction(DateTime(2022, 7, 7));
+
+      test('should load then succeed when request succeed', () async {
+        sut.givenStore = givenState()
+            .loggedInPoleEmploiUser() //
+            .store((f) => {f.agendaRepository = AgendaRepositorySuccessStub()});
+
+        sut.thenExpectChangingStatesInOrder([_shouldLoad, _shouldSucceedForPoleEmploiUser]);
+      });
+
+      test('should load then fail when request fail', () async {
+        sut.givenStore = givenState()
+            .loggedInPoleEmploiUser() //
             .store((f) => {f.agendaRepository = AgendaRepositoryErrorStub()});
 
         sut.thenExpectChangingStatesInOrder([_shouldLoad, _shouldFail]);
@@ -41,9 +63,16 @@ void _shouldLoad(AppState state) => expect(state.agendaState, AgendaLoadingState
 
 void _shouldFail(AppState state) => expect(state.agendaState, AgendaFailureState());
 
-void _shouldSucceed(AppState state) {
+void _shouldSucceedForMissionLocaleUser(AppState state) {
   expectTypeThen(state.agendaState, (AgendaSuccessState agendaState) {
     expect(agendaState.agenda.actions.length, 1);
+    expect(agendaState.agenda.rendezvous.length, 1);
+  });
+}
+
+void _shouldSucceedForPoleEmploiUser(AppState state) {
+  expectTypeThen(state.agendaState, (AgendaSuccessState agendaState) {
+    expect(agendaState.agenda.demarches.length, 1);
     expect(agendaState.agenda.rendezvous.length, 1);
   });
 }
@@ -56,6 +85,17 @@ class AgendaRepositorySuccessStub extends AgendaRepository {
     return Agenda(
       actions: [userActionStub()],
       demarches: [],
+      rendezvous: [rendezvousStub()],
+      delayedActions: 0,
+      dateDeDebut: DateTime(2042),
+    );
+  }
+
+  @override
+  Future<Agenda?> getAgendaPoleEmploi(String userId, DateTime maintenant) async {
+    return Agenda(
+      actions: [],
+      demarches: [demarcheStub()],
       rendezvous: [rendezvousStub()],
       delayedActions: 0,
       dateDeDebut: DateTime(2042),
