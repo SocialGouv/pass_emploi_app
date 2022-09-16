992f84f2166362ac6f209b131228cfb1df374029
Jordan Chapuy <chapuyj@gmail.com>
2022-09-16 15:24:34 +0200

    refactor: test dsl on details jeune repository

--- test/repositories/details_jeune_repository_test.dart
+++ test/repositories/details_jeune_repository_test.dart
@@ -1,52 +1,55 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
 import 'package:pass_emploi_app/models/details_jeune.dart';
 import 'package:pass_emploi_app/repositories/details_jeune/details_jeune_repository.dart';
 
-import '../doubles/fixtures.dart';
+import '../dsl/sut_repository.dart';
 import '../utils/mock_demo_client.dart';
-import '../utils/pass_emploi_mock_client.dart';
-import '../utils/test_assets.dart';
 import '../utils/test_datetime.dart';
 
 void main() {
-  test('should return details jeune on valid request', () async {
-    // Given
-    final httpClient = PassEmploiMockClient((request) async {
-      if (request.method != "GET") return invalidHttpResponse();
-      if (!request.url.toString().startsWith("BASE_URL/jeunes/id-jeune")) return invalidHttpResponse();
-      return Response(loadTestAssets("details_jeune.json"), 200);
+  final sut = RepositorySut<DetailsJeuneRepository>();
+  sut.givenRepository((client) => DetailsJeuneRepository("BASE_URL", client));
+
+  group("fetch", () {
+    sut.when((repository) => repository.fetch("id-jeune"));
+
+    group('when response is valid', () {
+      sut.givenResponse(fromJson: "details_jeune.json");
+
+      test('request should be valid', () async {
+        await sut.expectRequestBody(
+          method: "GET",
+          url: "BASE_URL/jeunes/id-jeune",
+        );
+      });
+
+      test('response should be valid', () async {
+        await sut.expectResult<DetailsJeune?>((result) {
+          expect(result, isNotNull);
+          expect(
+              result,
+              DetailsJeune(
+                conseiller: DetailsJeuneConseiller(
+                  firstname: "Nils",
+                  lastname: "Tavernier",
+                  sinceDate: parseDateTimeUtcWithCurrentTimeZone('2022-02-15T0:0:0.0Z'),
+                ),
+              ));
+        });
+      });
     });
-    final repository = DetailsJeuneRepository("BASE_URL", httpClient);
 
-    // When
-    final response = await repository.fetch("id-jeune");
+    group('when response is invalid', () {
+      sut.givenInvalidResponse();
 
-    // Then
-    expect(
-      response,
-      DetailsJeune(
-        conseiller: DetailsJeuneConseiller(
-          firstname: "Nils",
-          lastname: "Tavernier",
-          sinceDate: parseDateTimeUtcWithCurrentTimeZone('2022-02-15T0:0:0.0Z'),
-        ),
-      ),
-    );
+      test('response should be null', () async {
+        await sut.expectNullResult();
+      });
+    });
   });
 
-  test('should return null on failed request', () async {
-    // Given
-    final httpClient = PassEmploiMockClient((request) async => throw Exception());
-    final repository = DetailsJeuneRepository("BASE_URL", httpClient);
-
-    // When
-    final response = await repository.fetch("id-jeune");
-
-    // Then
-    expect(response, isNull);
-  });
-
+  // TODO : utile ? Il n'y a pas un test général ?
+  // Si oui utile, alors ajouter un helper dans le RepositorySut
   test('should return details jeune on valid request', () async {
     // Given
     final httpClient = MockModeDemoClient();
