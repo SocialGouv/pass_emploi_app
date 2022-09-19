e3e5338ebffcad85df5305f1bd5221906980a834
Jordan Chapuy <chapuyj@gmail.com>
2022-09-19 11:35:54 +0200

    refactor: test dsl on create demarche repository

--- test/repositories/demarche/create_demarche_repository_test.dart
+++ test/repositories/demarche/create_demarche_repository_test.dart
@@ -1,97 +1,87 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
-import 'package:pass_emploi_app/network/json_utf8_decoder.dart';
 import 'package:pass_emploi_app/repositories/demarche/create_demarche_repository.dart';
 
-import '../../doubles/fixtures.dart';
-import '../../utils/pass_emploi_mock_client.dart';
+import '../../dsl/sut_repository.dart';
 
 void main() {
-  group('createDemarche', () {
-    test('should return true when success', () async {
-      // Given
-      final httpClient = PassEmploiMockClient((request) async {
-        if (request.method != 'POST') return invalidHttpResponse();
-        if (!request.url.toString().startsWith('BASE_URL/jeunes/id/demarches')) return invalidHttpResponse();
-        final requestJson = jsonUtf8Decode(request.bodyBytes);
-        if (requestJson['codeQuoi'] != 'codeQuoi') return invalidHttpResponse();
-        if (requestJson['codePourquoi'] != 'codePourquoi') return invalidHttpResponse();
-        if (requestJson['codeComment'] != 'codeComment') return invalidHttpResponse();
-        if (requestJson['dateFin'] != '2021-12-23T12:08:10.000') return invalidHttpResponse();
-        return Response('', 200);
-      });
-      final repository = CreateDemarcheRepository('BASE_URL', httpClient);
+  final sut = RepositorySut<CreateDemarcheRepository>();
+  sut.givenRepository((client) => CreateDemarcheRepository("BASE_URL", client));
 
-      // When
-      final result = await repository.createDemarche(
+  group("createDemarche", () {
+    sut.when(
+      (repository) => repository.createDemarche(
         userId: 'id',
         codeQuoi: 'codeQuoi',
         codePourquoi: 'codePourquoi',
         codeComment: 'codeComment',
         dateEcheance: DateTime(2021, 12, 23, 12, 8, 10),
-      );
+      ),
+    );
 
-      // Then
-      expect(result, true);
+    group('when response is valid', () {
+      sut.givenResponseCode(200);
+
+      test('request should be valid', () async {
+        await sut.expectRequestBody(
+          method: "POST",
+          url: "BASE_URL/jeunes/id/demarches",
+          params: {
+            "codeQuoi": "codeQuoi",
+            "codePourquoi": "codePourquoi",
+            "codeComment": "codeComment",
+            "dateFin": "2021-12-23T12:08:10.000",
+          },
+        );
+      });
+
+      test('response should be valid', () async {
+        await sut.expectTrueAsResult();
+      });
     });
 
-    test('should return false when failure', () async {
-      // Given
-      final httpClient = PassEmploiMockClient((request) async => Response('', 400));
-      final repository = CreateDemarcheRepository('BASE_URL', httpClient);
+    group('when response is invalid', () {
+      sut.givenResponseCode(400);
 
-      // When
-      final result = await repository.createDemarche(
-        userId: 'id',
-        codeQuoi: 'codeQuoi',
-        codePourquoi: 'codePourquoi',
-        codeComment: 'codeComment',
-        dateEcheance: DateTime(2021, 12, 23, 12, 8, 10),
-      );
-
-      // Then
-      expect(result, false);
+      test('response should be null', () async {
+        await sut.expectFalseAsResult();
+      });
     });
   });
 
-  group('createDemarchePersonnalisee', () {
-    test('should return true when success', () async {
-      // Given
-      final httpClient = PassEmploiMockClient((request) async {
-        if (request.method != 'POST') return invalidHttpResponse();
-        if (!request.url.toString().startsWith('BASE_URL/jeunes/id/demarches')) return invalidHttpResponse();
-        final requestJson = jsonUtf8Decode(request.bodyBytes);
-        if (requestJson['description'] != 'Description accentuée') return invalidHttpResponse();
-        if (requestJson['dateFin'] != '2021-12-23T12:08:10.000') return invalidHttpResponse();
-        return Response('', 200);
+  group("createDemarchePersonnalisee", () {
+    sut.when(
+      (repository) => repository.createDemarchePersonnalisee(
+        userId: 'id',
+        commentaire: 'Description accentuée',
+        dateEcheance: DateTime(2021, 12, 23, 12, 8, 10),
+      ),
+    );
+
+    group('when response is valid', () {
+      sut.givenResponseCode(200);
+
+      test('request should be valid', () async {
+        await sut.expectRequestBody(
+          method: "POST",
+          url: "BASE_URL/jeunes/id/demarches",
+          params: {
+            "description": "Description accentuée",
+            "dateFin": "2021-12-23T12:08:10.000",
+          },
+        );
       });
-      final repository = CreateDemarcheRepository('BASE_URL', httpClient);
 
-      // When
-      final result = await repository.createDemarchePersonnalisee(
-        userId : 'id',
-        commentaire : 'Description accentuée',
-        dateEcheance : DateTime(2021, 12, 23, 12, 8, 10),
-      );
-
-      // Then
-      expect(result, true);
+      test('response should be valid', () async {
+        await sut.expectTrueAsResult();
+      });
     });
 
-    test('should return false when failure', () async {
-      // Given
-      final httpClient = PassEmploiMockClient((request) async => Response('', 400));
-      final repository = CreateDemarcheRepository('BASE_URL', httpClient);
+    group('when response is invalid', () {
+      sut.givenResponseCode(400);
 
-      // When
-      final result = await repository.createDemarchePersonnalisee(
-        userId : 'id',
-        commentaire : 'description',
-        dateEcheance : DateTime(2021, 12, 23, 12, 8, 10),
-      );
-
-      // Then
-      expect(result, false);
+      test('response should be null', () async {
+        await sut.expectFalseAsResult();
+      });
     });
   });
 }
