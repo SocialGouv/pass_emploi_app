d3b7986f98f354d966878f4eb3958b3ca24918d0
Jordan Chapuy <chapuyj@gmail.com>
2022-09-22 15:39:25 +0200

    feat(1042): view model offre emploi

--- lib/presentation/offre_emploi_search_view_model.dart
+++ lib/presentation/offre_emploi_search_view_model.dart
@@ -3,6 +3,7 @@ import 'package:pass_emploi_app/features/location/search_location_actions.dart';
 import 'package:pass_emploi_app/features/offre_emploi/list/offre_emploi_list_state.dart';
 import 'package:pass_emploi_app/features/offre_emploi/search/offre_emploi_search_actions.dart';
 import 'package:pass_emploi_app/features/offre_emploi/search/offre_emploi_search_state.dart';
+import 'package:pass_emploi_app/features/suggestions_recherche/suggestions_recherche_state.dart';
 import 'package:pass_emploi_app/models/location.dart';
 import 'package:pass_emploi_app/presentation/display_state.dart';
 import 'package:pass_emploi_app/presentation/location_view_model.dart';
@@ -12,6 +13,7 @@ import 'package:redux/redux.dart';
 
 class OffreEmploiSearchViewModel extends Equatable {
   final DisplayState displayState;
+  final bool hasSuggestionsRecherche;
   final List<LocationViewModel> locations;
   final String errorMessage;
   final Function(String? input) onInputLocation;
@@ -19,6 +21,7 @@ class OffreEmploiSearchViewModel extends Equatable {
 
   OffreEmploiSearchViewModel._({
     required this.displayState,
+    required this.hasSuggestionsRecherche,
     required this.locations,
     required this.errorMessage,
     required this.onInputLocation,
@@ -30,6 +33,7 @@ class OffreEmploiSearchViewModel extends Equatable {
     final searchResultsState = store.state.offreEmploiListState;
     return OffreEmploiSearchViewModel._(
       displayState: _displayState(searchState, searchResultsState),
+      hasSuggestionsRecherche: _hasSuggestionsRecherche(store.state.suggestionsRechercheState),
       locations: store.state.searchLocationState.locations
           .map((location) => LocationViewModel.fromLocation(location))
           .toList(),
@@ -45,6 +49,11 @@ class OffreEmploiSearchViewModel extends Equatable {
 
   @override
   List<Object?> get props => [displayState, errorMessage, locations];
+}
+
+bool _hasSuggestionsRecherche(SuggestionsRechercheState suggestionsRechercheState) {
+  return (suggestionsRechercheState is SuggestionsRechercheSuccessState &&
+      suggestionsRechercheState.suggestions.isNotEmpty);
 }
 
 String _setErrorMessage(OffreEmploiSearchState searchState, OffreEmploiListState searchResultsState) {
--- test/dsl/app_state_dsl.dart
+++ test/dsl/app_state_dsl.dart
@@ -14,6 +14,7 @@ import 'package:pass_emploi_app/features/partage_activite/partage_activites_stat
 import 'package:pass_emploi_app/features/partage_activite/update/partage_activite_update_state.dart';
 import 'package:pass_emploi_app/features/rating/rating_state.dart';
 import 'package:pass_emploi_app/features/rendezvous/rendezvous_state.dart';
+import 'package:pass_emploi_app/features/suggestions_recherche/suggestions_recherche_state.dart';
 import 'package:pass_emploi_app/features/tutorial/tutorial_state.dart';
 import 'package:pass_emploi_app/features/user_action/commentaire/create/action_commentaire_create_state.dart';
 import 'package:pass_emploi_app/features/user_action/commentaire/list/action_commentaire_list_state.dart';
@@ -264,5 +265,21 @@ extension AppStateDSL on AppState {
 
   AppState deleteActionFailure() {
     return copyWith(userActionDeleteState: UserActionDeleteFailureState());
+  }
+
+  AppState withSuggestionsRecherche() {
+    return copyWith(suggestionsRechercheState: SuggestionsRechercheSuccessState(mockSuggestionsRecherche()));
+  }
+
+  AppState emptySuggestionsRecherche() {
+    return copyWith(suggestionsRechercheState: SuggestionsRechercheSuccessState([]));
+  }
+
+  AppState loadingSuggestionsRecherche() {
+    return copyWith(suggestionsRechercheState: SuggestionsRechercheLoadingState());
+  }
+
+  AppState failedSuggestionsRecherche() {
+    return copyWith(suggestionsRechercheState: SuggestionsRechercheFailureState());
   }
 }
