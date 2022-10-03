a4ca442f325c8a6e742e66b5f086e1b0b9472c4c
Jordan Chapuy <chapuyj@gmail.com>
2022-10-03 15:38:50 +0200

    feat(1044): accepter suggestion - test for cache reset

--- test/repositories/suggestions_recherche_repository_test.dart
+++ test/repositories/suggestions_recherche_repository_test.dart
@@ -1,14 +1,18 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pass_emploi_app/models/suggestion_recherche.dart';
+import 'package:pass_emploi_app/network/cache_manager.dart';
 import 'package:pass_emploi_app/repositories/suggestions_recherche_repository.dart';
 
-import '../doubles/dummies.dart';
 import '../doubles/fixtures.dart';
+import '../doubles/spies.dart';
 import '../dsl/sut_repository.dart';
 
 void main() {
+  var cacheManager = SpyPassEmploiCacheManager();
+  setUp(() => cacheManager = SpyPassEmploiCacheManager());
+
   final sut = RepositorySut<SuggestionsRechercheRepository>();
-  sut.givenRepository((client) => SuggestionsRechercheRepository("BASE_URL", client, DummyPassEmploiCacheManager()));
+  sut.givenRepository((client) => SuggestionsRechercheRepository("BASE_URL", client, cacheManager));
 
   group('getSuggestions', () {
     sut.when((repository) => repository.getSuggestions("UID"));
@@ -55,6 +59,13 @@ void main() {
 
       test('response should be valid', () async {
         await sut.expectTrueAsResult();
+      });
+
+      test('cache for saved search and suggestions should be reset', () async {
+        await sut.expectResult<dynamic>((result) {
+          expect(cacheManager.removeRessourceParams, CachedRessource.SAVED_SEARCH);
+          expect(cacheManager.removeSuggestionsRechercheRessourceWasCalled, true);
+        });
       });
     });
 
