69274d4bde2ba38051c6639843a5b202c31a25bd
Jordan Chapuy <chapuyj@gmail.com>
2022-11-17 15:14:21 +0100

    feat(1188): event list - add repository

--- lib/repositories/event_list_repository.dart
+++ lib/repositories/event_list_repository.dart
@@ -1,5 +1,9 @@
 import 'package:http/http.dart';
 import 'package:pass_emploi_app/crashlytics/crashlytics.dart';
+import 'package:pass_emploi_app/models/rendezvous.dart';
+import 'package:pass_emploi_app/network/json_utf8_decoder.dart';
+import 'package:pass_emploi_app/network/status_code.dart';
+import 'package:pass_emploi_app/repositories/rendezvous/json_rendezvous.dart';
 
 class EventListRepository {
   final String _baseUrl;
@@ -8,4 +12,17 @@ class EventListRepository {
   final Crashlytics? _crashlytics;
 
   EventListRepository(this._baseUrl, this._httpClient, [this._crashlytics]);
+
+  Future<List<Rendezvous>?> get(String userId) async {
+    final url = Uri.parse(_baseUrl + "/jeunes/$userId/animations-collectives");
+    try {
+      final response = await _httpClient.get(url);
+      if (response.statusCode.isValid()) {
+        return response.bodyBytes.asListOf((json) => JsonRendezvous.fromJson(json).toRendezvous());
+      }
+    } catch (e, stack) {
+      _crashlytics?.recordNonNetworkException(e, stack, url);
+    }
+    return null;
+  }
 }
---
+++ test/repositories/event_list_repository_test.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pass_emploi_app/models/conseiller.dart';
+import 'package:pass_emploi_app/models/rendezvous.dart';
+import 'package:pass_emploi_app/repositories/event_list_repository.dart';
+
+import '../dsl/sut_repository.dart';
+import '../utils/test_datetime.dart';
+
+void main() {
+  group('EventListRepository', () {
+    final sut = RepositorySut<EventListRepository>();
+    sut.givenRepository((client) => EventListRepository("BASE_URL", client));
+
+    group('getAgendaPoleEmploi', () {
+      sut.when(
+        (repository) => repository.get("UID"),
+      );
+
+      group('when response is valid', () {
+        sut.givenJsonResponse(fromJson: "animations-collectives.json");
+
+        test('request should be valid', () async {
+          await sut.expectRequestBody(
+            method: "GET",
+            url: "BASE_URL/jeunes/UID/animations-collectives",
+          );
+        });
+
+        test('response should be valid', () async {
+          await sut.expectResult<List<Rendezvous>?>((result) {
+            expect(result, isNotNull);
+            expect(result?.length, 3);
+
+            expect(
+              result?[0],
+              Rendezvous(
+                id: '2d663392-b9ff-4b20-81ca-70a3c779e299',
+                date: parseDateTimeUtcWithCurrentTimeZone('2021-11-28T13:34:00.000Z'),
+                modality: 'en présentiel : Misson locale / Permanence',
+                isInVisio: false,
+                duration: 23,
+                withConseiller: true,
+                isAnnule: false,
+                type: RendezvousType(
+                    RendezvousTypeCode.ENTRETIEN_INDIVIDUEL_CONSEILLER, 'Entretien individuel conseiller'),
+                title: "super entretien",
+                comment: 'Amener votre CV',
+                conseiller: Conseiller(id: '1', firstName: 'Nils', lastName: 'Tavernier'),
+                createur: Conseiller(id: '2', firstName: 'Joe', lastName: 'Pesci'),
+                inscrit: true,
+              ),
+            );
+
+            expect(result?[0].inscrit, true);
+            expect(result?[1].inscrit, false);
+            expect(result?[2].inscrit, null);
+          });
+        });
+      });
+
+      group('when response is invalid', () {
+        sut.givenResponseCode(500);
+
+        test('response should be null', () async {
+          await sut.expectNullResult();
+        });
+      });
+    });
+  });
+}
