10956c69a783dec7693c8067b35ed7d99212d2dc
adrien.cens <adrien.cens@edu.esiee.fr>
2022-12-08 17:33:09 +0100

    add generic_card and replace redundant widgets in user_action_card.dart

---
+++ lib/widgets/cards/generic/generic_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:pass_emploi_app/ui/app_colors.dart';
+import 'package:pass_emploi_app/ui/margins.dart';
+import 'package:pass_emploi_app/ui/shadows.dart';
+
+class GenericCard extends StatelessWidget {
+  const GenericCard({super.key, required this.child, this.onTap});
+  final Widget child;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    const Color cardBackgroundColor = Colors.white;
+    final BorderRadius cardBorderRadius = BorderRadius.circular(16);
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: cardBackgroundColor,
+        borderRadius: cardBorderRadius,
+        boxShadow: [
+          Shadows.boxShadow,
+        ],
+      ),
+      child: Material(
+          color: cardBackgroundColor,
+          borderRadius: cardBorderRadius,
+          child: InkWell(
+            borderRadius: cardBorderRadius,
+            onTap: onTap,
+            splashColor: AppColors.primaryLighten,
+            child: Padding(
+              padding: EdgeInsets.all(Margins.spacing_base),
+              child: child,
+            ),
+          )),
+    );
+  }
+}
