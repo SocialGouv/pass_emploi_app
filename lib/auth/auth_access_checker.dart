34e2e1499b92df466023f58573d0957f398fa199
Jordan Chapuy <chapuyj@gmail.com>
2023-01-13 17:06:10 +0100

    build: intercept expired token on dio error

--- lib/auth/auth_access_checker.dart
+++ lib/auth/auth_access_checker.dart
@@ -1,5 +1,6 @@
 import 'package:pass_emploi_app/features/login/login_actions.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
+import 'package:pass_emploi_app/utils/log.dart';
 import 'package:redux/redux.dart';
 
 class AuthAccessChecker {
@@ -7,6 +8,7 @@ class AuthAccessChecker {
 
   void logoutUserIfTokenIsExpired(String? message, int statusCode) {
     if (message?.contains('token_pole_emploi_expired') == true && statusCode == 401) {
+      Log.i("Logout user on token_pole_emploi_expired.");
       _store.dispatch(RequestLogoutAction());
     }
   }
--- lib/network/interceptors/expired_token_interceptor.dart
+++ lib/network/interceptors/expired_token_interceptor.dart
@@ -6,14 +6,6 @@ class ExpiredTokenInterceptor extends Interceptor {
 
   ExpiredTokenInterceptor(this._authAccessChecker);
 
-  //TODO: vérifier que c'est OK. Et je pense qu'il faut qu'il soit juste dans onError.
-
-  @override
-  void onResponse(Response<dynamic> response, ResponseInterceptorHandler handler) {
-    _authAccessChecker.logoutUserIfTokenIsExpired(response.toString(), response.statusCode ?? 777);
-    handler.next(response);
-  }
-
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
     _authAccessChecker.logoutUserIfTokenIsExpired(err.response?.toString(), err.response?.statusCode ?? 700);
