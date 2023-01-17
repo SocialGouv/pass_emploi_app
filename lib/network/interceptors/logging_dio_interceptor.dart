0b6b3e0d5ce6f3fb8cdbc9a8ffd63b1807d4df2c
Jordan Chapuy <chapuyj@gmail.com>
2023-01-17 12:03:25 +0100

    build: better network logs

--- lib/network/interceptors/logging_dio_interceptor.dart
+++ lib/network/interceptors/logging_dio_interceptor.dart
@@ -3,8 +3,31 @@ import 'package:pass_emploi_app/utils/log.dart';
 
 class LoggingNetworkDioInterceptor extends Interceptor {
   @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    Log.i("""Dio Request 
+    - ${options.method} ${options.uri.toString()}
+    - headers: ${options.headers}
+    """);
+    handler.next(options);
+  }
+
+  @override
   void onResponse(Response<dynamic> response, ResponseInterceptorHandler handler) {
-    Log.i("Response from ${response.realUri.toString()} (code ${response.statusCode}): ${response.toString()}");
+    Log.i("""Dio Response 
+    - ${response.requestOptions.method} ${response.realUri.toString()}:
+    - code: ${response.statusCode}
+    - response: ${response.toString()}
+    """);
     handler.next(response);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    Log.i("""Dio Error 
+    - ${err.requestOptions.method} ${err.requestOptions.uri.toString()}:
+    - code: ${err.response?.statusCode}
+    - response: ${err.response.toString()}
+    """);
+    handler.next(err);
   }
 }
