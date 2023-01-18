55255168b1e98ee283a1c1333fe0af9aad32df6d
gabriel.adgeg <gabriel.adgeg@octo.com>
2023-01-18 11:25:37 +0100

    refactor(1311): Refactor SnackBar

--- lib/widgets/snack_bar/show_snack_bar.dart
+++ lib/widgets/snack_bar/show_snack_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:pass_emploi_app/ui/app_colors.dart';
-import 'package:pass_emploi_app/ui/dimens.dart';
 import 'package:pass_emploi_app/ui/drawables.dart';
 import 'package:pass_emploi_app/ui/margins.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
@@ -21,79 +20,55 @@ void _showSnackBar(BuildContext context, String label, VoidCallback? onSeeDetail
   _clearAllSnackBars();
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
-      padding: const EdgeInsets.symmetric(vertical: Margins.spacing_s),
+      padding: EdgeInsets.symmetric(
+        horizontal: Margins.spacing_base,
+        vertical: onSeeDetailTap != null ? Margins.spacing_s : Margins.spacing_base,
+      ),
       duration: Duration(days: 365),
       backgroundColor: success ? AppColors.secondaryLighten : AppColors.warningLight,
-      // Column is required to make stack height fit to children
       content: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.min,
         children: [
-          // required to keep close button height without impacting padding of See Detail button
-          Stack(
+          Row(
+            mainAxisSize: MainAxisSize.max,
             children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(
-                      Margins.spacing_base,
-                      12, // required to center text and close icon
-                      Margins.spacing_base,
-                      0,
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        SvgPicture.asset(
-                          success ? Drawables.icDoneCircle : Drawables.icImportant,
-                          color: success ? AppColors.secondary : AppColors.warning,
-                        ),
-                        SizedBox(width: Margins.spacing_s),
-                        Expanded(
-                          child: Text(
-                            label,
-                            style: TextStyle(color: success ? AppColors.secondary : AppColors.warning),
-                          ),
-                        ),
-                        SizedBox(width: kMinInteractiveDimension), // Take the width of the close icon button,
-                      ],
-                    ),
-                  ),
-                  if (onSeeDetailTap != null)
-                    Row(
-                      children: [
-                        SizedBox(width: Margins.spacing_s),
-                        InkWell(
-                          borderRadius: BorderRadius.circular(Dimens.radius_l),
-                          onTap: () {
-                            _clearAllSnackBars();
-                            onSeeDetailTap();
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(Margins.spacing_s),
-                            child: Text(
-                              Strings.seeDetail,
-                              style: TextStyles.textSRegular(color: AppColors.secondary),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                ],
+              SvgPicture.asset(
+                success ? Drawables.icDoneCircle : Drawables.icImportant,
+                color: success ? AppColors.secondary : AppColors.warning,
               ),
-              Align(
-                alignment: Alignment.centerRight,
+              SizedBox(width: Margins.spacing_s),
+              Expanded(child: Text(label, style: TextStyle(color: success ? AppColors.secondary : AppColors.warning))),
+              SizedBox(width: Margins.spacing_s),
+              SizedBox(
+                height: 30,
                 child: IconButton(
                   onPressed: () => _clearAllSnackBars(),
-                  icon: SvgPicture.asset(
-                    Drawables.icClose,
-                    color: success ? AppColors.secondary : AppColors.warning,
+                  icon: OverflowBox(
+                    maxHeight: 50,
+                    maxWidth: 50,
+                    child: Icon(
+                      Icons.close_rounded,
+                      size: 24,
+                      color: success ? AppColors.secondary : AppColors.warning,
+                    ),
                   ),
                 ),
               )
             ],
           ),
+          if (onSeeDetailTap != null)
+            SizedBox(
+              height: 30,
+              child: TextButton(
+                style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                onPressed: () {
+                  _clearAllSnackBars();
+                  onSeeDetailTap();
+                },
+                child: Text(Strings.seeDetail, style: TextStyles.textSRegular(color: AppColors.secondary)),
+              ),
+            ),
         ],
       ),
     ),
