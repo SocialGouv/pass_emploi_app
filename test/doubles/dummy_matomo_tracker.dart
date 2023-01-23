1cb81e8b299e1b6710646676d9c1ee7117589532
gabriel.adgeg <gabriel.adgeg@octo.com>
2023-01-23 11:49:58 +0100

    fix: fix compilations issues

--- test/doubles/dummy_matomo_tracker.dart
+++ test/doubles/dummy_matomo_tracker.dart
@@ -1,104 +1,30 @@
 import 'dart:async';
-import 'dart:collection';
-import 'dart:ui';
 
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:matomo_tracker/matomo_tracker.dart';
-import 'package:matomo_tracker/src/logger.dart';
-import 'package:matomo_tracker/src/matomo_event.dart';
-import 'package:matomo_tracker/src/session.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:pass_emploi_app/utils/pass_emploi_matomo_tracker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DummyMatomoTracker implements PassEmploiMatomoTracker {
   @override
-  late String contentBase;
+  Function(String p1)? onTrackScreen;
 
   @override
-  String? currentScreenId;
-
-  @override
-  late bool initialized;
-
-  @override
-  late Size screenResolution;
-
-  @override
-  late Session session;
-
-  @override
-  late int siteId;
-
-  @override
-  late Timer timer;
-
-  @override
-  late String url;
-
-  @override
-  String? userAgent;
-
-  @override
-  String? get getAuthToken => null;
-
-  @override
-  bool getOptOut() => false;
-
-  @override
-  Logger get log => throw UnimplementedError();
-
-  @override
-  bool? get optOut => false;
-
-  @override
-  Queue<MatomoEvent> get queue => throw UnimplementedError();
-
-  @override
-  Visitor get visitor => throw UnimplementedError();
-
-  @override
-  Function(String screen)? onTrackScreen;
-
-  @override
-  void clear() {
-    // Do nothing
-  }
-
-  @override
-  void dispatchEvents() {
-    // Do nothing
-  }
-
-  @override
-  void dispose() {
-    // Do nothing
-  }
-
-  @override
-  void pause() {
-    // Do nothing
-  }
-
-  @override
-  void resume() {
+  Future<void> initialize({
+    required int siteId,
+    required String url,
+    String? visitorId,
+    String? contentBaseUrl,
+    int dequeueInterval = 10,
+    String? tokenAuth,
+    SharedPreferences? prefs,
+    PackageInfo? packageInfo,
+  }) async {
     // Do nothing
   }
 
   @override
   Future<void> setOptOut({required bool optout}) async {
-    // Do nothing
-  }
-
-  @override
-  void trackCartUpdate(
-    List<TrackingOrderItem>? trackingOrderItems,
-    num? subTotal,
-    num? taxAmount,
-    num? shippingCost,
-    num? discountAmount, {
-    Map<String, String>? dimensions,
-  }) {
     // Do nothing
   }
 
@@ -113,25 +39,6 @@ class DummyMatomoTracker implements PassEmploiMatomoTracker {
     required String action,
     String? eventName,
     int? eventValue,
-    Map<String, String>? dimensions,
-  }) {
-    // Do nothing
-  }
-
-  @override
-  void trackGoal(int goalId, {double? revenue, Map<String, String>? dimensions}) {
-    // Do nothing
-  }
-
-  @override
-  void trackOrder(
-    String? orderId,
-    List<TrackingOrderItem>? trackingOrderItems,
-    num? revenue,
-    num? subTotal,
-    num? taxAmount,
-    num? shippingCost,
-    num? discountAmount, {
     Map<String, String>? dimensions,
   }) {
     // Do nothing
@@ -161,31 +68,6 @@ class DummyMatomoTracker implements PassEmploiMatomoTracker {
     String? path,
     Map<String, String>? dimensions,
   }) {
-    // Do nothing
-  }
-
-  @override
-  void trackSearch(
-      {required String searchKeyword, String? searchCategory, int? searchCount, Map<String, String>? dimensions}) {
-    // Do nothing
-  }
-
-  @override
-  Future<void> initialize({
-    required int siteId,
-    required String url,
-    String? visitorId,
-    String? contentBaseUrl,
-    int dequeueInterval = 10,
-    String? tokenAuth,
-    SharedPreferences? prefs,
-    PackageInfo? packageInfo,
-  }) async {
-    // Do nothing
-  }
-
-  @override
-  void setVisitorUserId(String? userId) {
     // Do nothing
   }
 }
