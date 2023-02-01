d25e041ab40359bfaaca60069d81e43597eaccab
gabriel.adgeg <gabriel.adgeg@octo.com>
2023-02-01 02:15:47 +0100

    chore(1353): use material 3 to avoid bad list effects

--- lib/widgets/default_app_bar.dart
+++ lib/widgets/default_app_bar.dart
@@ -10,17 +10,17 @@ import 'package:pass_emploi_app/widgets/profile_button.dart';
 class SecondaryAppBar extends StatelessWidget with PreferredSizeWidget {
   final String title;
   final Color? backgroundColor;
+
   const SecondaryAppBar({super.key, required this.title, this.backgroundColor});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       titleSpacing: 0,
-      iconTheme: IconThemeData(
-        color: Colors.black,
-      ),
+      iconTheme: IconThemeData(color: Colors.black),
       elevation: 0,
       centerTitle: false,
+      scrolledUnderElevation: 0,
       backgroundColor: backgroundColor ?? Theme.of(context).scaffoldBackgroundColor,
       title: Text(
         title,
@@ -71,6 +71,7 @@ class PrimaryAppBar extends StatelessWidget with PreferredSizeWidget {
   final String title;
   final Color? backgroundColor;
   final bool withProfileButton;
+
   const PrimaryAppBar({super.key, required this.title, this.backgroundColor, this.withProfileButton = true});
 
   @override
