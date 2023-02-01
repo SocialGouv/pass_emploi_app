3db379720c8d2eb009ca95917e751d16dbfb4b5b
adrien.cens <adrien.cens@edu.esiee.fr>
2023-02-01 10:25:53 +0100

    refacto(1345): BSR

--- lib/ui/drawables.dart
+++ lib/ui/drawables.dart
@@ -18,10 +18,6 @@ class Drawables {
   static String icLogo = _assets + "ic_logo" + _svg;
   static String icRefresh = _assets + "ic_refresh" + _svg;
   static String icActualisation = _assets + "ic_actualisation" + _svg;
-  static String icMenuHome = _assets + "ic_menu_home" + _svg;
-  static String icMenuAction = _assets + "ic_menu_action" + _svg;
-  static String icMenuChat = _assets + "ic_menu_chat" + _svg;
-  static String icMenuPlus = _assets + "ic_menu_plus" + _svg;
   static String icMenuSelectedBullet = _assets + "ic_menu_selected_bullet" + _svg;
   static String icSearchingBar = _assets + "ic_searching" + _svg;
   static String icBadge = _assets + "ic_badge" + _svg;
