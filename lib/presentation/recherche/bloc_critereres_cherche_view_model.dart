3737f71d82462a91295b3a0c0aacb1bc1bab0dca
gabriel.adgeg <gabriel.adgeg@octo.com>
2023-02-01 03:04:24 +0100

    chore(1353): add real criteres

--- lib/presentation/recherche/bloc_critereres_cherche_view_model.dart
+++ lib/presentation/recherche/bloc_critereres_cherche_view_model.dart
@@ -1,12 +1,20 @@
 import 'package:equatable/equatable.dart';
+import 'package:pass_emploi_app/features/recherche/recherche_actions.dart';
 import 'package:pass_emploi_app/features/recherche/recherche_state.dart';
+import 'package:pass_emploi_app/models/location.dart';
+import 'package:pass_emploi_app/models/offre_emploi_filtres_parameters.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
+import 'package:pass_emploi_app/repositories/offre_emploi_repository.dart';
 import 'package:redux/redux.dart';
 
 class BlocCriteresRechercheViewModel extends Equatable {
   final bool isOpen;
+  final Function(String keywords, Location? location, bool onlyAlternance) onSearchingRequest;
 
-  BlocCriteresRechercheViewModel(this.isOpen);
+  BlocCriteresRechercheViewModel({
+    required this.isOpen,
+    required this.onSearchingRequest,
+  });
 
   factory BlocCriteresRechercheViewModel.create(Store<AppState> store) {
     final isOpen = [
@@ -14,7 +22,18 @@ class BlocCriteresRechercheViewModel extends Equatable {
       RechercheStatus.loading,
       RechercheStatus.failure,
     ].contains(store.state.rechercheEmploiState.status);
-    return BlocCriteresRechercheViewModel(isOpen);
+    return BlocCriteresRechercheViewModel(
+      isOpen: isOpen,
+      onSearchingRequest: (keywords, location, onlyAlternance) {
+        store.dispatch(RechercheRequestAction<EmploiCriteresRecherche, OffreEmploiSearchParametersFiltres>(
+          RechercheRequest(
+            EmploiCriteresRecherche(keywords: keywords, location: location, onlyAlternance: onlyAlternance),
+            OffreEmploiSearchParametersFiltres.noFiltres(),
+            1,
+          ),
+        ));
+      },
+    );
   }
 
   @override
