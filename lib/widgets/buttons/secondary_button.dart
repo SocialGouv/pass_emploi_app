da26dae32c8e2c598c8753401e308b28b4d37e97
adrien.cens <adrien.cens@edu.esiee.fr>
2023-02-06 18:25:24 +0100

    feat(1376): add icon in secondary button

--- lib/widgets/buttons/secondary_button.dart
+++ lib/widgets/buttons/secondary_button.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pass_emploi_app/ui/app_colors.dart';
+import 'package:pass_emploi_app/ui/dimens.dart';
 import 'package:pass_emploi_app/ui/text_styles.dart';
 
 class SecondaryButton extends StatelessWidget {
   final String label;
   final String? drawableRes;
+  final IconData? icon;
   final VoidCallback? onPressed;
   final Color backgroundColor;
   final double? fontSize;
@@ -15,6 +17,7 @@ class SecondaryButton extends StatelessWidget {
     required this.label,
     required this.onPressed,
     this.drawableRes,
+    this.icon,
     this.backgroundColor = Colors.transparent,
     this.fontSize,
   }) : super(key: key);
@@ -35,15 +38,21 @@ class SecondaryButton extends StatelessWidget {
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            if (drawableRes != null)
+            if (drawableRes != null || icon != null)
               Padding(
                 padding: const EdgeInsets.only(right: 12),
-                child: SvgPicture.asset(
-                  drawableRes!,
-                  color: AppColors.primary,
-                  width: 10,
-                  height: 10,
-                ),
+                child: icon != null
+                    ? Icon(
+                        icon,
+                        color: AppColors.primary,
+                        size: Dimens.icon_size_base,
+                      )
+                    : SvgPicture.asset(
+                        drawableRes!,
+                        color: AppColors.primary,
+                        width: 10,
+                        height: 10,
+                      ),
               ),
             Flexible(child: Text(label, textAlign: TextAlign.center, style: usedTextStyle)),
           ],
