e5404d084da9c743de10b052ad11b3247481902d
adrien.cens <adrien.cens@edu.esiee.fr>
2023-02-06 17:49:39 +0100

    feat(1376): add icon size in dimens

--- lib/ui/dimens.dart
+++ lib/ui/dimens.dart
@@ -5,4 +5,7 @@ class Dimens {
   static const double bottomNavigationBarItemHeight = 21;
   static const double radius_base = 8;
   static const double radius_l = 24;
+
+  static const double icon_size_base = 16;
+  static const double icon_size_m = 24;
 }
