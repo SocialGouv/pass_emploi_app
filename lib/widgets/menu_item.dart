e39ba772eea5302a4ad9ea359df2e36ea0e675bb
adrien.cens <adrien.cens@edu.esiee.fr>
2023-02-07 17:56:13 +0100

    refacto(1376): nav icons

--- lib/widgets/menu_item.dart
+++ lib/widgets/menu_item.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:pass_emploi_app/widgets/default_menu_item.dart';
 
 class MenuItem extends BottomNavigationBarItem {
-  final String drawableRes;
-  final String inactiveDrawableRes;
+  final IconData defaultIcon;
+  final IconData inactiveIcon;
   final bool withBadge;
 
   MenuItem({
-    required this.drawableRes,
-    required this.inactiveDrawableRes,
+    required this.defaultIcon,
+    required this.inactiveIcon,
     required String label,
     this.withBadge = false,
   }) : super(
-          icon: DefaultMenuItem(drawableRes: inactiveDrawableRes, label: label, isActive: false, withBadge: withBadge),
-          activeIcon: DefaultMenuItem(drawableRes: drawableRes, label: label, isActive: true, withBadge: false),
+          icon: DefaultMenuItem(icon: inactiveIcon, label: label, isActive: false, withBadge: withBadge),
+          activeIcon: DefaultMenuItem(icon: defaultIcon, label: label, isActive: true, withBadge: false),
           label: label,
         );
 }
