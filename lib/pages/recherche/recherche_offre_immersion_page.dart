45586b48ca9cb503223170d7c7f6f08012386a2d
Jordan Chapuy <chapuyj@gmail.com>
2023-02-10 15:55:35 +0100

    feat(1356): recherche immersion - actions view model

--- lib/pages/recherche/recherche_offre_immersion_page.dart
+++ lib/pages/recherche/recherche_offre_immersion_page.dart
@@ -7,7 +7,7 @@ import 'package:pass_emploi_app/pages/offre_page.dart';
 import 'package:pass_emploi_app/pages/recherche/recherche_offre_page.dart';
 import 'package:pass_emploi_app/pages/service_civique/service_civique_filtres_page.dart';
 import 'package:pass_emploi_app/presentation/recherche/actions_recherche_view_model.dart';
-import 'package:pass_emploi_app/presentation/recherche/service_civique/actions_recherche_service_civique_view_model.dart';
+import 'package:pass_emploi_app/presentation/recherche/immersion/actions_recherche_immersion_view_model.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
 import 'package:pass_emploi_app/widgets/bottom_sheets/service_civique_saved_search_bottom_sheet.dart';
@@ -21,10 +21,9 @@ class RechercheOffreImmersionPage extends RechercheOffrePage<Immersion> {
     return MaterialPageRoute(builder: (context) => RechercheOffreImmersionPage());
   }
 
-  //TODO(1356)
   @override
   ActionsRechercheViewModel buildActionsRechercheViewModel(Store<AppState> store) {
-    return ActionsRechercheServiceCiviqueViewModel.create(store);
+    return ActionsRechercheImmersionViewModel.create(store);
   }
 
   @override
