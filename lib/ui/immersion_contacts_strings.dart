adee92095ec0242ad458ecc339461d947450df53
adrien.cens <adrien.cens@edu.esiee.fr>
2023-02-14 17:12:21 +0100

    feat(1358) strings

---
+++ lib/ui/immersion_contacts_strings.dart
@@ -0,0 +1,31 @@
+class ImmersionContactStrings {
+  static const String title = "Nos conseils pour cette première prise de contact !";
+  static const String subtitle1 = "Comment présenter votre demande ?";
+  static const String body1_1 = "Soyez ";
+  static const String body1_2bold = "direct, concret et courtois.";
+  static const String body1_3 =
+      "Présentez-vous, présentez simplement votre projet et l’objectif que vous recherchez en effectuant une immersion.\n\n";
+  static const String body1_4bold = "Par exemple : ";
+  static const String body1_5 =
+      "“Je souhaite devenir mécanicien auto et je voudrais découvrir comment ce métier se pratique dans un garage comme le vôtre. Ça me permettra de vérifier que cela me plaît vraiment. La personne qui m’accueillera et me présentera le métier pourra aussi vérifier si ce métier est fait pour moi.\n\n”";
+  static const String body1_6 = "Vous pouvez indiquer à votre interlocutrice ou interlocuteur que";
+  static const String body1_7bold =
+      "cette immersion sera encadrée par une convention signée par l'organisme qui vous suit.\n\n";
+  static const String body1_8 =
+      "Indiquez lui le moment où vous aimeriez faire une immersion et pourquoi vous voulez la faire à cette date.\n\n";
+  static const String body1_9bold = "Par exemple : ";
+  static const String body1_10 = "“il faudrait que je fasse une immersion avant de m’inscrire à une formation.\n\n“";
+  static const String body1_11 =
+      "Indiquez également le nombre de jours que vous aimeriez faire en immersion si vous le savez déjà.\n\n";
+  static const String body1_12 =
+      "Concluez en lui demandant un rendez-vous pour qu’il/elle se rende compte du sérieux de votre projet.\n\n";
+  static const String subtitle2 = "Comment expliquer simplement ce qu’est une immersion ?";
+  static const String body2 =
+      "C’est un stage d’observation, strictement encadré d’un point de vue juridique. Vous conservez votre statut et êtes couvert par votre Pôle emploi, votre Mission Locale ou le Conseil départemental (en fonction de votre situation).\n\nLe rôle de celui qui vous accueillera est de vous présenter le métier et de vérifier avec vous que ce métier vous convient en vous faisant des retours les plus objectifs possibles. Pendant la durée de votre présence, vous pouvez aider les salariés en donnant un coup de main mais vous n’êtes pas là pour remplacer un collègue absent.";
+  static const String subtitle3 = "Quelle est la durée d’une immersion ?";
+  static const String body3 =
+      "Les immersions se font le plus souvent pendant une semaine ou deux. Il n’est pas possible de dépasser un mois. Il est possible de faire une immersion de seulement un ou deux jours mais vous ne découvrirez pas parfaitement un métier.";
+  static const String subtitle4 = "Bon à savoir !";
+  static const String body4 =
+      "Il n’est pas nécessaire d’apporter votre CV. Vous êtes là pour demander à découvrir un métier et c’est ce projet qui est important, pas vos expériences professionnelles ni votre formation !";
+}
