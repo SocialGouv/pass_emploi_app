cc41769a17d15a769a13e1baa38bee458bb362c2
gabriel.adgeg <gabriel.adgeg@octo.com>
2023-02-20 16:25:47 +0100

    chore(1394): Use custom widget for expansion tile instead of previous copied-pasted one.

--- lib/widgets/recherche/bloc_criteres_recherche.dart
+++ lib/widgets/recherche/bloc_criteres_recherche.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-
 import 'package:pass_emploi_app/features/recherche/recherche_state.dart';
 import 'package:pass_emploi_app/presentation/recherche/bloc_criteres_cherche_view_model.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
-import 'package:pass_emploi_app/ui/app_colors.dart';
-import 'package:pass_emploi_app/ui/dimens.dart';
-import 'package:pass_emploi_app/widgets/customized_flutter_widgets/cej_expansion_tile.dart';
-import 'package:pass_emploi_app/widgets/recherche/criteres_recherche_bandeau.dart';
+import 'package:pass_emploi_app/widgets/recherche/criteres_recheche_expansion_tile.dart';
 
 class BlocCriteresRecherche<Result> extends StatefulWidget {
   final RechercheState Function(AppState) rechercheState;
@@ -20,56 +16,26 @@ class BlocCriteresRecherche<Result> extends StatefulWidget {
 }
 
 class _BlocCriteresRechercheState<Result> extends State<BlocCriteresRecherche<Result>> {
-  final _expansionTileKey = GlobalKey();
   int? _criteresActifsCount;
 
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, BlocCriteresRechercheViewModel<Result>>(
       converter: (store) => BlocCriteresRechercheViewModel.create(store, widget.rechercheState),
-      onDidChange: (previousViewModel, currentViewModel) {
-        if (!currentViewModel.isOpen) {
-          final currentState = _expansionTileKey.currentState;
-          if (currentState is CejExpansionTileState) {
-            currentState.closeExpansion();
-          }
-        }
-      },
       builder: _builder,
       distinct: true,
     );
   }
 
   Widget _builder(BuildContext context, BlocCriteresRechercheViewModel<Result> viewModel) {
-    return Material(
-      elevation: 16,
-      borderRadius: BorderRadius.circular(Dimens.radius_base),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(Dimens.radius_base),
-        child: Theme(
-          data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-          child: CejExpansionTile(
-            key: _expansionTileKey,
-            onExpansionChanged: viewModel.onExpansionChanged,
-            maintainState: true,
-            backgroundColor: Colors.white,
-            textColor: AppColors.contentColor,
-            iconColor: AppColors.primary,
-            collapsedBackgroundColor: AppColors.primary,
-            collapsedTextColor: Colors.white,
-            collapsedIconColor: Colors.white,
-            leading: Icon(Icons.search),
-            title: CriteresRechercheBandeau(criteresActifsCount: _criteresActifsCount ?? 0),
-            initiallyExpanded: viewModel.isOpen,
-            children: [
-              widget.buildCriteresContentWidget(
-                onNumberOfCriteresChanged: (number) {
-                  setState(() => _criteresActifsCount = number);
-                },
-              ),
-            ],
-          ),
-        ),
+    return CriteresRechercheExpansionTile(
+      initiallyExpanded: viewModel.isOpen,
+      onExpansionChanged: viewModel.onExpansionChanged,
+      criteresActifsCount: _criteresActifsCount ?? 0,
+      child: widget.buildCriteresContentWidget(
+        onNumberOfCriteresChanged: (number) {
+          setState(() => _criteresActifsCount = number);
+        },
       ),
     );
   }
