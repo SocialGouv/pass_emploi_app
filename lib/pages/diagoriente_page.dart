43d8d3bc6fb962cb67a48e467363224ed5fa6fbc
gabriel.adgeg <gabriel.adgeg@octo.com>
2023-02-22 11:46:19 +0100

    WIP 2

--- lib/pages/diagoriente_page.dart
+++ lib/pages/diagoriente_page.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:pass_emploi_app/app_initializer.dart';
 import 'package:pass_emploi_app/repositories/diagoriente_repository.dart';
+import 'package:pass_emploi_app/widgets/buttons/primary_action_button.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class DiagorientePage extends StatelessWidget {
   static MaterialPageRoute<void> materialPageRoute() => MaterialPageRoute(builder: (context) => DiagorientePage());
 
+  final controller = WebViewController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: PrimaryActionButton(
+        label: 'clic',
+        onPressed: () => controller.runJavaScript(
+          'window.dispatchEvent(new CustomEvent("INCA_ChatbotInteract_Req", { detail: { target: view_top_jobs_full } }));',
+        ),
+      ),
       body: FutureBuilder<String?>(
         future: DiagorienteRepository(appDio).getChatBotUrl('ZKBAC'),
         builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
           if (snapshot.hasData) {
-            final controller = WebViewController()
+            controller
               ..setJavaScriptMode(JavaScriptMode.unrestricted)
-              ..loadRequest(Uri.parse(snapshot.data!));
+              ..addJavaScriptChannel(
+                'messageHandler',
+                onMessageReceived: (JavaScriptMessage message) {
+                  print("####### message from the web view=\"${message.message}\"");
+                },
+              )
+              ..loadRequest(Uri.parse(snapshot.data!))
+              ..runJavaScript(
+                'window.addEventListener("INCA_ChatbotStateChange_Inf", (evt) => { messageHandler.postMessage(evt); });',
+              );
             return WebViewWidget(controller: controller);
           } else {
             return Center(child: CircularProgressIndicator());
