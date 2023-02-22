c09fca90221089fb1d33e2b8813be4646c84743a
gabriel.adgeg <gabriel.adgeg@octo.com>
2023-02-22 14:29:04 +0100

    WIP 4

--- lib/pages/diagoriente_page.dart
+++ lib/pages/diagoriente_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pass_emploi_app/app_initializer.dart';
 import 'package:pass_emploi_app/repositories/diagoriente_repository.dart';
-import 'package:pass_emploi_app/widgets/buttons/primary_action_button.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class DiagorientePage extends StatelessWidget {
@@ -18,15 +17,15 @@ class DiagorientePage extends StatelessWidget {
           if (snapshot.hasData) {
             controller
               ..setJavaScriptMode(JavaScriptMode.unrestricted)
+              ..loadRequest(Uri.parse(snapshot.data!))
               ..addJavaScriptChannel(
                 'cejHandler',
                 onMessageReceived: (JavaScriptMessage message) {
                   print("####### message from the web view=\"${message.message}\"");
                 },
               )
-              ..loadRequest(Uri.parse(snapshot.data!))
               ..runJavaScript(
-                'window.addEventListener("INCA_ChatbotStateChange_Inf", (evt) => { cejHandler.postMessage(evt); });',
+                'window.addEventListener("INCA_ChatbotStateChange_Inf", (evt) => { console.log("####### console.log"); cejHandler.postMessage(evt); });',
               );
             return WebViewWidget(controller: controller);
           } else {
@@ -37,19 +36,19 @@ class DiagorientePage extends StatelessWidget {
       floatingActionButton: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          PrimaryActionButton(
-            label: 'Voir tous les résultats',
-            onPressed: () => controller.runJavaScript(
-              'window.dispatchEvent(new CustomEvent("INCA_ChatbotInteract_Req", { detail: { target: "view_top_jobs_full" } }));',
-            ),
-          ),
-          SizedBox(height: 8),
-          PrimaryActionButton(
-            label: 'Fermer le déroulé des métiers',
-            onPressed: () => controller.runJavaScript(
-              'window.dispatchEvent(new CustomEvent("INCA_ChatbotInteract_Req", { detail: { target: "close_jobs_drawer" } }));',
-            ),
-          ),
+          //PrimaryActionButton(
+          //  label: 'Voir tous les résultats',
+          //  onPressed: () => controller.runJavaScript(
+          //    'window.dispatchEvent(new CustomEvent("INCA_ChatbotInteract_Req", { detail: { target: "view_top_jobs_full" } }));',
+          //  ),
+          //),
+          //SizedBox(height: 8),
+          //PrimaryActionButton(
+          //  label: 'Fermer le déroulé des métiers',
+          //  onPressed: () => controller.runJavaScript(
+          //    'window.dispatchEvent(new CustomEvent("INCA_ChatbotInteract_Req", { detail: { target: "close_jobs_drawer" } }));',
+          //  ),
+          //),
         ],
       ),
     );
