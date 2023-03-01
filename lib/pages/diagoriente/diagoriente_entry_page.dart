773436a7866db3b2ee3cc074b229ca908e5e4771
Jordan Chapuy <chapuyj@gmail.com>
2023-03-01 17:09:38 +0100

    fix: diagoriente scaffold

--- lib/pages/diagoriente/diagoriente_entry_page.dart
+++ lib/pages/diagoriente/diagoriente_entry_page.dart
@@ -8,11 +8,13 @@ import 'package:pass_emploi_app/presentation/diagoriente/diagoriente_chat_bot_pa
 import 'package:pass_emploi_app/presentation/diagoriente/diagoriente_entry_page_view_model.dart';
 import 'package:pass_emploi_app/presentation/display_state.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
+import 'package:pass_emploi_app/ui/app_colors.dart';
 import 'package:pass_emploi_app/ui/margins.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
 import 'package:pass_emploi_app/ui/text_styles.dart';
 import 'package:pass_emploi_app/widgets/buttons/primary_action_button.dart';
 import 'package:pass_emploi_app/widgets/cards/generic/card_container.dart';
+import 'package:pass_emploi_app/widgets/default_app_bar.dart';
 import 'package:pass_emploi_app/widgets/pressed_tip.dart';
 import 'package:pass_emploi_app/widgets/retry.dart';
 
@@ -26,14 +28,40 @@ class DiagorienteEntryPage extends StatelessWidget {
       child: StoreConnector<AppState, DiagorienteEntryPageViewModel>(
         onInit: (store) => store.dispatch(DiagorientePreferencesMetierRequestAction()),
         converter: (store) => DiagorienteEntryPageViewModel.create(store),
-        builder: _builder,
+        builder: (context, vm) => _Scaffold(vm),
         onDispose: (store) => store.dispatch(DiagorientePreferencesMetierResetAction()),
         distinct: true,
       ),
     );
   }
+}
 
-  Widget _builder(BuildContext context, DiagorienteEntryPageViewModel viewModel) {
+class _Scaffold extends StatelessWidget {
+  final DiagorienteEntryPageViewModel viewModel;
+
+  const _Scaffold(this.viewModel);
+
+  @override
+  Widget build(BuildContext context) {
+    const backgroundColor = AppColors.grey100;
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: SecondaryAppBar(title: Strings.diagorienteEntryPageTitle, backgroundColor: backgroundColor),
+      body: Padding(
+        padding: const EdgeInsets.all(Margins.spacing_base),
+        child: _Body(viewModel),
+      ),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final DiagorienteEntryPageViewModel viewModel;
+
+  const _Body(this.viewModel);
+
+  @override
+  Widget build(BuildContext context) {
     switch (viewModel.displayState) {
       case DisplayState.FAILURE:
         return Center(child: Retry(Strings.diagorienteMetiersCardError, () => viewModel.onRetry()));
@@ -52,17 +80,14 @@ class _Content extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(Margins.spacing_base),
-      child: Column(
-        children: [
-          if (withMetiersFavoris) ...[
-            _DiagorienteMetiersFavorisCard(),
-            SizedBox(height: Margins.spacing_base),
-          ],
-          _DecouvrirLesMetiersCard(),
+    return Column(
+      children: [
+        if (withMetiersFavoris) ...[
+          _DiagorienteMetiersFavorisCard(),
+          SizedBox(height: Margins.spacing_base),
         ],
-      ),
+        _DecouvrirLesMetiersCard(),
+      ],
     );
   }
 }
