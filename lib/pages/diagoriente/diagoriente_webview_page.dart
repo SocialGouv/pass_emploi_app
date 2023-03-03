4cf545f7b489834287e7cbf99774d1040ab0e27a
adrien.cens <adrien.cens@edu.esiee.fr>
2023-03-03 16:39:07 +0100

    feat(1431): WIP

--- lib/pages/diagoriente/diagoriente_webview_page.dart
+++ lib/pages/diagoriente/diagoriente_webview_page.dart
@@ -9,28 +9,20 @@ import 'package:pass_emploi_app/widgets/buttons/primary_action_button.dart';
 import 'package:pass_emploi_app/widgets/default_app_bar.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class DiagorienteWebviewPage extends StatefulWidget {
+class DiagorienteWebviewPage extends StatelessWidget {
   final DiagorienteWebviewMode mode;
-
-  const DiagorienteWebviewPage(this.mode);
+  DiagorienteWebviewPage(this.mode);
 
   static MaterialPageRoute<void> materialPageRoute(DiagorienteWebviewMode mode) {
     return MaterialPageRoute(fullscreenDialog: true, builder: (context) => DiagorienteWebviewPage(mode));
   }
 
   @override
-  State<DiagorienteWebviewPage> createState() => _DiagorienteWebviewPageState();
-}
-
-class _DiagorienteWebviewPageState extends State<DiagorienteWebviewPage> {
-  bool showBoutons = false;
-
-  @override
   Widget build(BuildContext context) {
     return Tracker(
       tracking: AnalyticsScreenNames.diagorienteChatBot,
       child: StoreConnector<AppState, DiagorienteWebviewViewModel>(
-        converter: (store) => DiagorienteWebviewViewModel.create(store, widget.mode),
+        converter: (store) => DiagorienteWebviewViewModel.create(store, mode),
         builder: _builder,
         rebuildOnChange: false,
       ),
@@ -39,7 +31,7 @@ class _DiagorienteWebviewPageState extends State<DiagorienteWebviewPage> {
 
   Widget _builder(BuildContext context, DiagorienteWebviewViewModel viewModel) {
     const backgroundColor = AppColors.grey100;
-
+    final DiagorienteDrawer diagorienteDrawer = DiagorienteDrawer();
     final controller = WebViewController();
     final delegate = NavigationDelegate(onPageFinished: (_) => controller.addChatbotListener());
     controller
@@ -51,35 +43,53 @@ class _DiagorienteWebviewPageState extends State<DiagorienteWebviewPage> {
         'cejHandler',
         onMessageReceived: (JavaScriptMessage message) {
           print("@@@ isJobsDrawerOpen=\"${message.message}\"");
-          //TODO: ça part en boucle avec le set state car la page envoie un evt à chaque démarrage
-          // setState(() {
-          //   showBoutons = message.toString() == "true";
-          // });
+          diagorienteDrawer.changed(message.message == "true");
         },
       );
 
     return Scaffold(
       appBar: SecondaryAppBar(title: viewModel.appBarTitle, backgroundColor: backgroundColor),
-      body: WebViewWidget(controller: controller),
-      floatingActionButton: showBoutons ? _Boutons(controller) : null,
+      body: Column(
+        children: [
+          Expanded(child: WebViewWidget(controller: controller)),
+          _Boutons(controller, diagorienteDrawer),
+        ],
+      ),
     );
   }
 }
 
-class _Boutons extends StatelessWidget {
+class _Boutons extends StatefulWidget {
   final WebViewController _controller;
+  final DiagorienteDrawer diagorienteDrawer;
 
-  const _Boutons(this._controller);
+  const _Boutons(this._controller, this.diagorienteDrawer);
+
+  @override
+  State<_Boutons> createState() => _BoutonsState();
+}
+
+class _BoutonsState extends State<_Boutons> {
+  bool showButtons = false;
+
+  @override
+  void initState() {
+    widget.diagorienteDrawer.addListener(() {
+      setState(() => showButtons = widget.diagorienteDrawer.open);
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (!showButtons) return SizedBox.shrink();
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         PrimaryActionButton(
           label: 'Voir tous les résultats',
           onPressed: () async {
-            await _controller.runJavaScript(
+            await widget._controller.runJavaScript(
               'window.dispatchEvent(new CustomEvent("INCA_ChatbotInteract_Req", { detail: { target: "view_top_jobs_full" } }));',
             );
           },
@@ -88,7 +98,7 @@ class _Boutons extends StatelessWidget {
         PrimaryActionButton(
           label: 'Fermer le déroulé des métiers',
           onPressed: () async {
-            await _controller.runJavaScript(
+            await widget._controller.runJavaScript(
               'window.dispatchEvent(new CustomEvent("INCA_ChatbotInteract_Req", { detail: { target: "close_jobs_drawer" } }));',
             );
           },
@@ -104,5 +114,14 @@ extension _WebViewController on WebViewController {
     runJavaScript('''window.addEventListener("INCA_ChatbotStateChange_Inf", (evt) => { 
               cejHandler.postMessage(evt.detail.isJobsDrawerOpen); 
           });''');
+  }
+}
+
+class DiagorienteDrawer extends ChangeNotifier {
+  bool open = false;
+
+  void changed(bool value) {
+    open = value;
+    notifyListeners();
   }
 }
