3fbc330c1224c5fa283116098fbf336e52bde898
adrien.cens <adrien.cens@edu.esiee.fr>
2023-03-03 12:14:41 +0100

    feat(1431): wip

--- lib/pages/diagoriente/diagoriente_webview_page.dart
+++ lib/pages/diagoriente/diagoriente_webview_page.dart
@@ -5,10 +5,11 @@ import 'package:pass_emploi_app/analytics/tracker.dart';
 import 'package:pass_emploi_app/presentation/diagoriente/diagoriente_webview_view_model.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
 import 'package:pass_emploi_app/ui/app_colors.dart';
+import 'package:pass_emploi_app/widgets/buttons/primary_action_button.dart';
 import 'package:pass_emploi_app/widgets/default_app_bar.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class DiagorienteWebviewPage extends StatelessWidget {
+class DiagorienteWebviewPage extends StatefulWidget {
   final DiagorienteWebviewMode mode;
 
   const DiagorienteWebviewPage(this.mode);
@@ -18,11 +19,18 @@ class DiagorienteWebviewPage extends StatelessWidget {
   }
 
   @override
+  State<DiagorienteWebviewPage> createState() => _DiagorienteWebviewPageState();
+}
+
+class _DiagorienteWebviewPageState extends State<DiagorienteWebviewPage> {
+  bool showBoutons = false;
+
+  @override
   Widget build(BuildContext context) {
     return Tracker(
       tracking: AnalyticsScreenNames.diagorienteChatBot,
       child: StoreConnector<AppState, DiagorienteWebviewViewModel>(
-        converter: (store) => DiagorienteWebviewViewModel.create(store, mode),
+        converter: (store) => DiagorienteWebviewViewModel.create(store, widget.mode),
         builder: _builder,
         rebuildOnChange: false,
       ),
@@ -31,14 +39,70 @@ class DiagorienteWebviewPage extends StatelessWidget {
 
   Widget _builder(BuildContext context, DiagorienteWebviewViewModel viewModel) {
     const backgroundColor = AppColors.grey100;
-    final controller = WebViewController()
+
+    final controller = WebViewController();
+    final delegate = NavigationDelegate(onPageFinished: (_) => controller.addChatbotListener());
+    controller
       ..setBackgroundColor(backgroundColor)
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(viewModel.url));
+      ..setNavigationDelegate(delegate)
+      ..loadRequest(Uri.parse(viewModel.url))
+      ..addJavaScriptChannel(
+        'cejHandler',
+        onMessageReceived: (JavaScriptMessage message) {
+          print("@@@ isJobsDrawerOpen=\"${message.message}\"");
+          //TODO: ça part en boucle avec le set state car la page envoie un evt à chaque démarrage
+          // setState(() {
+          //   showBoutons = message.toString() == "true";
+          // });
+        },
+      );
 
     return Scaffold(
       appBar: SecondaryAppBar(title: viewModel.appBarTitle, backgroundColor: backgroundColor),
       body: WebViewWidget(controller: controller),
+      floatingActionButton: showBoutons ? _Boutons(controller) : null,
     );
+  }
+}
+
+class _Boutons extends StatelessWidget {
+  final WebViewController _controller;
+
+  const _Boutons(this._controller);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        PrimaryActionButton(
+          label: 'Voir tous les résultats',
+          onPressed: () async {
+            await _controller.runJavaScript(
+              'window.dispatchEvent(new CustomEvent("INCA_ChatbotInteract_Req", { detail: { target: "view_top_jobs_full" } }));',
+            );
+          },
+        ),
+        SizedBox(height: 8),
+        PrimaryActionButton(
+          label: 'Fermer le déroulé des métiers',
+          onPressed: () async {
+            await _controller.runJavaScript(
+              'window.dispatchEvent(new CustomEvent("INCA_ChatbotInteract_Req", { detail: { target: "close_jobs_drawer" } }));',
+            );
+          },
+        ),
+        SizedBox(height: 8),
+      ],
+    );
+  }
+}
+
+extension _WebViewController on WebViewController {
+  Future<void> addChatbotListener() async {
+    runJavaScript('''window.addEventListener("INCA_ChatbotStateChange_Inf", (evt) => { 
+              cejHandler.postMessage(evt.detail.isJobsDrawerOpen); 
+          });''');
   }
 }
