cdcae69e8a66b1fa57752b2bce078354d8f09cae
adrien.cens <adrien.cens@edu.esiee.fr>
2023-03-06 15:08:57 +0100

    refacto(1425): extract nos offres view

---
+++ lib/pages/recherche/recherches_recentes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecherchesRecentes extends StatelessWidget {
+  const RecherchesRecentes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
