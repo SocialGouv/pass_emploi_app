261708562200e312054871c6ebb70d59fd7c9ed4
adrien.cens <adrien.cens@edu.esiee.fr>
2023-03-14 11:32:44 +0100

    feat(1476): dispatch request action

--- lib/widgets/text_form_fields/full_screen_text_form_field_page.dart
+++ lib/widgets/text_form_fields/full_screen_text_form_field_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:pass_emploi_app/features/recherches_derniers_mots_cles/recherches_derniers_mots_cles_actions.dart';
 import 'package:pass_emploi_app/presentation/derniers_mots_cles_view_model.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
 import 'package:pass_emploi_app/widgets/text_form_fields/utils/debounce_text_form_field.dart';
@@ -35,9 +36,15 @@ class KeywordTextFormFieldPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return FullScreenTextFormFieldScaffold(
       body: StoreConnector<AppState, DerniersMotsClesViewModel>(
+        onInit: (store) => store.dispatch(RecherchesDerniersMotsClesRequestAction()),
         converter: (store) => DerniersMotsClesViewModel.create(store),
         builder: (context, viewModel) {
-          return _Body(title: title, hint: hint, selectedKeyword: selectedKeyword, heroTag: heroTag);
+          return _Body(
+            title: title,
+            hint: hint,
+            selectedKeyword: selectedKeyword,
+            heroTag: heroTag,
+          );
         },
       ),
     );
