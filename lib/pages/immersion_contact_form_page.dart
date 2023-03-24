cb06c6ab79e1ccf3530ccf502ffeb5b656e0ccb5
adrien.cens <adrien.cens@edu.esiee.fr>
2023-03-24 16:05:04 +0100

    feat(1510): add form validation logic

--- lib/pages/immersion_contact_form_page.dart
+++ lib/pages/immersion_contact_form_page.dart
@@ -28,9 +28,64 @@ class ImmersionContactFormPage extends StatelessWidget {
   }
 }
 
-class _Content extends StatelessWidget {
+class _Content extends StatefulWidget {
   final ImmersionContactFormViewModel viewModel;
   const _Content(this.viewModel);
+
+  @override
+  State<_Content> createState() => _ContentState();
+}
+
+class _ContentState extends State<_Content> {
+  late TextEditingController _userEmail;
+  late TextEditingController _userFirstName;
+  late TextEditingController _userLastName;
+  late TextEditingController _message;
+  bool _isFormValid = false;
+
+  @override
+  void initState() {
+    _userEmail = TextEditingController(text: widget.viewModel.userEmailInitialValue)..addListener(_onAnyFieldChanged);
+    _userFirstName = TextEditingController(text: widget.viewModel.userFirstNameInitialValue)
+      ..addListener(_onAnyFieldChanged);
+    _userLastName = TextEditingController(text: widget.viewModel.userLastNameInitialValue)
+      ..addListener(_onAnyFieldChanged);
+    _message = TextEditingController(text: widget.viewModel.messageInitialValue)..addListener(_onAnyFieldChanged);
+    _isFormValid = _isFormFieldsValid();
+    super.initState();
+  }
+
+  bool _isFormFieldsValid() {
+    return _isEmailValid() && _isFirstNameValid() && _isLastNameValid() && _isMessageValid();
+  }
+
+  bool _isEmailValid() {
+    final emailRegex = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final value = _userEmail.text;
+    return emailRegex.hasMatch(value) && !_isEmailEmpty();
+  }
+
+  bool _isEmailEmpty() {
+    return _userEmail.text.isEmpty;
+  }
+
+  bool _isFirstNameValid() {
+    return _userFirstName.text.isNotEmpty;
+  }
+
+  bool _isLastNameValid() {
+    return _userLastName.text.isNotEmpty;
+  }
+
+  bool _isMessageValid() {
+    return _message.text.isNotEmpty;
+  }
+
+  void _onAnyFieldChanged() {
+    setState(() {
+      _isFormValid = _isFormFieldsValid();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +98,7 @@ class _Content extends StatelessWidget {
             child: PrimaryActionButton(
               label: Strings.immersionContactFormButton,
               icon: AppIcons.outgoing_mail,
-              onPressed: () {},
+              onPressed: _isFormValid ? () {} : null,
             ),
           ),
         ),
@@ -67,26 +122,34 @@ class _Content extends StatelessWidget {
                 SizedBox(height: Margins.spacing_m),
                 ImmersionTextFormField(
                   isMandatory: true,
-                  initialValue: viewModel.userEmailInitialValue,
+                  mandatoryError: _isEmailValid()
+                      ? null
+                      : _isEmailEmpty()
+                          ? "Champ obligatoire"
+                          : "Email invalide",
+                  controller: _userEmail,
                   label: Strings.immersitionContactFormEmailHint,
                 ),
                 SizedBox(height: Margins.spacing_m),
                 ImmersionTextFormField(
                   isMandatory: true,
-                  initialValue: viewModel.userFirstNameInitialValue,
+                  mandatoryError: _isFirstNameValid() ? null : "Champ obligatoire",
+                  controller: _userFirstName,
                   label: Strings.immersitionContactFormSurnameHint,
                 ),
                 SizedBox(height: Margins.spacing_m),
                 ImmersionTextFormField(
                   isMandatory: true,
-                  initialValue: viewModel.userLastNameInitialValue,
+                  mandatoryError: _isLastNameValid() ? null : "Champ obligatoire",
+                  controller: _userLastName,
                   label: Strings.immersitionContactFormNameHint,
                 ),
                 SizedBox(height: Margins.spacing_m),
                 ImmersionTextFormField(
                   isMandatory: true,
+                  mandatoryError: _isMessageValid() ? null : "Champ obligatoire",
                   maxLines: 10,
-                  initialValue: viewModel.messageInitialValue,
+                  controller: _message,
                   label: Strings.immersitionContactFormMessageHint,
                 ),
                 SizedBox(height: Margins.spacing_huge * 2),
@@ -99,19 +162,19 @@ class _Content extends StatelessWidget {
 
 class ImmersionTextFormField extends StatelessWidget {
   final String label;
-  final String? initialValue;
   final bool isMandatory;
   final String? mandatoryError;
   final ValueChanged<String>? onChanged;
   final int maxLines;
+  final TextEditingController? controller;
 
   const ImmersionTextFormField({
-    this.initialValue,
     required this.isMandatory,
     this.mandatoryError,
     this.onChanged,
     required this.label,
     this.maxLines = 1,
+    this.controller,
   }) : super();
 
   @override
@@ -122,11 +185,11 @@ class ImmersionTextFormField extends StatelessWidget {
         Text("${isMandatory ? "*" : null}$label", style: TextStyles.textBaseMedium),
         SizedBox(height: Margins.spacing_base),
         TextFormField(
-          initialValue: initialValue,
           minLines: 1,
           maxLines: maxLines,
+          controller: controller,
           decoration: InputDecoration(
-              contentPadding: const EdgeInsets.all(16),
+              contentPadding: const EdgeInsets.all(Dimens.radius_base),
               errorText: mandatoryError,
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(Dimens.radius_base),
