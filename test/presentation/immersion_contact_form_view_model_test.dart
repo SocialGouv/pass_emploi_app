14794bd9861bd868758ebfd780799102d82870d1
Jordan Chapuy <chapuyj@gmail.com>
2023-03-28 17:50:05 +0200

    feat(1536): sending state tests

--- test/presentation/immersion_contact_form_view_model_test.dart
+++ test/presentation/immersion_contact_form_view_model_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pass_emploi_app/features/contact_immersion/contact_immersion_actions.dart';
+import 'package:pass_emploi_app/features/contact_immersion/contact_immersion_state.dart';
 import 'package:pass_emploi_app/presentation/immersion_contact_form_view_model.dart';
+import 'package:pass_emploi_app/presentation/sending_state.dart';
 
 import '../doubles/fixtures.dart';
 import '../dsl/app_state_dsl.dart';
@@ -17,6 +19,21 @@ void main() {
     expect(viewModel.userEmailInitialValue, "first.last@milo.fr");
     expect(viewModel.userFirstNameInitialValue, "F");
     expect(viewModel.userLastNameInitialValue, "L");
+  });
+
+  group("sending state", () {
+    void assertFoo(SendingState state, ContactImmersionState bbb) {
+      test("should be $state when state is $bbb", () {
+        final store = givenState().loggedInMiloUser().copyWith(contactImmersionState: bbb).store();
+        final viewModel = ImmersionContactFormViewModel.create(store);
+        expect(viewModel.sendingState, state);
+      });
+    }
+
+    assertFoo(SendingState.none, ContactImmersionNotInitializedState());
+    assertFoo(SendingState.loading, ContactImmersionLoadingState());
+    assertFoo(SendingState.success, ContactImmersionSuccessState());
+    assertFoo(SendingState.failure, ContactImmersionFailureState());
   });
 
   test('should send form', () {
