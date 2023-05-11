f7e4b7216353415d361ecc8be38d3ed9431cbc66
gabriel.adgeg <gad@octo.com>
2023-05-11 14:47:12 +0200

    fix(1604): Fix wrong parsing

--- lib/models/offre_type.dart
+++ lib/models/offre_type.dart
@@ -5,11 +5,10 @@ enum OffreType {
   serviceCivique;
 
   static OffreType? from(String value) {
-    if (value == "OFFRES_EMPLOI") return OffreType.emploi;
-    if (value == "OFFRES_ALTERNANCE") return OffreType.alternance;
-    if (value == "OFFRES_IMMERSION") return OffreType.immersion;
-    if (value == "OFFRES_SERVICES_CIVIQUE") return OffreType.serviceCivique;
+    if (["OFFRE_EMPLOI", "OFFRES_EMPLOI"].contains(value)) return OffreType.emploi;
+    if (["OFFRE_ALTERNANCE", "OFFRES_ALTERNANCE"].contains(value)) return OffreType.alternance;
+    if (["OFFRE_IMMERSION", "OFFRES_IMMERSION"].contains(value)) return OffreType.immersion;
+    if (["OFFRE_SERVICE_CIVIQUE", "OFFRES_SERVICES_CIVIQUE"].contains(value)) return OffreType.serviceCivique;
     return null;
   }
-
 }
