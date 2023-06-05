c472e6614de05f6d368fe1554dd6aa243c7e2596
Jordan Chapuy <chapuyj@gmail.com>
2023-06-05 11:59:52 +0200

    feat(1654): evenement emploi - details - vm tests

--- test/presentation/evenement_emploi_details_page_view_model_test.dart
+++ test/presentation/evenement_emploi_details_page_view_model_test.dart
@@ -1,3 +1,70 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pass_emploi_app/features/evenement_emploi_details/evenement_emploi_details_actions.dart';
+import 'package:pass_emploi_app/features/evenement_emploi_details/evenement_emploi_details_state.dart';
+import 'package:pass_emploi_app/presentation/display_state.dart';
+import 'package:pass_emploi_app/presentation/evenement_emploi_details_page_view_model.dart';
+
+import '../doubles/fixtures.dart';
+import '../doubles/spies.dart';
+import '../dsl/app_state_dsl.dart';
+
 void main() {
-  // TODO: Implement test
+  //TODO: manque un test sur les properties. Reviewer, je m'en occupe TKT :p
+
+  group('Display State', () {
+    test('when state is loading', () {
+      // Given
+      final store = givenState()
+          .loggedInUser() //
+          .copyWith(evenementEmploiDetailsState: EvenementEmploiDetailsLoadingState())
+          .store();
+
+      // When
+      final viewModel = EvenementEmploiDetailsPageViewModel.create(store);
+
+      // Then
+      expect(viewModel.displayState, DisplayState.LOADING);
+    });
+
+    test('when state is success', () {
+      // Given
+      final store = givenState()
+          .loggedInUser() //
+          .copyWith(evenementEmploiDetailsState: EvenementEmploiDetailsSuccessState(mockEvenementEmploiDetails()))
+          .store();
+
+      // When
+      final viewModel = EvenementEmploiDetailsPageViewModel.create(store);
+
+      // Then
+      expect(viewModel.displayState, DisplayState.CONTENT);
+    });
+
+    test('when state is failure', () {
+      // Given
+      final store = givenState()
+          .loggedInUser() //
+          .copyWith(evenementEmploiDetailsState: EvenementEmploiDetailsFailureState())
+          .store();
+
+      // When
+      final viewModel = EvenementEmploiDetailsPageViewModel.create(store);
+
+      // Then
+      expect(viewModel.displayState, DisplayState.FAILURE);
+    });
+  });
+
+  test('onRetry', () {
+    // Given
+    final store = StoreSpy();
+    final viewModel = EvenementEmploiDetailsPageViewModel.create(store);
+
+    // When
+    viewModel.retry("id");
+
+    // Then
+    expect(store.dispatchedAction is EvenementEmploiDetailsRequestAction, isTrue);
+    expect((store.dispatchedAction as EvenementEmploiDetailsRequestAction).eventId, "id");
+  });
 }
