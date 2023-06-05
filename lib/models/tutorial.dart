9b3e7611118472a16f66155009dc65b04741136c
Jordan Chapuy <chapuyj@gmail.com>
2023-06-05 10:11:29 +0200

    feat(1682): new tuto

--- lib/models/tutorial.dart
+++ lib/models/tutorial.dart
@@ -1,85 +1,34 @@
 import 'package:equatable/equatable.dart';
 
 class Tutorial extends Equatable {
-  static const String version = '9';
+  static const String version = '10';
 
   static const String _tuto = "assets/tuto/";
   static const String _svg = ".svg";
 
-  static String title1 = "Découvrez votre nouvelle page d'accueil !";
-  static String title2 = "Retrouvez le récapitulatif de votre semaine !";
-  static String title3 = "Consultez votre prochain rendez-vous";
-  static String title4 = "Retrouvez vos alertes plus facilement...";
-  static String title5 = "ainsi que vos favoris !";
-  static String description1 = "Swipez pour en savoir plus !";
-  static String descriptionPe2 = "Avec la synthèse de vos rendez-vous et démarches à réaliser sur la semaine";
-  static String descriptionMilo2 = "Avec la synthèse de vos rendez-vous et actions à réaliser sur la semaine";
-  static String description3 = "";
-  static String description4 =
-      "Vous avez sauvegardé des recherches d'offres ? Visualisez les dans la nouvelle section alerte";
-  static String description5 = "Retrouvez également vos offres mises en favoris";
+  static String title1 = "Vous avez des CV sur votre espace personnel Pôle emploi ?";
+  static String description1 =
+      "L’application du CEJ les récupérera automatiquement et vous pourrez les retrouver depuis votre profil";
 
-  static String svgPe1 = _tuto + "tuto_pe1" + _svg;
-  static String svgMilo1 = _tuto + "tuto_milo1" + _svg;
-  static String svgPe2 = _tuto + "tuto_pe2" + _svg;
-  static String svgMilo2 = _tuto + "tuto_milo2" + _svg;
-  static String svg3 = _tuto + "tuto3" + _svg;
-  static String svg4 = _tuto + "tuto4" + _svg;
-  static String svg5 = _tuto + "tuto5" + _svg;
+  static String title2 = "Et surtout… ";
+  static String description2 =
+      "Téléchargez votre CV sur votre téléphone afin de candidater plus facilement lorsque vous serez redirigé sur le site de l'offre";
 
-  static List<Tutorial> milo = [
-    Tutorial(
-      title: title1,
-      description: description1,
-      image: svgMilo1,
-    ),
-    Tutorial(
-      title: title2,
-      description: descriptionMilo2,
-      image: svgMilo2,
-    ),
-    Tutorial(
-      title: title3,
-      description: description3,
-      image: svg3,
-    ),
-    Tutorial(
-      title: title4,
-      description: description4,
-      image: svg4,
-    ),
-    Tutorial(
-      title: title5,
-      description: description5,
-      image: svg5,
-    ),
-  ];
+  static String svg1 = _tuto + "tuto1" + _svg;
+  static String svg2 = _tuto + "tuto2" + _svg;
+
+  static List<Tutorial> milo = [];
 
   static List<Tutorial> poleEmploi = [
     Tutorial(
       title: title1,
       description: description1,
-      image: svgPe1,
+      image: svg1,
     ),
     Tutorial(
       title: title2,
-      description: descriptionPe2,
-      image: svgPe2,
-    ),
-    Tutorial(
-      title: title3,
-      description: description3,
-      image: svg3,
-    ),
-    Tutorial(
-      title: title4,
-      description: description4,
-      image: svg4,
-    ),
-    Tutorial(
-      title: title5,
-      description: description5,
-      image: svg5,
+      description: description2,
+      image: svg2,
     ),
   ];
 
