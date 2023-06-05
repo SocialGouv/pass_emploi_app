cc9ddec964fcfc8d1794be95cdab99b1ddc6499b
adrien.cens <adrien.cens@edu.esiee.fr>
2023-06-05 18:06:35 +0200

    feat(1605): add suggestion  alerte location form

---
+++ lib/presentation/suggestions/suggestion_alerte_location_form_view_model.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+import 'package:pass_emploi_app/models/offre_type.dart';
+import 'package:pass_emploi_app/presentation/suggestions/suggestion_recherche_card_view_model.dart';
+import 'package:pass_emploi_app/ui/strings.dart';
+
+class SuggestionAlerteLocationFormViewModel extends Equatable {
+  SuggestionAlerteLocationFormViewModel({
+    required this.title,
+    required this.hint,
+    required this.villesOnly,
+  });
+
+  final String title;
+  final String hint;
+  final bool villesOnly;
+
+  factory SuggestionAlerteLocationFormViewModel.create(SuggestionRechercheCardViewModel baseViewModel) {
+    final type = baseViewModel.type;
+    return SuggestionAlerteLocationFormViewModel(
+      title: Strings.localisationFormTitle,
+      hint: _hint(type),
+      villesOnly: _villesOnly(type),
+    );
+  }
+
+  @override
+  List<Object?> get props => [title, hint, villesOnly];
+}
+
+String _hint(OffreType type) {
+  return switch (type) {
+    OffreType.emploi => Strings.localisationFormEmploiSubtitle,
+    OffreType.immersion => Strings.localisationFormImmersionSubtitle,
+    _ => throw Exception("Forbidden type $type"),
+  };
+}
+
+bool _villesOnly(OffreType type) {
+  return switch (type) {
+    OffreType.emploi => false,
+    OffreType.immersion => true,
+    _ => throw Exception("Forbidden type $type"),
+  };
+}
