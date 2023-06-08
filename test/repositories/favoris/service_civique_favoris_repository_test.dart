7f4bdbe47322024686f3a99fb6a7b2b976bf3839
Jordan Chapuy <chapuyj@gmail.com>
2023-06-08 16:29:28 +0200

    refactor(sut): ServiceCiviqueFavorisRepository

--- test/repositories/favoris/service_civique_favoris_repository_test.dart
+++ test/repositories/favoris/service_civique_favoris_repository_test.dart
@@ -1,171 +1,125 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
 import 'package:pass_emploi_app/models/service_civique.dart';
-import 'package:pass_emploi_app/network/json_utf8_decoder.dart';
 import 'package:pass_emploi_app/repositories/favoris/service_civique_favoris_repository.dart';
 
 import '../../doubles/dummies.dart';
-import '../../doubles/fixtures.dart';
-import '../../utils/pass_emploi_mock_client.dart';
-import '../../utils/test_assets.dart';
+import '../../dsl/sut_repository.dart';
 
 void main() {
-  test('getFavorisId when response is valid with all parameters should return offres', () async {
-    // Given
-    final httpClient = PassEmploiMockClient((request) async {
-      if (request.method != "GET") return invalidHttpResponse();
-      if (!request.url.toString().startsWith("BASE_URL/jeunes/jeuneId/favoris/services-civique")) {
-        return invalidHttpResponse();
-      }
-      return Response.bytes(loadTestAssetsAsBytes("service_civique_favoris_id.json"), 200);
+  final sut = RepositorySut<ServiceCiviqueFavorisRepository>();
+  sut.givenRepository((client) => ServiceCiviqueFavorisRepository("BASE_URL", client, DummyPassEmploiCacheManager()));
+
+  group("getFavorisId", () {
+    sut.when((repository) => repository.getFavorisId("jeuneId"));
+
+    group('when response is valid', () {
+      sut.givenJsonResponse(fromJson: "service_civique_favoris_id.json");
+
+      test('request should be valid', () async {
+        await sut.expectRequestBody(
+          method: "GET",
+          url: "BASE_URL/jeunes/jeuneId/favoris/services-civique",
+        );
+      });
+
+      test('response should be valid', () async {
+        await sut.expectResult<Set<String>?>((result) {
+          expect(result, [
+            "4f44d3ec-6568-41f0-b66e-e53a9e1fe904",
+            "4277d7b7-3d86-453d-9375-14aee8fde94d",
+            "b2d70bb3-ba69-4dd8-880a-62171b48ecbc",
+          ]);
+        });
+      });
     });
-    final repository = ServiceCiviqueFavorisRepository("BASE_URL", httpClient, DummyPassEmploiCacheManager());
 
-    // When
-    final favoris = await repository.getFavorisId("jeuneId");
+    group('when response is invalid', () {
+      sut.givenResponseCode(500);
 
-    // Then
-    expect(favoris, [
-      "4f44d3ec-6568-41f0-b66e-e53a9e1fe904",
-      "4277d7b7-3d86-453d-9375-14aee8fde94d",
-      "b2d70bb3-ba69-4dd8-880a-62171b48ecbc",
-    ]);
+      test('response should be null', () async {
+        await sut.expectNullResult();
+      });
+    });
   });
 
-  test('getFavorisId when response is invalid should return null', () async {
-    // Given
-    final httpClient = PassEmploiMockClient((request) async => invalidHttpResponse());
-    final repository = ServiceCiviqueFavorisRepository("BASE_URL", httpClient, DummyPassEmploiCacheManager());
+  group("postFavori", () {
+    sut.when((repository) => repository.postFavori("jeuneId", _offreWithFullData()));
 
-    // When
-    final favoris = await repository.getFavorisId("jeuneId");
+    group('when response is valid', () {
+      sut.givenResponseCode(200);
 
-    // Then
-    expect(favoris, isNull);
+      test('request should be valid', () async {
+        await sut.expectRequestBody(
+          method: "POST",
+          url: "BASE_URL/jeunes/jeuneId/favoris/services-civique",
+          jsonBody: {
+            'id': 'GOAT',
+            'titre': 'Cristiano Ronaldo',
+            'domaine': 'Footballeur',
+            'ville': 'Manchester',
+            'dateDeDebut': 'as soon as possible',
+            'organisation': 'United'
+          },
+        );
+      });
+
+      test('response should be valid', () async {
+        await sut.expectTrueAsResult();
+      });
+    });
+
+    group('when response is invalid', () {
+      sut.givenResponseCode(500);
+
+      test('response should be false', () async {
+        await sut.expectFalseAsResult();
+      });
+    });
+
+    group('when response is 409', () {
+      sut.givenResponseCode(409);
+
+      test('response should be true', () async {
+        await sut.expectTrueAsResult();
+      });
+    });
   });
 
-  test(
-      "postFavori when adding favori should post correct data when all fields are not null and return true when response is valid",
-      () async {
-    // Given
-    final httpClient = _mockClientForFullData();
+  group("deleteFavori", () {
+    sut.when((repository) => repository.deleteFavori("jeuneId", "offreId"));
 
-    final repository = ServiceCiviqueFavorisRepository("BASE_URL", httpClient, DummyPassEmploiCacheManager());
+    group('when response is valid', () {
+      sut.givenResponseCode(204);
 
-    // When
-    final result = await repository.postFavori("jeuneId", _offreWithFullData());
+      test('request should be valid', () async {
+        await sut.expectRequestBody(
+          method: "DELETE",
+          url: "BASE_URL/jeunes/jeuneId/favoris/services-civique/offreId",
+        );
+      });
 
-    // Then
-    expect(result, isTrue);
+      test('response should be valid', () async {
+        await sut.expectTrueAsResult();
+      });
+    });
+
+    group('when response is 404', () {
+      sut.givenResponseCode(404);
+
+      test('response should be true', () async {
+        await sut.expectTrueAsResult();
+      });
+    });
+
+    group('when response is invalid', () {
+      sut.givenResponseCode(500);
+
+      test('response should be null', () async {
+        await sut.expectFalseAsResult();
+      });
+    });
   });
 
-  test("deleteFavori when removing favori should delete with correct id and return true when response is valid",
-      () async {
-    // Given
-    final httpClient = _successfulClientForDelete();
-    final repository = ServiceCiviqueFavorisRepository("BASE_URL", httpClient, DummyPassEmploiCacheManager());
-
-    // When
-    final result = await repository.deleteFavori("jeuneId", "offreId");
-
-    // Then
-    expect(result, isTrue);
-  });
-
-  test("deleteFavori when removing favori should return true when response is a 404", () async {
-    // Given
-    final httpClient = _notFoundClient();
-    final repository = ServiceCiviqueFavorisRepository("BASE_URL", httpClient, DummyPassEmploiCacheManager());
-
-    // When
-    final result = await repository.deleteFavori("jeuneId", "offreId");
-
-    // Then
-    expect(result, true);
-  });
-
-  test("deleteFavori when removing favori should return false when response is invalid", () async {
-    // Given
-    final httpClient = _failureClient();
-    final repository = ServiceCiviqueFavorisRepository("BASE_URL", httpClient, DummyPassEmploiCacheManager());
-
-    // When
-    final result = await repository.deleteFavori("jeuneId", "offreId");
-
-    // Then
-    expect(result, isFalse);
-  });
-
-  test("postFavori when adding favori should return false when response is invalid", () async {
-    // Given
-    final httpClient = _failureClient();
-    final repository = ServiceCiviqueFavorisRepository("BASE_URL", httpClient, DummyPassEmploiCacheManager());
-
-    // When
-    final result = await repository.postFavori("jeuneId", _offreWithFullData());
-
-    // Then
-    expect(result, isFalse);
-  });
-
-  test("postFavori when adding favori should return true when response is 409", () async {
-    // Given
-    final httpClient = _alreadyExistsClient();
-    final repository = ServiceCiviqueFavorisRepository("BASE_URL", httpClient, DummyPassEmploiCacheManager());
-
-    // When
-    final result = await repository.postFavori("jeuneId", _offreWithFullData());
-
-    // Then
-    expect(result, isTrue);
-  });
-}
-
-BaseClient _failureClient() {
-  return PassEmploiMockClient((request) async {
-    return Response("", 500);
-  });
-}
-
-BaseClient _notFoundClient() {
-  return PassEmploiMockClient((request) async {
-    return Response("", 404);
-  });
-}
-
-BaseClient _alreadyExistsClient() {
-  return PassEmploiMockClient((request) async {
-    return Response("", 409);
-  });
-}
-
-BaseClient _successfulClientForDelete() {
-  return PassEmploiMockClient((request) async {
-    if (request.method != "DELETE") return invalidHttpResponse();
-    if (!request.url.toString().startsWith("BASE_URL/jeunes/jeuneId/favoris/services-civique/offreId")) {
-      return invalidHttpResponse();
-    }
-    return Response("", 204);
-  });
-}
-
-BaseClient _mockClientForFullData() {
-  return PassEmploiMockClient((request) async {
-    if (request.method != "POST") return invalidHttpResponse();
-    if (!request.url.toString().startsWith("BASE_URL/jeunes/jeuneId/favoris/services-civique")) {
-      return invalidHttpResponse();
-    }
-    final requestJson = jsonUtf8Decode(request.bodyBytes);
-    if (requestJson["id"] != "GOAT") {
-      return invalidHttpResponse(message: "idOffre KO");
-    }
-    if (requestJson["domaine"] != "Footballeur") return invalidHttpResponse(message: "metier KO");
-    if (requestJson["titre"] != "Cristiano Ronaldo") return invalidHttpResponse(message: "nomEtablissement KO");
-    if (requestJson["dateDeDebut"] != "as soon as possible") return invalidHttpResponse(message: "secteurActivite KO");
-    if (requestJson["ville"] != "Manchester") return invalidHttpResponse(message: "ville KO");
-    if (requestJson["organisation"] != "United") return invalidHttpResponse(message: "organisation KO");
-    return Response("", 201);
-  });
 }
 
 ServiceCivique _offreWithFullData() {
