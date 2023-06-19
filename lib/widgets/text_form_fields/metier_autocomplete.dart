b85e94c0c637ad2bb493a83b96af2f8d4d606ea4
Jordan Chapuy <chapuyj@gmail.com>
2023-06-19 15:11:04 +0200

    fix: lint [skip ci]

--- lib/widgets/text_form_fields/metier_autocomplete.dart
+++ lib/widgets/text_form_fields/metier_autocomplete.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:pass_emploi_app/analytics/analytics_constants.dart';
