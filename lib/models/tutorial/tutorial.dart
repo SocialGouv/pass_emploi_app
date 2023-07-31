34ddeaa082168be10f964d2548e1f20abc089976
Jordan Chapuy <chapuyj@gmail.com>
2023-07-31 16:05:49 +0200

    feat: new tuto

--- lib/models/tutorial/tutorial.dart
+++ lib/models/tutorial/tutorial.dart
@@ -1,21 +1,18 @@
 import 'package:pass_emploi_app/models/tutorial/tutorial_page.dart';
 
 class Tutorial {
-  static const String version = '1687509259';
-  static List<TutorialPage> milo = [
-    TutorialPage(
-      title: "Retrouvez les événements liés à l’emploi organisés autour de chez vous !",
-      description:
-          "Dans l'onglet \"Mes événements\", retrouvez désormais les différents types d'événements organisés au sein de votre Mission Locale ou en dehors de votre Mission Locale",
-      image: "assets/tuto/img_evenements_MiLo.svg",
-    ),
-  ];
+  static const String version = '1690812072';
+  static List<TutorialPage> milo = [];
   static List<TutorialPage> pe = [
     TutorialPage(
-      title: "Retrouvez les événements liés à l’emploi organisés autour de chez vous !",
-      description:
-          "Dans l'onglet \"Mes événements\", recherchez différents types d'événements organisés au sein de votre agence ou par des partenaires externes",
-      image: "assets/tuto/img_evenement_PE.svg",
+      title: "Renseignez plus facilement vos démarches grâce à la recherche par thématique",
+      description: "Dans l'onglet \"Démarches\", choisissez une démarche pré-définie parmi différentes thématiques",
+      image: "assets/tuto/c15a8676081d7a5eed9fcd2e0e40cf85bfb87010b7ee5557f7944d629a35efed.svg",
+    ),
+    TutorialPage(
+      title: "Renseignez plus facilement vos démarches grâce aux top démarches",
+      description: "Dans l'onglet \"Démarches\", choisissez une démarche pré-définie parmi les plus utilisées",
+      image: "assets/tuto/f0c9c2f4c6b3bf92989b89acd3694ab4a496b231bef3ba2e47fac72ac079f18b.svg",
     ),
   ];
 }
