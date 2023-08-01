f112fec6bb021c273f1e79973abefcaedc8c93c4
Jordan Chapuy <chapuyj@gmail.com>
2023-08-01 14:41:23 +0200

    test: safari web front

--- maestro/export-env.sh
+++ maestro/export-env.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 # Must be called with `source export-env.sh`
-export MAESTRO_LOGIN=TODO
-export MAESTRO_PASSWORD=TODO
+export MAESTRO_LOGIN_APP=TODO
+export MAESTRO_PASSWORD_APP=TODO
+export MAESTRO_LOGIN_WEB=TODO
+export MAESTRO_PASSWORD_WEB=TODO
