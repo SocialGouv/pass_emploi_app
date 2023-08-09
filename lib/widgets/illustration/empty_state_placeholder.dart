2a255010ea50e9137d1b8575f7ace61ed7bc5420
adrien.cens <adrien.cens@edu.esiee.fr>
2023-08-09 14:34:57 +0200

    feat(1855): add empty page list widget

---
+++ lib/widgets/illustration/empty_state_placeholder.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:pass_emploi_app/ui/margins.dart';
+import 'package:pass_emploi_app/ui/text_styles.dart';
+import 'package:pass_emploi_app/widgets/illustration/illustration.dart';
+
+class EmptyStatePlaceholder extends StatelessWidget {
+  const EmptyStatePlaceholder({
+    super.key,
+    required this.illustration,
+    required this.title,
+    required this.subtitle,
+  });
+  final Illustration illustration;
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(Margins.spacing_m),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ConstrainedBox(
+            constraints: BoxConstraints(maxHeight: 130, maxWidth: 130),
+            child: illustration,
+          ),
+          SizedBox(height: Margins.spacing_xl),
+          Text(
+            title,
+            style: TextStyles.textBaseBold,
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: Margins.spacing_base),
+          Text(
+            subtitle,
+            style: TextStyles.textBaseRegular,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
