e274a4406f8ec3a6a064105d9f8e1ec1aadb6d33
adrien.cens <adrien.cens@edu.esiee.fr>
2023-08-16 14:46:48 +0200

    feat(1853): replace illustration in user action details page

--- lib/ui/drawables.dart
+++ lib/ui/drawables.dart
@@ -17,7 +17,6 @@ class Drawables {
   static String modeDemoExplicationIllustration = _assets + "connexion" + _svg;
   static String unJeuneUneSolutionIllustration = _assets + "logo_1jeune1solution_light" + _svg;
   static String republiqueFrancaiseLogo = _assets + "logo_republique_francaise" + _svg;
-  static String congratulationsIllustration = _assets + "ic_congratulations" + _svg;
   static String trashAlertIllustration = _assets + "ic_trash_alert" + _svg;
   static String actionInacessibleIllustration = _assets + "action_inacessible" + _svg;
 
