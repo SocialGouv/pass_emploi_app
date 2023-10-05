715cd6c4a9aa29ffd8da05278e3a10b1bb7c32ab
gabriel.adgeg <gad@octo.com>
2023-10-05 16:01:27 +0200

    feat(tuto): Add new tutorial

--- lib/models/tutorial/tutorial.dart
+++ lib/models/tutorial/tutorial.dart
@@ -1,21 +1,21 @@
 import 'package:pass_emploi_app/models/tutorial/tutorial_page.dart';
 
 class Tutorial {
-  static const String version = '1687509259';
+  static const String versionTimestamp = '1696511133';
   static List<TutorialPage> milo = [
     TutorialPage(
-      title: "Retrouvez les événements liés à l’emploi organisés autour de chez vous !",
+      title: "Récupérez vos dernières informations à jour à la demande !",
       description:
-          "Dans l'onglet \"Mes événements\", retrouvez désormais les différents types d'événements organisés au sein de votre Mission Locale ou en dehors de votre Mission Locale",
-      image: "assets/tuto/img_evenements_MiLo.svg",
+          "En swippant vers le bas sur les écrans au format liste, vous aurez accès à vos dernières informations à jour.",
+      image: "assets/tuto/img_pull_Milo.svg",
     ),
   ];
   static List<TutorialPage> pe = [
     TutorialPage(
-      title: "Retrouvez les événements liés à l’emploi organisés autour de chez vous !",
+      title: "Récupérez vos dernières informations à jour à la demande !",
       description:
-          "Dans l'onglet \"Mes événements\", recherchez différents types d'événements organisés au sein de votre agence ou par des partenaires externes",
-      image: "assets/tuto/img_evenement_PE.svg",
+          "En swippant vers le bas sur les écrans au format liste, vous aurez accès à vos dernières informations à jour.",
+      image: "assets/tuto/img_pull_PE.svg",
     ),
   ];
 }
