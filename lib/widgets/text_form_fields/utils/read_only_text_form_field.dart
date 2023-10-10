caa933c4f9f49a1ceee5a4046feece5b868e3fef
adrien.cens <adrien.cens@edu.esiee.fr>
2023-10-10 17:01:34 +0200

    feat(2008): handle enter on place textfield

--- lib/widgets/text_form_fields/utils/read_only_text_form_field.dart
+++ lib/widgets/text_form_fields/utils/read_only_text_form_field.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pass_emploi_app/ui/app_colors.dart';
 import 'package:pass_emploi_app/ui/margins.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
 import 'package:pass_emploi_app/ui/text_styles.dart';
 import 'package:pass_emploi_app/widgets/text_form_fields/utils/text_form_field_input_decoration.dart';
 
-class ReadOnlyTextFormField extends StatelessWidget {
+class ReadOnlyTextFormField extends StatefulWidget {
   final String title;
   final String heroTag;
   final Key textFormFieldKey;
@@ -28,33 +29,53 @@ class ReadOnlyTextFormField extends StatelessWidget {
   });
 
   @override
+  State<ReadOnlyTextFormField> createState() => _ReadOnlyTextFormFieldState();
+}
+
+class _ReadOnlyTextFormFieldState extends State<ReadOnlyTextFormField> {
+  late final FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode(onKeyEvent: (node, event) {
+      if (event is KeyDownEvent && event.logicalKey == LogicalKeyboardKey.enter) {
+        widget.onTextTap();
+        return KeyEventResult.handled;
+      }
+      return KeyEventResult.ignored;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(title, style: TextStyles.textBaseBold),
-        if (hint != null) Text(hint!, style: TextStyles.textSRegularWithColor(AppColors.contentColor)),
+        Text(widget.title, style: TextStyles.textBaseBold),
+        if (widget.hint != null) Text(widget.hint!, style: TextStyles.textSRegularWithColor(AppColors.contentColor)),
         SizedBox(height: Margins.spacing_base),
         Stack(
           alignment: Alignment.centerRight,
           children: [
             Hero(
-              tag: heroTag,
+              tag: widget.heroTag,
               child: Material(
                 type: MaterialType.transparency,
                 child: TextFormField(
-                  key: textFormFieldKey,
+                  focusNode: _focusNode,
+                  key: widget.textFormFieldKey,
                   style: TextStyles.textBaseBold,
                   decoration: TextFormFieldInputDecoration(),
                   readOnly: true,
-                  initialValue: initialValue,
-                  onTap: onTextTap,
+                  initialValue: widget.initialValue,
+                  onTap: widget.onTextTap,
                 ),
               ),
             ),
-            if (withDeleteButton)
+            if (widget.withDeleteButton)
               IconButton(
-                onPressed: onDeleteTap,
+                onPressed: widget.onDeleteTap,
                 tooltip: Strings.suppressionLabel,
                 icon: const Icon(Icons.close),
               ),
