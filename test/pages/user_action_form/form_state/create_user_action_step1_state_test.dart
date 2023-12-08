b437e1a85410d7d1903e82e6882dfe0f4bbd07f0
adrien.cens <adrien.cens@edu.esiee.fr>
2023-12-08 20:56:46 +0100

    feat(2147): step 1 tests

---
+++ test/pages/user_action_form/form_state/create_user_action_step1_state_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pass_emploi_app/pages/user_action_form/create_user_action_form_step1_page.dart';
+import 'package:pass_emploi_app/pages/user_action_form/form_state/create_user_action_form_state.dart';
+
+void main() {
+  group('CreateUserActionStep1State', () {
+    test('initial state should be invalid', () {
+      // Given
+      final state = CreateUserActionStep1State();
+
+      // When & Then
+      expect(state.isValid, false);
+    });
+
+    test('should be valid when category is not null', () {
+      // Given
+      final state = CreateUserActionStep1State(actionCategory: UserActionReferentielType.cultureSportLoisirs);
+
+      // When & Then
+      expect(state.isValid, true);
+    });
+  });
+}
