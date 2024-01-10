9406385e09337f60624250733632b3a9924527fa
adrien.cens <adrien.cens@edu.esiee.fr>
2024-01-10 12:07:10 +0100

    feat(tuto): tuto actions

--- lib/models/tutorial/tutorial.dart
+++ lib/models/tutorial/tutorial.dart
@@ -1,21 +1,28 @@
 import 'package:pass_emploi_app/models/tutorial/tutorial_page.dart';
 
 class Tutorial {
-  static const String versionTimestamp = '1696511133';
+  static const String versionTimestamp = '1696511134';
   static List<TutorialPage> milo = [
     TutorialPage(
-      title: "Récupérez vos dernières informations à jour à la demande !",
-      description:
-          "En swippant vers le bas sur les écrans au format liste, vous aurez accès à vos dernières informations à jour.",
-      image: "assets/tuto/img_pull_Milo.svg",
+      title: "Le parcours de création des actions fait peau neuve !",
+      description: "Choississez dans un premier temps la catégorie de votre action",
+      image: "assets/tuto/img_creation_categorie.svg",
     ),
-  ];
-  static List<TutorialPage> pe = [
     TutorialPage(
-      title: "Récupérez vos dernières informations à jour à la demande !",
-      description:
-          "En swippant vers le bas sur les écrans au format liste, vous aurez accès à vos dernières informations à jour.",
-      image: "assets/tuto/img_pull_PE.svg",
+      title: "Le parcours de création des actions fait peau neuve !",
+      description: "Puis le nom de votre action grâce aux suggestions d’activité qui vous sont proposées",
+      image: "assets/tuto/img_creation_titre.svg",
+    ),
+    TutorialPage(
+      title: "Le parcours de création des actions fait peau neuve !",
+      description: "Enfin, le statut et la date de réalisation de votre action",
+      image: "assets/tuto/img_creation_date.svg",
+    ),
+    TutorialPage(
+      title: "Le parcours de création des actions fait peau neuve !",
+      description: "Et hop, c’est terminé ! Maintenant, à vous de jouer !",
+      image: "assets/tuto/img_creation_fin.svg",
     ),
   ];
+  static List<TutorialPage> pe = [];
 }
