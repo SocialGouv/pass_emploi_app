7c5c6a5bd4fddef41420adac7053e4016e68cc92
gabriel.adgeg <gad@octo.com>
2024-01-16 15:27:20 +0100

    feat(2239): Create new mon_suivi_milo_page.dart

---
+++ lib/pages/mon_suivi_milo_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:pass_emploi_app/ui/app_colors.dart';
+import 'package:pass_emploi_app/ui/strings.dart';
+import 'package:pass_emploi_app/widgets/connectivity_widgets.dart';
+import 'package:pass_emploi_app/widgets/default_app_bar.dart';
+
+class MonSuiviMiloPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.grey100,
+      appBar: PrimaryAppBar(title: Strings.monSuiviAppBarTitle),
+      body: ConnectivityContainer(child: SizedBox.shrink()),
+    );
+  }
+}
