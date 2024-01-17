63940ec136111df8027cd905f0d28ec9343591c0
adrien.cens <adrien.cens@edu.esiee.fr>
2024-01-17 15:56:11 +0100

    feat(cvm_android): decorelate events from methods, decorelate init and start, and return a list of messages, and add stop method

--- android/app/src/main/kotlin/fr/fabrique/social/gouv/pass_emploi_app/EventChannelHandler.kt
+++ android/app/src/main/kotlin/fr/fabrique/social/gouv/pass_emploi_app/EventChannelHandler.kt
@@ -2,11 +2,10 @@ package fr.fabrique.social.gouv.pass_emploi_app
 
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
-import io.flutter.plugin.common.MethodChannel
 
 class EventChannelHandler(
-    private val flutterEngine: FlutterEngine,
-    private val cvmRepository: CvmRepository
+        private val flutterEngine: FlutterEngine,
+        private val cvmRepository: CvmRepository
 ) : EventChannel.StreamHandler {
 
     companion object {
@@ -18,6 +17,7 @@ class EventChannelHandler(
     fun initializeEventChannel() {
         val eventChannel = EventChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
         eventChannel.setStreamHandler(this)
+        cvmRepository.setCallback { messages -> eventsSink?.success(messages) }
     }
 
     override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
@@ -27,14 +27,5 @@ class EventChannelHandler(
     override fun onCancel(arguments: Any?) {
         cvmRepository.stopListenMessage()
         eventsSink = null
-    }
-
-    fun startListeningMessages() {
-        cvmRepository.listenMessage { messages ->
-            //TODO: passer en liste
-            messages.forEach { message ->
-                eventsSink?.success(message)
-            }
-        }
     }
 }
