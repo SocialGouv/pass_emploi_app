9b63ce2418f9ae6a6834df21c5e1537ed86b209d
Jordan Chapuy <chapuyj@gmail.com>
2024-01-18 17:45:03 +0100

    feat(cvm-ios): refactoring and handling errors

--- ios/Runner/CVM/CvmRepository.swift
+++ ios/Runner/CVM/CvmRepository.swift
@@ -10,6 +10,8 @@ import BenedicteSDK
 
 typealias EventJson = Dictionary<String, Any>
 typealias EventsReceived = ([EventJson]) -> Void
+typealias SuccessCompletion = (Bool) -> Void
+typealias VoidCompletion = () -> Void
 
 class CvmRepository {
     
@@ -21,52 +23,46 @@ class CvmRepository {
     }
     
     func setMessageCallback(_ onMessages: @escaping EventsReceived) {
-        print("#CVM CvmRepository.setMessageCallback")
         self.onMessages = onMessages
     }
     
-    func startListenMessages(token: String, ex160Url: String) {
-        //TODO: refactoring
-        print("#CVM CvmRepository.startListenMessage")
-        MatrixManager.sharedInstance.loginAndStartSession(accessToken: token, oauthServer: ex160Url) { success in
-            print("#CVM CvmRepository.startListenMessage login ? \(success)")
+    func startListenMessages(token: String, ex160Url: String, completion: @escaping SuccessCompletion) {
+        loginThenListenEvents(token: token, ex160Url: ex160Url, completion: completion)
+    }
+
+    private func loginThenListenEvents(token: String, ex160Url: String, completion: @escaping SuccessCompletion) {
+        MatrixManager.sharedInstance.loginAndStartSession(accessToken: token, oauthServer: ex160Url) { [weak self] success in
             if (success) {
-                MatrixManager.sharedInstance.joinFirstRoom { [weak self] room in
-                    print("#CVM CvmRepository.startListenMessage room ? \(room)")
-                    self?.room = room
-                    if let room = room {
-                        MatrixManager.sharedInstance.startMessageListener(room: room) { [weak self] events in
-                            print("#CVM CvmRepository.startListenMessage events callback - count(\(events.count) - \(events)")
-                            
-                            //TODO: gestion erreurs - json sans toutes les valeurs
-                            let eventsJson = events.compactMap { event in
-                                [
-                                    "id": event.eventId ?? "no-id",
-                                    "isFromUser": event.senderID == SessionManager.sharedInstance.userId,
-                                    "content": event.message ?? "no-message",
-                                    "date": Int64((event.date?.timeIntervalSince1970 ?? Date().timeIntervalSince1970) * 1000),
-                                    //TODO: d'autres champs ?
-                                    // "readBy": event.readBy,
-                                    // eventType
-                                ]
-                            }
-                            
-                            self?.onMessages?(eventsJson)
-                        }
-                    } else {
-                        //TODO: gestion erreurs - pas de room
-                        //TODO: listen room ?
-                    }
-                    
-                }
+                self?.joinRoomThenListenEvents(completion: completion)
             } else {
-                //TODO: gestion erreurs - login échec
+                completion(false)
+            }
+        }
+    }
+
+    private func joinRoomThenListenEvents(completion: @escaping SuccessCompletion) {
+        MatrixManager.sharedInstance.joinFirstRoom { [weak self] room in
+            self?.room = room
+            if let room = room {
+                self?.startListenEvents(room: room, completion: completion)
+            } else {
+                completion(false)
+                //TODO: listen room ? c'est peut-être plus complexe que ça
+                // peut-être qu'on veut retourner une erreur, et que c'est au client Flutter de dire
+                // "ok on commence à listen room"
+                // ou peut-être qu'on veut même dire que re-join à chaque fois qu'on affiche la page ?
             }
         }
     }
     
+    private func startListenEvents(room: Room, completion: @escaping SuccessCompletion) {
+        MatrixManager.sharedInstance.startMessageListener(room: room) { [weak self] events in
+            self?.onMessages?(events.map { $0.toJson() })
+        }
+        completion(true)
+    }
+    
     func stopListenMessages() {
-        //TODO: MatrixManager.sharedInstance.stopRoomListener() ?
         if let room = room {
             MatrixManager.sharedInstance.stopMessageListener(room: room)
         }
@@ -74,22 +70,34 @@ class CvmRepository {
         self.room = nil
     }
     
-    func sendMessage(_ message: String) {
-        print("#CVM CvmRepository.sendMessage \(message)")
-
-        guard let room = self.room else { return }
-        MatrixManager.sharedInstance.sendMessage(room: room, message: message) { success in
-            print("#CVM CvmRepository.sendMessage success ? \(success)")
-            //TODO: erreur
+    func sendMessage(_ message: String, completion: @escaping SuccessCompletion) {
+        guard let room = self.room else {
+            completion(false)
+            return
         }
+        MatrixManager.sharedInstance.sendMessage(room: room, message: message, completion: completion)
     }
     
-    func loadMore() {
-        print("#CVM CvmRepository.loadMore")
-        guard let room = self.room else { return }
-        
-        MatrixManager.sharedInstance.loadMoreMessage(room: room, withPaginationSize: 5) {
-            print("#CVM CvmRepository.loadMore completion")
+    func loadMore(completion: @escaping VoidCompletion) {
+        guard let room = self.room else {
+            completion()
+            return
         }
+        
+        MatrixManager.sharedInstance.loadMoreMessage(room: room, withPaginationSize: 5, completion: completion)
+    }
+}
+
+private extension Event {
+    func toJson() -> EventJson {
+        return [
+            "id": eventId ?? "no-id",
+            "isFromUser": senderID == SessionManager.sharedInstance.userId,
+            "content": message ?? "no-message",
+            "date": Int64((date?.timeIntervalSince1970 ?? Date().timeIntervalSince1970) * 1000),
+            //TODO: d'autres champs ?
+            // "readBy": event.readBy,
+            // eventType
+        ]
     }
 }
