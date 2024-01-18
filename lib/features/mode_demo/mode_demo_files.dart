0a19d5f8bbed7e2b11000830e7ddc760ac79997e
gabriel.adgeg <gad@octo.com>
2024-01-18 16:49:04 +0100

    fix(2239): Fix mode demo

--- lib/features/mode_demo/mode_demo_files.dart
+++ lib/features/mode_demo/mode_demo_files.dart
@@ -1,3 +1,5 @@
+import 'package:pass_emploi_app/models/date/interval.dart';
+
 String? getDemoFileName(String url, String query) {
   if (url.contains('/milo/accueil')) return 'accueil_mission_locale';
   if (url.contains('/pole-emploi/accueil')) return 'accueil_pole_emploi';
@@ -19,7 +21,8 @@ String? getDemoFileName(String url, String query) {
   if (url.endsWith('/referentiels/pole-emploi/types-demarches')) return 'referentiel_demarches';
   if (url.endsWith('/commentaires')) return 'commentaires';
   if (url.endsWith('/favoris')) return 'favoris';
-  if (url.endsWith('/mon-suivi')) return 'mon_suivi_mission_locale';
+  if (url.endsWith('/mon-suivi') && _queryIntervalContainsNow(query)) return 'mon_suivi_mission_locale_maintenant';
+  if (url.endsWith('/mon-suivi')) return 'mon_suivi_mission_locale_autre_periode';
   if (url.contains('recherches/suggestions')) return 'suggestions_recherche';
   if (url.removeLastPath().endsWith('/services-civique')) return 'service_civique_detail';
   if (url.removeLastPath().endsWith('/offres-immersion')) return 'immersion_detail';
@@ -37,6 +40,14 @@ String? getDemoFileName(String url, String query) {
   if (url.contains('/sessions/')) return 'session_milo_details';
 
   return null;
+}
+
+bool _queryIntervalContainsNow(String query) {
+  final interval = Interval(
+    DateTime.parse(query.split('debut=')[1].substring(0, 10)),
+    DateTime.parse(query.split('fin=')[1].substring(0, 10)),
+  );
+  return interval.contains(DateTime.now());
 }
 
 extension UrlExtensions on String {
