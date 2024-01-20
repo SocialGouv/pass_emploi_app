f04b467a271edbd10154e5f9565336ded3745c2f
gabriel.adgeg <gad@octo.com>
2024-01-20 23:56:40 +0100

    feat(2239): Add proper shimmer loader

--- lib/pages/agenda_page.dart
+++ lib/pages/agenda_page.dart
@@ -440,8 +440,6 @@ class _DemarcheAgendaItem extends StatelessWidget {
 }
 
 class _AgendaLoading extends StatelessWidget {
-  const _AgendaLoading();
-
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
