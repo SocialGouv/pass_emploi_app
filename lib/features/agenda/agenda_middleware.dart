285b979a25460a1a81f4eee7668ed181b6adc582
gabriel.adgeg <gad@octo.com>
2024-02-13 11:49:26 +0100

    fix: Remove wrong and useless calls to GET/ Pole emploi agenda for Milo user creating user actions

--- lib/features/agenda/agenda_middleware.dart
+++ lib/features/agenda/agenda_middleware.dart
@@ -1,6 +1,4 @@
 import 'package:pass_emploi_app/features/agenda/agenda_actions.dart';
-import 'package:pass_emploi_app/features/user_action/create/pending/user_action_create_pending_actions.dart';
-import 'package:pass_emploi_app/features/user_action/create/user_action_create_actions.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
 import 'package:pass_emploi_app/repositories/agenda_repository.dart';
 import 'package:redux/redux.dart';
@@ -12,15 +10,12 @@ class AgendaMiddleware extends MiddlewareClass<AppState> {
 
   @override
   void call(Store<AppState> store, action, NextDispatcher next) async {
-    // Need to be done before the action is dispatched to the reducer
-    final int currentPendingActionsCount = store.state.userActionCreatePendingState.getPendingCreationsCount();
-
     next(action);
 
     final user = store.state.user();
     if (user == null) return;
 
-    if (_needFetchingAgenda(currentPendingActionsCount, action)) {
+    if (action is AgendaRequestAction || action is AgendaRequestReloadAction) {
       final DateTime maintenant = _getMaintenant(action);
       final agenda = await _repository.getAgendaPoleEmploi(user.id, maintenant);
       store.dispatch(agenda != null ? AgendaRequestSuccessAction(agenda) : AgendaRequestFailureAction());
@@ -32,16 +27,4 @@ DateTime _getMaintenant(dynamic action) {
   return (action is AgendaRequestAction || action is AgendaRequestReloadAction)
       ? action.maintenant as DateTime
       : DateTime.now();
-}
-
-bool _needFetchingAgenda(int currentPendingActionsCount, dynamic action) {
-  return action is UserActionCreateSuccessAction ||
-      action is AgendaRequestAction ||
-      action is AgendaRequestReloadAction ||
-      _newUserActionsCreated(currentPendingActionsCount, action);
-}
-
-bool _newUserActionsCreated(int currentPendingActionsCount, dynamic action) {
-  if (action is! UserActionCreatePendingAction) return false;
-  return action.pendingCreationsCount < currentPendingActionsCount;
 }
