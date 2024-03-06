f80df635d6846632ab11bb987eb206713d828428
gabriel.adgeg <gad@octo.com>
2024-03-06 16:27:32 +0100

    feat(cvm): Add proper design to cvm_chat_page.dart

---
+++ lib/presentation/chat/cvm_chat_item.dart
@@ -0,0 +1,72 @@
+import 'package:equatable/equatable.dart';
+import 'package:pass_emploi_app/models/chat/sender.dart';
+
+sealed class CvmChatItem extends Equatable {
+  final String messageId;
+
+  CvmChatItem(this.messageId);
+
+  @override
+  List<Object?> get props => [messageId];
+}
+
+class DayItem extends CvmChatItem {
+  final String dayLabel;
+
+  DayItem(this.dayLabel) : super('');
+
+  @override
+  List<Object?> get props => [messageId, dayLabel];
+}
+
+class InformationItem extends CvmChatItem {
+  final String title;
+  final String description;
+
+  InformationItem(this.title, this.description) : super('');
+
+  @override
+  List<Object?> get props => [messageId, title, description];
+}
+
+sealed class MessageItem extends CvmChatItem {
+  final String content;
+  final String caption;
+
+  MessageItem(super.messageId, this.content, this.caption);
+
+  @override
+  List<Object?> get props => [
+        messageId,
+        content,
+        caption,
+      ];
+}
+
+class TextMessageItem extends MessageItem {
+  final Sender sender;
+
+  TextMessageItem({
+    required String messageId,
+    required String content,
+    required String caption,
+    required this.sender,
+  }) : super(messageId, content, caption);
+
+  @override
+  List<Object?> get props => [messageId, content, caption, sender];
+}
+
+class PieceJointeConseillerMessageItem extends MessageItem {
+  final String attachmentUrl;
+
+  PieceJointeConseillerMessageItem({
+    required String messageId,
+    required String content,
+    required String caption,
+    required this.attachmentUrl,
+  }) : super(messageId, content, caption);
+
+  @override
+  List<Object?> get props => [messageId, content, caption, attachmentUrl];
+}
