7649396e344cdaac66f85443561590c6525280a9
gabriel.adgeg <gad@octo.com>
2024-03-06 16:27:32 +0100

    feat(cvm): Add proper design to cvm_chat_page.dart

---
+++ lib/widgets/chat/cvm/cvm_chat_text_message.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:pass_emploi_app/models/chat/sender.dart';
+import 'package:pass_emploi_app/presentation/chat/cvm_chat_item.dart';
+import 'package:pass_emploi_app/ui/text_styles.dart';
+import 'package:pass_emploi_app/widgets/chat/chat_message_container.dart';
+import 'package:pass_emploi_app/widgets/text_with_clickable_links.dart';
+
+class CvmChatTextMessage extends StatelessWidget {
+  final TextMessageItem item;
+
+  const CvmChatTextMessage(this.item) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isMyMessage = item.sender == Sender.jeune;
+    final TextStyle textStyle = isMyMessage ? TextStyles.textSRegular(color: Colors.white) : TextStyles.textSRegular();
+    return ChatMessageContainer(
+      content: SelectableTextWithClickableLinks(
+        item.content,
+        linkStyle: textStyle,
+        style: textStyle,
+      ),
+      isMyMessage: isMyMessage,
+      caption: item.caption,
+      captionColor: null,
+    );
+  }
+}
