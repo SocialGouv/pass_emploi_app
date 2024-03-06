a0f90db726cc496f201e6dd6327aa06b8a1bb4c3
adrien.cens <adrien.cens@edu.esiee.fr>
2024-03-06 11:27:37 +0100

    fix(notifications_request): brsa

--- lib/features/notifications_permissions/notifications_permissions_middleware.dart
+++ lib/features/notifications_permissions/notifications_permissions_middleware.dart
@@ -1,4 +1,6 @@
+import 'package:pass_emploi_app/features/bootstrap/bootstrap_action.dart';
 import 'package:pass_emploi_app/features/notifications_permissions/notifications_permissions_actions.dart';
+import 'package:pass_emploi_app/models/brand.dart';
 import 'package:pass_emploi_app/push/push_notification_manager.dart';
 import 'package:pass_emploi_app/redux/app_state.dart';
 import 'package:redux/redux.dart';
@@ -10,7 +12,7 @@ class NotificationsPermissionsMiddleware extends MiddlewareClass<AppState> {
 
   @override
   void call(Store<AppState> store, action, NextDispatcher next) async {
-    if (action is NotificationsPermissionsRequestAction) {
+    if (action is NotificationsPermissionsRequestAction || (Brand.isBrsa() && action is BootstrapAction)) {
       _repository.requestPermission();
     }
   }
