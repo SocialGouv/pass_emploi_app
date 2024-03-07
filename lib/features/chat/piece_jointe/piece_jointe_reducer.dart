4e42a9c7033495543237932f0502028020495443
gabriel.adgeg <gad@octo.com>
2024-03-07 14:31:05 +0100

    refactor(cvm): Rename actions

--- lib/features/chat/piece_jointe/piece_jointe_reducer.dart
+++ lib/features/chat/piece_jointe/piece_jointe_reducer.dart
@@ -2,8 +2,8 @@ import 'package:pass_emploi_app/features/chat/piece_jointe/piece_jointe_actions.
 import 'package:pass_emploi_app/features/chat/piece_jointe/piece_jointe_state.dart';
 
 PiecesJointesState pieceJointeReducer(PiecesJointesState current, dynamic action) {
-  if (action is PieceJointeTypeIdRequestAction) return current.updated(action.fileId, PieceJointeStatus.loading);
-  if (action is PieceJointeTypeUrlRequestAction) return current.updated(action.fileId, PieceJointeStatus.loading);
+  if (action is PieceJointeFromIdRequestAction) return current.updated(action.fileId, PieceJointeStatus.loading);
+  if (action is PieceJointeFromUrlRequestAction) return current.updated(action.fileId, PieceJointeStatus.loading);
   if (action is PieceJointeFailureAction) return current.updated(action.fileId, PieceJointeStatus.failure);
   if (action is PieceJointeUnavailableAction) return current.updated(action.fileId, PieceJointeStatus.unavailable);
   if (action is PieceJointeSuccessAction) return current.updated(action.fileId, PieceJointeStatus.success);
