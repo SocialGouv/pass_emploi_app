5bac9f5d5e33d18a88b13ff969ea85d1630d9547
gabriel.adgeg <gad@octo.com>
2024-03-18 15:23:31 +0100

    feat(onboarding): Use PageView, and adapt UI to small devices

--- lib/pages/first_lauch_onboarding_page.dart
+++ lib/pages/first_lauch_onboarding_page.dart
@@ -1,77 +1,106 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
 import 'package:pass_emploi_app/features/first_launch_onboarding/first_launch_onboarding_actions.dart';
-import 'package:pass_emploi_app/redux/app_state.dart';
 import 'package:pass_emploi_app/ui/animation_durations.dart';
 import 'package:pass_emploi_app/ui/app_colors.dart';
 import 'package:pass_emploi_app/ui/app_icons.dart';
 import 'package:pass_emploi_app/ui/margins.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
 import 'package:pass_emploi_app/ui/text_styles.dart';
+import 'package:pass_emploi_app/utils/context_extensions.dart';
 import 'package:pass_emploi_app/widgets/buttons/primary_action_button.dart';
 import 'package:pass_emploi_app/widgets/buttons/secondary_button.dart';
 import 'package:pass_emploi_app/widgets/cards/generic/card_container.dart';
 import 'package:pass_emploi_app/widgets/drawables/app_logo.dart';
 import 'package:pass_emploi_app/widgets/entree_biseau_background.dart';
 
-class FirstLaunchOnboardingPage extends StatelessWidget {
-  const FirstLaunchOnboardingPage({super.key});
+class FirstLaunchOnboardingPage extends StatefulWidget {
+  static const int _mediumHeight = 700;
+
+  @override
+  State<FirstLaunchOnboardingPage> createState() => _FirstLaunchOnboardingPageState();
+}
+
+class _FirstLaunchOnboardingPageState extends State<FirstLaunchOnboardingPage> {
+  bool _firstScreen = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return _firstScreen ? _FirstScreen(onStart: () => setState(() => _firstScreen = false)) : _PageViewScreen();
+  }
+}
+
+class _FirstScreen extends StatelessWidget {
+  final VoidCallback onStart;
+
+  const _FirstScreen({required this.onStart});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: const <Widget>[
+        children: [
           EntreeBiseauBackground(),
-          _Content(),
+          Align(
+            alignment: Alignment.center,
+            child: _Welcome(firstScreen: true),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(Margins.spacing_xl),
+              child: SizedBox(
+                width: double.infinity,
+                child: SecondaryButton(label: Strings.start, onPressed: onStart),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-class _Content extends StatefulWidget {
-  const _Content();
-
-  @override
-  State<_Content> createState() => _ContentState();
-}
-
-class _ContentState extends State<_Content> {
-  int _index = 0;
-
-  void incrementIndex() {
-    setState(() {
-      _index++;
-    });
-  }
+class _PageViewScreen extends StatelessWidget {
+  final _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: Margins.spacing_m),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+    final smallDevice = MediaQuery.of(context).size.height < FirstLaunchOnboardingPage._mediumHeight;
+    return Scaffold(
+      body: Stack(
         children: [
-          Expanded(child: SizedBox()),
-          Expanded(
-            flex: 2,
-            child: Center(child: _Welcome(firstScreen: _index == 0)),
+          EntreeBiseauBackground(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                flex: smallDevice ? 0 : 13,
+                child: SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: smallDevice ? 15 : 37,
+                child: Center(child: _Welcome(firstScreen: false)),
+              ),
+              Expanded(
+                flex: 40,
+                child: PageView(
+                  controller: _pageController,
+                  children: [
+                    _FirstPageContent(onContinue: () => _pageController.next()),
+                    _SecondPageContent(onContinue: () => _pageController.next()),
+                    _ThirdPageContent(onContinue: () => context.dispatch(FirstLaunchOnboardingFinishAction())),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: smallDevice ? 5 : 10,
+                child: Center(
+                  child: _CarouselStepperIndicator(_pageController),
+                ),
+              ),
+            ],
           ),
-          AnimatedSwitcher(
-            duration: AnimationDurations.medium,
-            child: switch (_index) {
-              1 => _FirstPageContent(onContinue: incrementIndex),
-              2 => _SecondPageContent(onContinue: incrementIndex),
-              3 => _ThirdPageContent(
-                  onContinue: () => StoreProvider.of<AppState>(context).dispatch(FirstLaunchOnboardingFinishAction())),
-              _ => _InitialPageContent(onStart: incrementIndex),
-            },
-          ),
-          SizedBox(height: Margins.spacing_base),
-          if (_index == 0) SizedBox(height: Margins.spacing_s),
-          if (_index != 0) _CarouselStepperIndicator(index: _index - 1, length: 3),
-          SizedBox(height: MediaQuery.of(context).padding.bottom + Margins.spacing_base),
         ],
       ),
     );
@@ -85,50 +114,80 @@ class _Welcome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bool withText = firstScreen || MediaQuery.of(context).size.height > 700;
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        AppLogo(width: 120),
-        const SizedBox(height: Margins.spacing_base),
-        if (withText) ...[
-          Text(Strings.welcome, style: TextStyles.textLBold(color: Colors.white), textAlign: TextAlign.center),
-          SizedBox(height: Margins.spacing_base),
-          Text(Strings.welcomeMessage, style: TextStyles.textSMedium(color: Colors.white), textAlign: TextAlign.center),
-          SizedBox(height: Margins.spacing_m),
+    final bool withText = firstScreen || MediaQuery.of(context).size.height > FirstLaunchOnboardingPage._mediumHeight;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AppLogo(width: 120),
+          if (withText) ...[
+            const SizedBox(height: Margins.spacing_base),
+            Text(Strings.welcome, style: TextStyles.textLBold(color: Colors.white), textAlign: TextAlign.center),
+            SizedBox(height: Margins.spacing_base),
+            Text(
+              Strings.welcomeMessage,
+              style: TextStyles.textBaseRegular.copyWith(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: Margins.spacing_m),
+          ],
         ],
-      ],
+      ),
     );
   }
 }
 
-class _CarouselStepperIndicator extends StatelessWidget {
-  const _CarouselStepperIndicator({required this.index, required this.length});
+class _FirstPageContent extends StatelessWidget {
+  const _FirstPageContent({required this.onContinue});
 
-  final int index;
-  final int length;
+  final VoidCallback onContinue;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: List.generate(length, (i) {
-        return Container(
-          width: Margins.spacing_s,
-          height: Margins.spacing_s,
-          margin: const EdgeInsets.symmetric(horizontal: Margins.spacing_xs),
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: i == index ? Colors.white : Colors.white.withOpacity(0.5),
-          ),
-        );
-      }),
+    return _PageContent(
+      title: Strings.firstLaunchOnboardingCardTitle1,
+      subtitle: Strings.firstLaunchOnboardingCardContent1,
+      icon: AppIcons.people_outline_rounded,
+      onContinue: onContinue,
     );
   }
 }
 
-class _FirstLaunchOnboardingCard extends StatelessWidget {
-  const _FirstLaunchOnboardingCard({
+class _SecondPageContent extends StatelessWidget {
+  const _SecondPageContent({required this.onContinue});
+
+  final VoidCallback onContinue;
+
+  @override
+  Widget build(BuildContext context) {
+    return _PageContent(
+      title: Strings.firstLaunchOnboardingCardTitle2,
+      subtitle: Strings.firstLaunchOnboardingCardContent2,
+      icon: AppIcons.chat_outlined,
+      onContinue: onContinue,
+    );
+  }
+}
+
+class _ThirdPageContent extends StatelessWidget {
+  const _ThirdPageContent({required this.onContinue});
+
+  final VoidCallback onContinue;
+
+  @override
+  Widget build(BuildContext context) {
+    return _PageContent(
+      title: Strings.firstLaunchOnboardingCardTitle3,
+      subtitle: Strings.firstLaunchOnboardingCardContent3,
+      icon: AppIcons.lock_rounded,
+      onContinue: onContinue,
+    );
+  }
+}
+
+class _PageContent extends StatelessWidget {
+  const _PageContent({
     required this.title,
     required this.subtitle,
     required this.icon,
@@ -142,7 +201,9 @@ class _FirstLaunchOnboardingCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CardContainer(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: Margins.spacing_m),
+      child: CardContainer(
         padding: const EdgeInsets.all(Margins.spacing_m),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -159,68 +220,56 @@ class _FirstLaunchOnboardingCard extends StatelessWidget {
             Text(title, style: TextStyles.textBaseBold),
             SizedBox(height: Margins.spacing_s),
             Text(subtitle, style: TextStyles.textSRegular()),
-            SizedBox(height: Margins.spacing_m),
+            Expanded(child: SizedBox.shrink()),
             PrimaryActionButton(label: Strings.continueLabel, onPressed: onContinue),
+            SizedBox(height: Margins.spacing_s),
           ],
-        ));
-  }
-}
-
-class _InitialPageContent extends StatelessWidget {
-  const _InitialPageContent({required this.onStart});
-
-  final VoidCallback onStart;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(width: double.infinity, child: SecondaryButton(label: Strings.start, onPressed: onStart));
-  }
-}
-
-class _FirstPageContent extends StatelessWidget {
-  const _FirstPageContent({required this.onContinue});
-
-  final VoidCallback onContinue;
-
-  @override
-  Widget build(BuildContext context) {
-    return _FirstLaunchOnboardingCard(
-      title: Strings.firstLaunchOnboardingCardTitle1,
-      subtitle: Strings.firstLaunchOnboardingCardContent1,
-      icon: AppIcons.people_outline_rounded,
-      onContinue: onContinue,
+        ),
+      ),
     );
   }
 }
 
-class _SecondPageContent extends StatelessWidget {
-  const _SecondPageContent({required this.onContinue});
+class _CarouselStepperIndicator extends StatefulWidget {
+  final PageController _pageController;
 
-  final VoidCallback onContinue;
+  const _CarouselStepperIndicator(this._pageController);
+
+  @override
+  State<_CarouselStepperIndicator> createState() => _CarouselStepperIndicatorState();
+}
+
+class _CarouselStepperIndicatorState extends State<_CarouselStepperIndicator> {
+  static const _length = 3;
+  int currentPage = 0;
+
+  @override
+  void initState() {
+    widget._pageController.addListener(() {
+      setState(() => currentPage = widget._pageController.page!.round());
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return _FirstLaunchOnboardingCard(
-      title: Strings.firstLaunchOnboardingCardTitle2,
-      subtitle: Strings.firstLaunchOnboardingCardContent2,
-      icon: AppIcons.chat_outlined,
-      onContinue: onContinue,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(_length, (i) {
+        return Container(
+          width: Margins.spacing_s,
+          height: Margins.spacing_s,
+          margin: const EdgeInsets.symmetric(horizontal: Margins.spacing_xs),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: i == currentPage ? Colors.white : Colors.white.withOpacity(0.5),
+          ),
+        );
+      }),
     );
   }
 }
 
-class _ThirdPageContent extends StatelessWidget {
-  const _ThirdPageContent({required this.onContinue});
-
-  final VoidCallback onContinue;
-
-  @override
-  Widget build(BuildContext context) {
-    return _FirstLaunchOnboardingCard(
-      title: Strings.firstLaunchOnboardingCardTitle3,
-      subtitle: Strings.firstLaunchOnboardingCardContent3,
-      icon: AppIcons.lock_rounded,
-      onContinue: onContinue,
-    );
-  }
+extension on PageController {
+  Future<void> next() => nextPage(duration: AnimationDurations.medium, curve: Curves.fastEaseInToSlowEaseOut);
 }
