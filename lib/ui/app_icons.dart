261dffcd5085e8b4d77787f7f8f18b01acbc1902
gabriel.adgeg <gad@octo.com>
2024-05-27 13:05:41 +0200

    refactor(mon-suivi-pe): Remove all unused icons

--- lib/ui/app_icons.dart
+++ lib/ui/app_icons.dart
@@ -3,17 +3,7 @@ import 'package:pass_emploi_app/ui/app_icons_additional_icons.dart';
 
 class AppIcons {
   // Navigation
-  static const IconData menu_rounded = Icons.menu_rounded;
-  static const IconData arrow_forward_rounded = Icons.arrow_forward_rounded;
-  static const IconData swap_vert_rounded = Icons.swap_vert_rounded;
-  static const IconData arrow_downward_rounded = Icons.arrow_downward_rounded;
-  static const IconData arrow_upward_rounded = Icons.arrow_upward_rounded;
-  static const IconData last_page_rounded = Icons.last_page_rounded;
-  static const IconData first_page_rounded = Icons.first_page_rounded;
   static const IconData open_in_new_rounded = Icons.open_in_new_rounded;
-  static const IconData logout_rounded = Icons.logout_rounded;
-  static const IconData expand_more_rounded = Icons.expand_more_rounded;
-  static const IconData expand_less_rounded = Icons.expand_less_rounded;
   static const IconData close_rounded = Icons.close_rounded;
   static const IconData chevron_right_rounded = Icons.chevron_right_rounded;
   static const IconData chevron_left_rounded = Icons.chevron_left_rounded;
@@ -21,7 +11,6 @@ class AppIcons {
   // Action
   static const IconData outgoing_mail = Icons.outgoing_mail;
   static const IconData notifications_rounded = Icons.notifications_rounded;
-  static const IconData person_add_rounded = Icons.person_add_rounded;
   static const IconData refresh_rounded = Icons.refresh_rounded;
   static const IconData download_rounded = Icons.download_rounded;
   static const IconData attach_file_rounded = Icons.attach_file_rounded;
@@ -31,8 +20,6 @@ class AppIcons {
   static const IconData tune_rounded = Icons.tune_rounded;
   static const IconData add_rounded = Icons.add_rounded;
   static const IconData share_rounded = Icons.share_rounded;
-  static const IconData bookmark_rounded = Icons.bookmark_rounded;
-  static const IconData bookmark_outline_rounded = Icons.bookmark_outline_rounded;
   static const IconData content_copy_rounded = Icons.content_copy_rounded;
   static const IconData camera_alt_outlined = Icons.camera_alt_outlined;
   static const IconData image_outlined = Icons.image_outlined;
@@ -40,22 +27,14 @@ class AppIcons {
   static const IconData description_outlined = Icons.description_outlined;
 
   // Information
-  static const IconData videocam_rounded = Icons.videocam_rounded;
   static const IconData signpost_rounded = Icons.signpost_rounded;
-  static const IconData rocket_launch_rounded = Icons.rocket_launch_rounded;
-  static const IconData timer_rounded = Icons.timer_rounded;
   static const IconData lock_rounded = Icons.lock_rounded;
-  static const IconData flag_rounded = Icons.flag_rounded;
   static const IconData schedule = Icons.schedule;
   static const IconData schedule_rounded = AppIconsAdditional.schedule;
   static const IconData error_rounded = Icons.error_rounded;
-  static const IconData person_rounded = Icons.person_rounded;
   static const IconData info_rounded = Icons.info_rounded;
-  static const IconData apartment_rounded = Icons.apartment_rounded;
-  static const IconData call_rounded = Icons.call_rounded;
   static const IconData cancel_rounded = Icons.cancel_rounded;
   static const IconData euro_rounded = Icons.euro_rounded;
-  static const IconData help_rounded = Icons.help_rounded;
   static const IconData event = Icons.event_outlined;
   static const IconData event_repeat_rounded = Icons.event_repeat_rounded;
   static const IconData description_rounded = Icons.description_rounded;
@@ -63,7 +42,6 @@ class AppIcons {
   static const IconData today_rounded = Icons.today_rounded;
   static const IconData today_outlined = Icons.today_outlined;
   static const IconData account_circle_rounded = Icons.account_circle_rounded;
-  static const IconData account_circle_outlined = Icons.account_circle_outlined;
   static const IconData pageview_rounded = Icons.pageview;
   static const IconData pageview_outlined = Icons.pageview_outlined;
   static const IconData home_rounded = Icons.home_rounded;
@@ -71,17 +49,9 @@ class AppIcons {
   static const IconData chat_rounded = Icons.chat_rounded;
   static const IconData chat_outlined = Icons.chat_outlined;
   static const IconData checklist_rounded = Icons.checklist_rtl_rounded;
-  static const IconData rule_rounded = Icons.rule_rounded;
   static const IconData favorite_rounded = Icons.favorite_rounded;
   static const IconData favorite_outline_rounded = Icons.favorite_outline_rounded;
-  static const IconData group_rounded = Icons.group_rounded;
-  static const IconData group_outlined = Icons.group_outlined;
-  static const IconData check_circle_rounded = Icons.check_circle_rounded;
   static const IconData check_circle_outline_rounded = Icons.check_circle_outline_rounded;
-  static const IconData arrow_circle_right_rounded = Icons.arrow_circle_right_rounded;
-  static const IconData arrow_circle_right_outlined = Icons.arrow_circle_right_outlined;
-  static const IconData leaderboard_rounded = Icons.leaderboard_rounded;
-  static const IconData leaderboard_outline_rounded = Icons.leaderboard_outlined;
   static const IconData forum_rounded = Icons.forum_rounded;
   static const IconData bolt_rounded = Icons.bolt_rounded;
   static const IconData bolt_outlined = AppIconsAdditional.bolt_outlined;
@@ -91,11 +61,8 @@ class AppIcons {
   static const IconData add_alert_rounded = Icons.add_alert_rounded;
   static const IconData remove_alert_rounded = Icons.notifications_off_rounded;
   static const IconData construction = Icons.construction;
-  static const IconData folder_off = Icons.folder_off;
-  static const IconData diversity_3 = Icons.diversity_3_rounded;
   static const IconData connectivity_off = Icons.wifi_off_rounded;
   static const IconData celebration_rounded = Icons.celebration_rounded;
-  static const IconData account_tree_rounded = Icons.account_tree_rounded;
   static const IconData people_outline_rounded = Icons.people_outline_rounded;
   static const IconData hotel_class = Icons.hotel_class_outlined;
   static const IconData warning_rounded = Icons.warning_rounded;
