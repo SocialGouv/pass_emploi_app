535ef3616c31a1079c4dd76fd076c76d374464ff
gabriel.adgeg <gad@octo.com>
2024-06-18 11:55:05 +0200

    feat(tuto): Update tutorial

--- lib/models/tutorial/tutorial.dart
+++ lib/models/tutorial/tutorial.dart
@@ -1,13 +1,20 @@
 import 'package:pass_emploi_app/models/tutorial/tutorial_page.dart';
 
 class Tutorial {
-  static const String versionTimestamp = '1717421525';
-  static List<TutorialPage> milo = [];
-  static List<TutorialPage> pe = [
+  static const String versionTimestamp = '1718704177';
+  static List<TutorialPage> milo = [
     TutorialPage(
-      title: "Votre page \" Mon suivi\" fait peau neuve\u{00A0}!",
-      description: "Découvrez vos rendez-vous et démarches en un coup d'œil avec la nouvelle vue agenda.",
-      image: "assets/tuto/img_mon_suivi_landscape.webp",
+      title: "Modifiez, dupliquez et supprimez aisément vos actions\u{00A0}!",
+      description:
+          "Appuyez longuement sur une action pour voir les options d'édition. Vous pouvez ainsi modifier, dupliquer ou supprimer facilement vos actions\u{00A0}!",
+      image: "assets/tuto/img_mon_suivi.webp",
+    ),
+    TutorialPage(
+      title: "Modifiez, dupliquez et supprimez aisément vos actions\u{00A0}!",
+      description:
+          "Retrouvez également ces fonctions en appuyant sur les trois petits points dans le détail de l'action.",
+      image: "assets/tuto/img_mon_action.webp",
     ),
   ];
+  static List<TutorialPage> pe = [];
 }
