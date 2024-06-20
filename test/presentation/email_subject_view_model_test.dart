4a6dd1f81fa8025b9eff44ab548cd8f2fa175e3a
gabriel.adgeg <gad@octo.com>
2024-06-20 16:06:54 +0200

    feat(aij): Update email subject for accompagnement AIJ

--- test/presentation/email_subject_view_model_test.dart
+++ test/presentation/email_subject_view_model_test.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pass_emploi_app/models/accompagnement.dart';
+import 'package:pass_emploi_app/models/login_mode.dart';
 import 'package:pass_emploi_app/presentation/email_subject_view_model.dart';
 
 import '../dsl/app_state_dsl.dart';
@@ -6,10 +8,12 @@ import '../dsl/app_state_dsl.dart';
 void main() {
   group('EmailObjectViewModel', () {
     group('contactEmailObject', () {
-      group('when app is CEJ', () {
+      group('when accompagnement is and app are CEJ', () {
         test('should return expected email subject for login mode MILO', () {
           // Given
-          final store = givenState().loggedInMiloUser().store();
+          final store = givenState() //
+              .loggedInUser(loginMode: LoginMode.MILO, accompagnement: Accompagnement.cej)
+              .store();
 
           // When
           final viewModel = EmailObjectViewModel.create(store);
@@ -21,7 +25,9 @@ void main() {
 
         test('should return expected email subject for login mode POLE_EMPLOI', () {
           // Given
-          final store = givenState().loggedInPoleEmploiUser().store();
+          final store = givenState() //
+              .loggedInUser(loginMode: LoginMode.POLE_EMPLOI, accompagnement: Accompagnement.cej)
+              .store();
 
           // When
           final viewModel = EmailObjectViewModel.create(store);
@@ -32,38 +38,46 @@ void main() {
         });
       });
 
-      group('when brand is pass emploi', () {
-        test('should return expected email subject for login mode MILO', () {
-          // Given
-          final store = givenPassEmploiState().loggedInMiloUser().store();
-
-          // When
-          final viewModel = EmailObjectViewModel.create(store);
-
-          // Then
-          expect(viewModel.contactEmailObject,
-              "Mission Locale - Prise de contact avec l’équipe de l’application pass emploi");
-        });
-
+      group('when accompagnement is RSA and app is pass emploi', () {
         test('should return expected email subject for login mode POLE_EMPLOI', () {
           // Given
-          final store = givenPassEmploiState().loggedInPoleEmploiUser().store();
+          final store = givenPassEmploiState() //
+              .loggedInUser(accompagnement: Accompagnement.rsa)
+              .store();
 
           // When
           final viewModel = EmailObjectViewModel.create(store);
 
           // Then
           expect(viewModel.contactEmailObject,
-              "France Travail - Prise de contact avec l’équipe de l’application pass emploi");
+              "France Travail - RSA - Prise de contact avec l’équipe de l’application pass emploi");
+        });
+      });
+
+      group('when accompagnement is AIJ and app is pass emploi', () {
+        test('should return expected email subject for login mode POLE_EMPLOI', () {
+          // Given
+          final store = givenPassEmploiState() //
+              .loggedInUser(accompagnement: Accompagnement.aij)
+              .store();
+
+          // When
+          final viewModel = EmailObjectViewModel.create(store);
+
+          // Then
+          expect(viewModel.contactEmailObject,
+              "France Travail - AIJ - Prise de contact avec l’équipe de l’application pass emploi");
         });
       });
     });
 
     group('ratingEmailObject', () {
-      group('when app is CEJ', () {
+      group('when accompagnement is and app are CEJ', () {
         test('should return expected email subject for login mode MILO', () {
           // Given
-          final store = givenState().loggedInMiloUser().store();
+          final store = givenState() //
+              .loggedInUser(loginMode: LoginMode.MILO, accompagnement: Accompagnement.cej)
+              .store();
 
           // When
           final viewModel = EmailObjectViewModel.create(store);
@@ -74,7 +88,9 @@ void main() {
 
         test('should return expected email subject for login mode POLE_EMPLOI', () {
           // Given
-          final store = givenState().loggedInPoleEmploiUser().store();
+          final store = givenState() //
+              .loggedInUser(loginMode: LoginMode.POLE_EMPLOI, accompagnement: Accompagnement.cej)
+              .store();
 
           // When
           final viewModel = EmailObjectViewModel.create(store);
@@ -84,27 +100,33 @@ void main() {
         });
       });
 
-      group('when brand is pass emploi', () {
-        test('should return expected email subject for login mode MILO', () {
-          // Given
-          final store = givenPassEmploiState().loggedInMiloUser().store();
-
-          // When
-          final viewModel = EmailObjectViewModel.create(store);
-
-          // Then
-          expect(viewModel.ratingEmailObject, "Mission Locale - Mon avis sur l’application pass emploi");
-        });
-
+      group('when accompagnement is RSA and app is pass emploi', () {
         test('should return expected email subject for login mode POLE_EMPLOI', () {
           // Given
-          final store = givenPassEmploiState().loggedInPoleEmploiUser().store();
+          final store = givenPassEmploiState() //
+              .loggedInUser(accompagnement: Accompagnement.rsa)
+              .store();
 
           // When
           final viewModel = EmailObjectViewModel.create(store);
 
           // Then
-          expect(viewModel.ratingEmailObject, "France Travail - Mon avis sur l’application pass emploi");
+          expect(viewModel.ratingEmailObject, "France Travail - RSA - Mon avis sur l’application pass emploi");
+        });
+      });
+
+      group('when accompagnement is AIJ and app is pass emploi', () {
+        test('should return expected email subject for login mode POLE_EMPLOI', () {
+          // Given
+          final store = givenPassEmploiState() //
+              .loggedInUser(accompagnement: Accompagnement.aij)
+              .store();
+
+          // When
+          final viewModel = EmailObjectViewModel.create(store);
+
+          // Then
+          expect(viewModel.ratingEmailObject, "France Travail - AIJ - Mon avis sur l’application pass emploi");
         });
       });
     });
