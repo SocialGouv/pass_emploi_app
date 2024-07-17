a84b6fb9771ac1fef5d34bc5e033fe89682e372f
Alwein <adrien.cens@edu.esiee.fr>
2024-07-17 14:55:50 +0200

    feat(2750): demarche duplication tuto

--- lib/models/tutorial/tutorial.dart
+++ lib/models/tutorial/tutorial.dart
@@ -1,20 +1,19 @@
 import 'package:pass_emploi_app/models/tutorial/tutorial_page.dart';
 
 class Tutorial {
-  static const String versionTimestamp = '1718704177';
-  static List<TutorialPage> milo = [
+  static const String versionTimestamp = '1718704179';
+  static List<TutorialPage> milo = [];
+  static List<TutorialPage> pe = [
     TutorialPage(
-      title: "Modifiez, dupliquez et supprimez aisément vos actions\u{00A0}!",
-      description:
-          "Appuyez longuement sur une action pour voir les options d'édition. Vous pouvez ainsi modifier, dupliquer ou supprimer facilement vos actions\u{00A0}!",
-      image: "assets/tuto/img_mon_suivi.webp",
+      title: "Dupliquez aisément vos démarches\u{00A0}!",
+      description: "Appuyez longuement sur une démarche pour pouvoir la dupliquer facilement\u{00A0}!",
+      image: "assets/tuto/img_mon_suivi_demarche.webp",
     ),
     TutorialPage(
-      title: "Modifiez, dupliquez et supprimez aisément vos actions\u{00A0}!",
+      title: "Dupliquez aisément vos démarches\u{00A0}!",
       description:
-          "Retrouvez également ces fonctions en appuyant sur les trois petits points dans le détail de l'action.",
-      image: "assets/tuto/img_mon_action.webp",
+          "Retrouvez également la duplication en appuyant sur les trois petits points dans le détail de la démarche.",
+      image: "assets/tuto/img_ma_demarche.webp",
     ),
   ];
-  static List<TutorialPage> pe = [];
 }
