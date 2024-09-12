99a4c1bdeee0c5a51d49d43a2ef01e140278d96e
Alwein <adrien.cens@edu.esiee.fr>
2024-09-12 16:48:37 +0200

    feat(a11y): make rich text responsive

--- lib/features/mode_demo/explication_page_mode_demo.dart
+++ lib/features/mode_demo/explication_page_mode_demo.dart
@@ -97,6 +97,7 @@ class _Contenu extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
                 child: RichText(
+                  textScaler: MediaQuery.of(context).textScaler,
                   text: TextSpan(
                     children: [
                       TextSpan(text: Strings.modeDemoExplicationSecondPoint1, style: TextStyles.textBaseRegular),
@@ -108,6 +109,7 @@ class _Contenu extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(left: 24, bottom: 30, right: 24),
                 child: RichText(
+                  textScaler: MediaQuery.of(context).textScaler,
                   text: TextSpan(
                     children: [
                       TextSpan(text: Strings.modeDemoExplicationTroisiemePoint1, style: TextStyles.textBaseRegular),
