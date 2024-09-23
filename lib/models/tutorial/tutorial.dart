4d48497024b35eb1d2f847162b150a10df2ce128
Alwein <adrien.cens@edu.esiee.fr>
2024-09-23 11:36:35 +0200

    feat(2897): tuto pj

--- lib/models/tutorial/tutorial.dart
+++ lib/models/tutorial/tutorial.dart
@@ -1,19 +1,14 @@
 import 'package:pass_emploi_app/models/tutorial/tutorial_page.dart';
 
 class Tutorial {
-  static const String versionTimestamp = '1718704179';
-  static List<TutorialPage> milo = [];
-  static List<TutorialPage> pe = [
+  static const String versionTimestamp = '1718704180';
+  static List<TutorialPage> milo = [
     TutorialPage(
-      title: "Dupliquez aisément vos démarches\u{00A0}!",
-      description: "Appuyez longuement sur une démarche pour pouvoir la dupliquer facilement\u{00A0}!",
-      image: "assets/tuto/img_mon_suivi_demarche.webp",
-    ),
-    TutorialPage(
-      title: "Dupliquez aisément vos démarches\u{00A0}!",
+      title: "Partagez des pièces-jointes à votre conseiller\u{00A0}!",
       description:
-          "Retrouvez également la duplication en appuyant sur les trois petits points dans le détail de la démarche.",
-      image: "assets/tuto/img_ma_demarche.webp",
+          "Rendez-vous dans votre messagerie puis appuyez sur le trombone à gauche de l'encart message pour partager une photo ou un fichier.",
+      image: "assets/tuto/img_pj_tuto.webp",
     ),
   ];
+  static List<TutorialPage> pe = [];
 }
