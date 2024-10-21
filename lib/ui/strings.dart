1bcbb2e7c132961e6448553beb24ad3fdc266449
gabriel.adgeg <gad@octo.com>
2024-10-21 12:09:27 +0200

    feat(favoris): Update wordings

--- lib/ui/strings.dart
+++ lib/ui/strings.dart
@@ -1458,9 +1458,9 @@ class Strings {
   static String a11yStatus = "Statut : ";
 
   // textes alternatifs
-  static String favoriHeartRemove(String offre) => "Retirer l'offre $offre des favoris";
+  static String favoriHeartRemove(String offre) => "Retirer l'offre $offre des offres enregistrées";
 
-  static String favoriHeartAdd(String offre) => "Mettre l'offre $offre en favoris";
+  static String favoriHeartAdd(String offre) => "Mettre l'offre $offre des offres enregistrées";
   static String link = "Lien";
   static String openInNewTab = "Ouvrir dans un nouvel onglet";
   static String semanticsLabelInformation = "Information";
