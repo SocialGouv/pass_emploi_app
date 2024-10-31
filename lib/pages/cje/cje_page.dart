75f5b0d066e3c833a157d7cbac70b3b2723c7d74
gabriel.adgeg <gad@octo.com>
2024-10-31 12:37:44 +0100

    feat(cje): Launch external url on special routes

--- lib/pages/cje/cje_page.dart
+++ lib/pages/cje/cje_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pass_emploi_app/utils/launcher_utils.dart';
+import 'package:pass_emploi_app/utils/log.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 const url = "URL";
@@ -10,6 +12,19 @@ class CjePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (NavigationRequest request) {
+            Log.i('[CJE web view] Navigate to ${request.url}');
+            if (request.url.contains('signup') || request.url.contains('login-widget')) {
+              final navigator = Navigator.of(context);
+              launchExternalUrl(request.url).then((value) => navigator.pop());
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
       ..loadRequest(Uri.parse(url));
     return Scaffold(
       body: SafeArea(child: WebViewWidget(controller: controller)),
