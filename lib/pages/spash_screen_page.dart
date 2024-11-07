6cb90399a88e849b2247f0ce371adc69a37fde77
Alwein <adrien.cens@edu.esiee.fr>
2024-11-07 12:24:08 +0100

    feat: add clear shared prefs on splash screen

--- lib/pages/spash_screen_page.dart
+++ lib/pages/spash_screen_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pass_emploi_app/features/developer_option/activation/developer_options_action.dart';
 import 'package:pass_emploi_app/ui/margins.dart';
 import 'package:pass_emploi_app/ui/text_styles.dart';
+import 'package:pass_emploi_app/utils/context_extensions.dart';
 import 'package:pass_emploi_app/widgets/biseau_background.dart';
 import 'package:pass_emploi_app/widgets/drawables/app_logo.dart';
+import 'package:pass_emploi_app/widgets/snack_bar/show_snack_bar.dart';
 import 'package:pass_emploi_app/wrappers/package_info_wrapper.dart';
 
 class SplashScreenPage extends StatelessWidget {
@@ -35,11 +38,17 @@ class _AppVersion extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.bottomRight,
-      child: Padding(
-        padding: const EdgeInsets.all(Margins.spacing_base),
-        child: Text(
-          _appVersion,
-          style: TextStyles.textBaseBoldWithColor(Colors.white),
+      child: GestureDetector(
+        onDoubleTap: () {
+          context.dispatch(DeveloperOptionsDeleteAllPrefsAction());
+          showSnackBarWithSystemError(context, "Killez 💀- voire supprimer 🗑 - l'app");
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(Margins.spacing_base),
+          child: Text(
+            _appVersion,
+            style: TextStyles.textBaseBoldWithColor(Colors.white),
+          ),
         ),
       ),
     );
