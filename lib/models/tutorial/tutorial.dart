6cb73af6febfa0a8da657a0610225adfc5bec856
Alwein <adrien.cens@edu.esiee.fr>
2025-01-14 10:47:05 +0100

    fix(tutorial): update tutorial content and version timestamp

--- lib/models/tutorial/tutorial.dart
+++ lib/models/tutorial/tutorial.dart
@@ -1,21 +1,21 @@
 import 'package:pass_emploi_app/models/tutorial/tutorial_page.dart';
 
 class Tutorial {
-  static const String versionTimestamp = '1718704181';
+  static const String versionTimestamp = '1718704182';
   static List<TutorialPage> milo = [
     TutorialPage(
-      title: "Vos favoris font peaux neuves\u{00A0}!",
+      title: "Retrouvez vos notifications en un clic\u{00A0}!",
       description:
-          "Enregistrez vos offres et retrouvez les facilement dans l'onglet ”Offres enregistrées” présent dans la page ”Offre”",
-      image: "assets/tuto/img_offres_enregistrees_tuto.webp",
+          "Depuis la page d'accueil, retrouvez les notifications liées à vos RDV et événements, vos rappels ainsi que vos alertes d'emploi.",
+      image: "assets/tuto/tuto.webp",
     ),
   ];
   static List<TutorialPage> pe = [
     TutorialPage(
-      title: "Vos favoris font peaux neuves\u{00A0}!",
+      title: "Retrouvez vos notifications en un clic\u{00A0}!",
       description:
-          "Enregistrez vos offres et retrouvez les facilement dans l'onglet ”Offres enregistrées” présent dans la page ”Offre”",
-      image: "assets/tuto/img_offres_enregistrees_tuto.webp",
+          "Depuis la page d'accueil, retrouvez les notifications liées à vos RDV et événements, vos rappels ainsi que vos alertes d'emploi.",
+      image: "assets/tuto/tuto.webp",
     ),
   ];
 }
