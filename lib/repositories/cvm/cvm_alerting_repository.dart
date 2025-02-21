1cc78514aecea9d71b6e7dbdb0292d16b8a16a18
Alwein <adrien.cens@edu.esiee.fr>
2025-02-21 10:50:44 +0100

    feat(cvm_alerting): add comment to clarify 404 error logging in traceFailure method

--- lib/repositories/cvm/cvm_alerting_repository.dart
+++ lib/repositories/cvm/cvm_alerting_repository.dart
@@ -5,6 +5,7 @@ class CvmAlertingRepository {
 
   CvmAlertingRepository(this._httpClient);
 
+  // On poste sur une route qui existe pas pour logger une erreur 404
   Future<void> traceFailure() async {
     try {
       await _httpClient.post('/app/logs/cvm/failure');
