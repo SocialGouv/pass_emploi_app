a677a0ed4080c575c93698e6f45686d23671a378
Alwein <adrien.cens@edu.esiee.fr>
2025-02-25 10:32:55 +0100

    feat(tutorial): update tutorial messages for improved clarity and user guidance

--- lib/models/tutorial/tutorial.dart
+++ lib/models/tutorial/tutorial.dart
@@ -1,21 +1,14 @@
 import 'package:pass_emploi_app/models/tutorial/tutorial_page.dart';
 
 class Tutorial {
-  static const String versionTimestamp = '1718704182';
+  static const String versionTimestamp = '1718704183';
   static List<TutorialPage> milo = [
     TutorialPage(
-      title: "Retrouvez vos notifications en un clic\u{00A0}!",
+      title: "Inscrivez-vous facilement aux événements de votre mission locale\u{00A0}!",
       description:
-          "Depuis la page d'accueil, retrouvez les notifications liées à vos RDV et événements, vos rappels ainsi que vos alertes d'emploi.",
+          "Depuis l'onglet 'Événements', inscrivez-vous en toute autonomie aux ateliers et animations collectives qui vous intéressent. Ne manquez plus aucune opportunité !",
       image: "assets/tuto/tuto.webp",
     ),
   ];
-  static List<TutorialPage> pe = [
-    TutorialPage(
-      title: "Retrouvez vos notifications en un clic\u{00A0}!",
-      description:
-          "Depuis la page d'accueil, retrouvez les notifications liées à vos RDV et événements, vos rappels ainsi que vos alertes d'emploi.",
-      image: "assets/tuto/tuto.webp",
-    ),
-  ];
+  static List<TutorialPage> pe = [];
 }
