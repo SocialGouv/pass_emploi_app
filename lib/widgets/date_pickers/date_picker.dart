443feb95e9ebd3ecca8d1231b7c3e5759fdd3f21
Alwein <adrien.cens@edu.esiee.fr>
2025-03-18 10:36:52 +0100

    feat(date_picker): replace iOS date picker by Material Date Picker for ergonomy purpose

--- lib/widgets/date_pickers/date_picker.dart
+++ lib/widgets/date_pickers/date_picker.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pass_emploi_app/ui/app_colors.dart';
 import 'package:pass_emploi_app/ui/app_icons.dart';
 import 'package:pass_emploi_app/ui/strings.dart';
-import 'package:pass_emploi_app/ui/text_styles.dart';
 import 'package:pass_emploi_app/utils/accessibility_utils.dart';
 import 'package:pass_emploi_app/utils/date_extensions.dart';
 import 'package:pass_emploi_app/widgets/a11y/auto_focus.dart';
@@ -69,43 +65,9 @@ class _DatePickerState extends State<DatePicker> {
     );
   }
 
-  Future<void> openDatePicker(BuildContext context) =>
-      Platform.isIOS ? _iOSDatePicker(context) : _androidDatePicker(context);
+  Future<void> openDatePicker(BuildContext context) => datePicker(context);
 
-  Future<void> _iOSDatePicker(BuildContext context) async {
-    showCupertinoModalPopup(
-      context: context,
-      builder: (context) => Container(
-        height: 300,
-        color: Colors.white,
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed: () => Navigator.of(context).pop(),
-                child: Text(
-                  Strings.close,
-                  style: TextStyles.textSecondaryButton,
-                ),
-              ),
-            ),
-            Flexible(
-              child: CupertinoDatePicker(
-                  initialDateTime: widget.initialDateValue ?? DateTime.now(),
-                  minimumDate: widget.firstDate,
-                  mode: CupertinoDatePickerMode.date,
-                  onDateTimeChanged: (value) {
-                    widget.onDateSelected(value);
-                  }),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Future<void> _androidDatePicker(BuildContext context) async {
+  Future<void> datePicker(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
       context: context,
       firstDate: widget.firstDate ?? DateTime(2020),
