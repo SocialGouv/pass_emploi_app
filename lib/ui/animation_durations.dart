0803a039e638be0f770c2b700a5275909469d1f0
Alwein <adrien.cens@edu.esiee.fr>
2025-05-07 15:09:12 +0200

    wip

--- lib/ui/animation_durations.dart
+++ lib/ui/animation_durations.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 
 class AnimationDurations {
   static const Duration extraLong = Durations.extralong3;
+
   static const Duration verySlow = Durations.long2;
+
   static const Duration slow = Durations.medium4;
+
   static const Duration medium = Durations.medium2;
+
   static const Duration medium3 = Durations.medium3;
+
   static const Duration fast = Durations.short4;
+
   static const Duration veryFast = Durations.short2;
 }
